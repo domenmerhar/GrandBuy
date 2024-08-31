bb68cec98727a8471b159a5b95d5e486ce793a43
Domen Merhar <domen.merhar@students.vegova.si>
2024-08-31 13:29:08 +0200

    Add errorController.ts

---
+++ server/src/controllers/errorController.ts
@@ -0,0 +1,86 @@
+import { Request, Response, NextFunction } from "express";
+import AppError from "../utils/AppError";
+
+interface IError extends Error {
+  statusCode?: number;
+  status?: string;
+  isOperational?: boolean;
+  code?: number;
+  errors?: Record<string, any>;
+  path?: string;
+  value?: any;
+  errmsg?: string;
+}
+
+const sendErrorDev = (err: IError, res: Response) => {
+  res.status(err.statusCode || 500).json({
+    status: err.status || "error",
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const handleCastErrorDB = (err: IError) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err: IError) => {
+  const value = err.errmsg?.match(/"(.*?)"/)?.[0];
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err: IError) => {
+  const errors = Object.values(err.errors || {}).map((el: any) => el.message);
+  const message = `Invalid input data. ${errors.join(" ")}`;
+
+  return new AppError(message, 400);
+};
+
+const handleJWTError = () =>
+  new AppError("Invalid token. Please log in again.", 401);
+
+const handleJWTExpired = () =>
+  new AppError("Token expired. Please log in again.", 401);
+
+const sendErrorProd = (err: IError, res: Response) => {
+  if (err.isOperational) {
+    return res.status(err.statusCode || 500).json({
+      status: err.status || "error",
+      message: err.message,
+    });
+  }
+
+  console.error("ERROR", err);
+
+  res.status(500).json({
+    status: "error",
+    message: "Something went wrong",
+  });
+};
+
+export default (
+  err: IError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    let error: IError = { ...err };
+
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (err.name === "ValidationError") error = handleValidationErrorDB(err);
+    if (err.name === "JsonWebTokenError") error = handleJWTError();
+    if (err.name === "TokenExpiredError") error = handleJWTExpired();
+    sendErrorProd(error, res);
+  }
+};
