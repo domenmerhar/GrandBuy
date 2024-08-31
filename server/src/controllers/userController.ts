63422ad14374b8bfd308b0d9fdec24fa246eca12
Domen Merhar <domen.merhar@students.vegova.si>
2024-08-31 13:29:36 +0200

    Update userController.ts

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,8 +1,51 @@
 import { Request, Response } from "express";
+import User from "../models/userModel";
+import catchAsync from "../utils/catchAsync";
 
-export const getUsers = (req: Request, res: Response) => {
-  res.status(200).json({ message: "GET /user" });
+export const getUsers = async (req: Request, res: Response) => {
+  const users = await User.find();
+
+  if (users.length === 0) {
+    return res.status(404).json({
+      status: "fail",
+      message: "No users found.",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    length: users.length,
+    data: {
+      users: [],
+    },
+  });
 };
+
+export const createUser = catchAsync(
+  async (
+    req: Request<
+      {},
+      {
+        username: string;
+        email: string;
+        password: string;
+        confirmPassword: string;
+      }
+    >,
+    res: Response
+  ) => {
+    const { username, email, password, confirmPassword } = req.body;
+
+    const newUser = await User.create({
+      username,
+      email,
+      password,
+      confirmPassword,
+    });
+
+    res.status(201).json({ status: "sucess", data: newUser });
+  }
+);
 
 export const updateUser = (req: Request, res: Response) => {
   res.status(200).json({ message: "PATCH /user" });
