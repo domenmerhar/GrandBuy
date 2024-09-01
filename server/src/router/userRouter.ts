f692a16c5306ad64f8ef3c02600b2b302a260c66
Domen Merhar <domen.merhar@students.vegova.si>
2024-09-01 08:19:56 +0200

    Update userRouter.ts

--- server/src/router/userRouter.ts
+++ server/src/router/userRouter.ts
@@ -13,23 +13,20 @@ import {
 import { protect, restrictTo } from "../controllers/authController";
 const userRouter = Router();
 
-if (process.env.NODE_ENV === "development") {
-  userRouter.route("/").get(protect, getUsers);
-}
-
-userRouter.route("/updateMe").patch(updateMe);
-
 userRouter.route("/login").post(login);
 
 userRouter.route("/signup").post(signup);
 
-userRouter.route("/logout").post(logout);
-
-userRouter.route("/change-password").patch(changePassword);
-
 userRouter.route("/forgot-password").patch(forgotPassword);
 
-userRouter.use("/:userId", protect, restrictTo("admin"));
+userRouter.use(protect);
+userRouter.route("/update-me").patch(updateMe);
+userRouter.route("/logout").post(logout);
+userRouter.route("/change-password").patch(changePassword);
+
+userRouter.use(restrictTo("admin"));
+userRouter.route("/").get(protect, restrictTo("admin"), getUsers);
+
 userRouter.route("/:userId").delete(deleteUser);
 userRouter.route("/:userId/role/:role").patch(updateRole);
 
