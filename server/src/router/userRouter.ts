79bc3427ed747bd9e75e8047071628dc10726474
Domen Merhar <domen.merhar@students.vegova.si>
2024-09-01 07:40:34 +0200

    update userRouter.ts

--- server/src/router/userRouter.ts
+++ server/src/router/userRouter.ts
@@ -1,21 +1,20 @@
 import { Router } from "express";
 import {
   changePassword,
-  createUser,
   deleteUser,
   forgotPassword,
   getUsers,
   login,
   logout,
-  signUp,
+  signup,
   updateMe,
   updateUser,
 } from "../controllers/userController";
-import { protect } from "../controllers/authController";
+import { protect, restrictTo } from "../controllers/authController";
 const userRouter = Router();
 
 if (process.env.NODE_ENV === "development") {
-  userRouter.route("/").get(protect, getUsers).post(createUser);
+  userRouter.route("/").get(protect, getUsers);
 }
 
 userRouter.route("/:userId").patch(updateUser).delete(deleteUser);
@@ -24,7 +23,7 @@ userRouter.route("/updateMe").patch(updateMe);
 
 userRouter.route("/login").post(login);
 
-userRouter.route("/signup").post(signUp);
+userRouter.route("/signup").post(signup);
 
 userRouter.route("/logout").post(logout);
 
