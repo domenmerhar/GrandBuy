8fd8d39f6be7b18f995a260f35533f5c467aa0f1
Domen Merhar <domen.merhar@students.vegova.si>
2024-09-02 15:52:48 +0200

    Add productModel.ts

---
+++ server/src/models/productModel.ts
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: [true, "Please provide a user ID."],
+  },
+  name: {
+    type: String,
+    minLength: [1, "Please provide a name with at least 1 character."],
+    maxLength: [50, "Please provide a name shorter than 50 characters."],
+    required: [true, "Please provide a name."],
+  },
+  images: [
+    {
+      type: String,
+    },
+  ],
+  price: {
+    type: Number,
+    min: [0, "Please provide a price higher than 0."],
+    max: [10000, "Please provide a price lower than 10000."],
+    required: [true, "Please provide a price."],
+  },
+  shipping: {
+    type: Number,
+    validate: {
+      validator: (val: string) => +val >= 0 && +val <= 10000,
+    },
+    required: [true, "Please provide a price."],
+  },
+  descriptionLink: {
+    type: Number,
+  },
+  lastChanged: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+ProductSchema.virtual("averageRating");
+
+//TODO: ADD INDEX
+
+export default mongoose.model("Product", ProductSchema);
