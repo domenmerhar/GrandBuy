c9d69cae467174f8156b491cdce5f53c762b4f1a
Domen Merhar <domen.merhar@students.vegova.si>
2024-09-02 13:33:47 +0200

    Add reviwModel.ts

---
+++ server/src/models/reviewModel.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const ReviewSchema = new mongoose.Schema({
+  userId: {},
+  rating: {
+    type: Number,
+    min: [1, "Please provide a rating higher than 1."],
+    max: [5, "Please provide a rating lower than 5."],
+    required: [true, "Please provide a rating."],
+  },
+  review: {
+    type: String,
+    minLength: [1, "Please provide a review with at least 1 character."],
+    maxLength: [500, "Please provide a review shorter than 500 characters."],
+    required: [true, "Please provide a review."],
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  lastChange: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export default mongoose.model("Review", ReviewSchema);
