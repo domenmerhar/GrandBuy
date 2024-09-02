87b203863c49571c180a6b1200dab38735e10c6d
Domen Merhar <domen.merhar@students.vegova.si>
2024-09-02 15:53:05 +0200

    Add productRouter.ts

---
+++ server/src/router/productRouter.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import {
+  createProduct,
+  getHighestDiscount,
+  getProducts,
+  getSellerProducts,
+  updateProduct,
+} from "../controllers/productController";
+import { protect, restrictTo } from "../controllers/authController";
+
+const productRouter = express.Router();
+
+productRouter
+  .route("/")
+  .get(getProducts)
+  .post(protect, restrictTo("seller"), createProduct);
+
+productRouter.route("/highest-discount").get(getHighestDiscount, getProducts);
+
+productRouter.route("/seller/:sellerId").get(getSellerProducts);
+
+productRouter
+  .route("/:productId")
+  .patch(protect, restrictTo("seller"), updateProduct)
+  .delete(protect, restrictTo("seller"), updateProduct);
+
+export default productRouter;
