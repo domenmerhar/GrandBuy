39377ebc378469287f4ad3524c40bb09100ad0de
Domen Merhar <domen.merhar@students.vegova.si>
2024-09-03 07:42:26 +0200

    Update reviewController.ts

--- server/src/controllers/reviewController.ts
+++ server/src/controllers/reviewController.ts
@@ -1,21 +1,28 @@
 import { NextFunction, Request, Response } from "express";
 import catchAsync from "../utils/catchAsync";
-
-export const getReviews = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).json({ message: "getReviews" });
-  }
-);
+import Review from "../models/reviewModel";
+import path from "path";
 
 export const getProductReviews = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
+    //TODO: GET REVIEW STATS
     res.status(200).json({ message: "getProductReviews" });
   }
 );
 
 export const createReview = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    res.status(201).json({ message: "createReview" });
+    const { rating, review } = req.body;
+    const { productId } = req.params;
+
+    const newReview = await Review.create({
+      userId: res.locals.user._id,
+      productId,
+      review,
+      rating,
+    });
+
+    res.status(201).json({ status: "success", data: newReview });
   }
 );
 
@@ -48,3 +55,15 @@ export const deleteReview = catchAsync(
     res.status(204).json({ message: "deleteReview" });
   }
 );
+
+// export const likeComment = catchAsync(
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     res.status(204).json({ message: "likeComment" });
+//   }
+// );
+
+// export const dislikeComment = catchAsync(
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     res.status(204).json({ message: "dislikeComment" });
+//   }
+// );
