f98ad3b5eb17e733d04b7ae6c47bb89061976d18
Domen Merhar <domen.merhar@students.vegova.si>
2024-09-04 18:52:48 +0200

    update wishlistItemController.ts

--- server/src/controllers/wishlistItemController.ts
+++ server/src/controllers/wishlistItemController.ts
@@ -1,9 +1,30 @@
 import { NextFunction, Request, Response } from "express";
 import catchAsync from "../utils/catchAsync";
 import WishlistItem from "../models/wishlistItemModel";
+import APIFeatures from "../utils/ApiFeatures";
 
 export const getWishlist = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {}
+  async (req: Request, res: Response, next: NextFunction) => {
+    const userId = res.locals.user._id;
+
+    const features = new APIFeatures(
+      WishlistItem.find({ userId }).populate({
+        path: "productId",
+        select: "name price imageCover",
+      }),
+      req.query
+    );
+
+    const wishlistItems = await features.filter().sort().paginate().query;
+
+    res.status(200).json({
+      status: "success",
+      results: wishlistItems.length,
+      data: {
+        wishlistItems,
+      },
+    });
+  }
 );
 
 export const addToWishlist = catchAsync(
