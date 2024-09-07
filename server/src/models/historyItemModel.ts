1893d3b620f31491cc8e1d395af559870514cc51
Domen Merhar <domen.merhar@students.vegova.si>
2024-09-07 13:41:22 +0200

    update historyItemModel.ts

--- server/src/models/historyItemModel.ts
+++ server/src/models/historyItemModel.ts
@@ -4,11 +4,13 @@ const historyItemSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
+    required: true,
   },
 
   productId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Product",
+    required: true,
   },
 
   createdAt: {
