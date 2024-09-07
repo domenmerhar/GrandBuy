e9b996edcbde9ee30b81b17ddff2e7cad61eec47
Domen Merhar <domen.merhar@students.vegova.si>
2024-09-07 13:42:03 +0200

    Add cartItemModel.ts

---
+++ server/src/models/cartItemModel.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const CartItemSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+
+  quantity: {
+    type: Number,
+    min: [1, "Quantity must be at least 1"],
+    required: true,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+CartItemSchema.index({ userId: 1, productId: 1 }, { unique: true });
+
+export default mongoose.model("CartItem", CartItemSchema);
