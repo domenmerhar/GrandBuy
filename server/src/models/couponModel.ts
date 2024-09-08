9159d622d552057b138400645569abb4039b3ab8
Domen Merhar <domen.merhar@students.vegova.si>
2024-09-08 07:51:44 +0200

    Update couponModel.ts

--- server/src/models/couponModel.ts
+++ server/src/models/couponModel.ts
@@ -15,6 +15,9 @@ const CouponSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
+    trim: true,
+    maxLength: [20, "Code must be at most 20 characters"],
+    minLength: [1, "Code must be at least 1 character"],
   },
 
   discount: {
@@ -26,6 +29,12 @@ const CouponSchema = new mongoose.Schema({
   expireAt: {
     type: Date,
     required: true,
+    validate: {
+      validator: function (value: Date) {
+        return value > new Date();
+      },
+      message: "Expiration date must be in the future",
+    },
   },
 });
 
@@ -37,13 +46,14 @@ CouponSchema.pre("save", function (next) {
   next();
 });
 
-CouponSchema.virtual("isExpired").get(function () {
-  return this.expireAt < new Date();
-});
-
 CouponSchema.index({ expireAt: 1, code: 1 });
 
-CouponSchema.set("toJSON", { virtuals: true });
-CouponSchema.set("toObject", { virtuals: true });
+CouponSchema.set("toJSON", {
+  versionKey: false,
+});
+
+CouponSchema.set("toObject", {
+  versionKey: false,
+});
 
 export default mongoose.model("Coupon", CouponSchema);
