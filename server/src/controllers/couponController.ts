272b7f032f85b5b349e18f4b766fdf211874a0c6
Domen Merhar <domen.merhar@students.vegova.si>
2024-09-08 06:32:54 +0200

    Update couponController.ts

--- server/src/controllers/couponController.ts
+++ server/src/controllers/couponController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import catchAsync from "../utils/catchAsync";
 import Coupon from "../models/couponModel";
+import AppError from "../utils/AppError";
 
 export const getCoupon = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -22,7 +23,20 @@ export const addCoupon = catchAsync(
 
 export const updateCoupon = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).json({ status: "success" });
+    const { id } = req.params;
+    const { products, expireAt, discount } = req.body;
+
+    const coupon = await Coupon.findOne({ _id: id });
+
+    if (!coupon) return next(new AppError("Coupon not found", 404));
+
+    coupon.products = [...coupon.products, ...products];
+    coupon.expireAt = expireAt;
+    coupon.discount = discount;
+
+    await coupon.save();
+
+    res.status(200).json({ status: "success", data: { coupon } });
   }
 );
 
