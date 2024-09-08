199c06e02bc7eda501f047c12ee78429ab6960b8
Domen Merhar <domen.merhar@students.vegova.si>
2024-09-08 07:12:26 +0200

    Update couponController.ts

--- server/src/controllers/couponController.ts
+++ server/src/controllers/couponController.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import catchAsync from "../utils/catchAsync";
 import Coupon from "../models/couponModel";
 import AppError from "../utils/AppError";
+import CartItem from "../models/cartItemModel";
 
 export const getCoupon = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -28,7 +29,7 @@ export const updateCoupon = catchAsync(
 
     const coupon = await Coupon.findOne({ _id: id });
 
-    if (!coupon) return next(new AppError("Coupon not found", 404));
+    if (!coupon) return next(new AppError("Coupon not found.", 404));
 
     coupon.products = [...coupon.products, ...products];
     coupon.expireAt = expireAt;
@@ -46,7 +47,7 @@ export const deleteCoupon = catchAsync(
 
     const coupon = await Coupon.findByIdAndDelete(id);
 
-    if (!coupon) return next(new AppError("Coupon not found", 404));
+    if (!coupon) return next(new AppError("Coupon not found.", 404));
 
     res.status(204).json({ status: "success", data: null });
   }
@@ -54,6 +55,28 @@ export const deleteCoupon = catchAsync(
 
 export const applyCoupon = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).json({ status: "success" });
+    const { couponCode } = req.params;
+    const id = res.locals.user._id;
+
+    const coupon = await Coupon.findOne({
+      code: couponCode,
+      expireAt: { $gte: new Date() },
+    });
+
+    if (!coupon) return next(new AppError("Coupon not found.", 404));
+
+    const result = await Promise.all(
+      coupon.products.map((productId) =>
+        CartItem.findOneAndUpdate(
+          { userId: id, productId },
+          { discount: coupon.discount },
+          { new: true }
+        )
+      )
+    );
+
+    const cartItems = result.filter((item) => item !== null);
+
+    res.status(200).json({ status: "success", data: { cartItems } });
   }
 );
