420c7e8388888a2cea4dd59ef5656e0520fa47e1
Domen Merhar <domen.merhar@students.vegova.si>
2024-09-08 19:02:37 +0200

    Update app.ts, package.json; Implement rate limiting

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import { rateLimit } from "express-rate-limit";
 import helmet from "helmet";
 import morgan from "morgan";
 import userRouter from "./router/userRouter";
@@ -10,10 +11,19 @@ import historyRouter from "./router/historyRouter";
 import cartRouter from "./router/cartRouter";
 import couponRouter from "./router/couponRouter";
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: "draft-7",
+  legacyHeaders: false,
+});
+
 const app = express();
 
 app.use(helmet());
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+
+app.use(limiter);
 
 app.use(express.json());
 
