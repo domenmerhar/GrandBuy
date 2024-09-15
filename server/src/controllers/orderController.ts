81fb384eab102f001d0244b6c3979652c3aa2233
Domen Merhar <domen.merhar@students.vegova.si>
2024-09-15 12:32:06 +0200

    Update orderController.ts

--- server/src/controllers/orderController.ts
+++ server/src/controllers/orderController.ts
@@ -1,9 +1,26 @@
 import { NextFunction, Request, Response } from "express";
 import catchAsync from "../utils/catchAsync";
 import AppError from "../utils/AppError";
+import APIFeatures from "../utils/ApiFeatures";
+import Order from "../models/orderModel";
+import path from "path";
 
 export const getUserOrders = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {}
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = res.locals.user._id;
+
+    const ordersQuery = new APIFeatures(
+      Order.find({ user: id }).populate({ path: "products.product" }),
+      req.query
+    );
+
+    const orders = await ordersQuery.filter().sort().limitFields().paginate()
+      .query;
+
+    res
+      .status(200)
+      .json({ staus: "success", length: orders.length, data: { orders } });
+  }
 );
 
 export const addOrder = catchAsync(
