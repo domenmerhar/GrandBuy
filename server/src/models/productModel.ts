0d5c25db898cb129287f0220b46130be803644d5
Domen Merhar <domen.merhar@students.vegova.si>
2024-09-16 18:38:22 +0200

    Update productModel.ts

--- server/src/models/productModel.ts
+++ server/src/models/productModel.ts
@@ -6,50 +6,64 @@ const ProductSchema = new mongoose.Schema({
     ref: "User",
     required: [true, "Please provide a user ID."],
   },
+
   name: {
     type: String,
     minLength: [1, "Please provide a name with at least 1 character."],
     maxLength: [50, "Please provide a name shorter than 50 characters."],
     required: [true, "Please provide a name."],
   },
+
   coverImage: {
     type: String,
     required: [true, "Please provide a cover image."],
   },
+
   images: [
     {
       type: String,
       required: true,
     },
   ],
+
   price: {
     type: Number,
     min: [0, "Please provide a price higher than 0."],
     max: [10000, "Please provide a price lower than 10000."],
     required: [true, "Please provide a price."],
   },
+
   shipping: {
     type: Number,
     min: [0, "Please provide a shipping higher than 0."],
     max: [10000, "Please provide a shipping lower than 10000."],
     required: [true, "Please provide a price."],
   },
+
   discount: {
     type: Number,
     min: [0, "Please provide a discount higher than 0."],
     max: [100, "Please provide a discount lower than 100."],
     default: 0,
   },
+
   totalPrice: {
     type: Number,
   },
+
   description: {
     type: String,
     //required: true,
   },
+
   lastChanged: {
     type: Date,
     default: Date.now(),
+  },
+
+  isSelling: {
+    type: Boolean,
+    default: true,
   },
 });
 
