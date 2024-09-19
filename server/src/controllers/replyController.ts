ed3e58488ad522fb8c37624a3e96299b576f1ae4
Domen Merhar <domen.merhar@students.vegova.si>
2024-09-19 17:35:37 +0200

    Update replyController.ts

--- server/src/controllers/replyController.ts
+++ server/src/controllers/replyController.ts
@@ -1,5 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 import catchAsync from "../utils/catchAsync";
+import Review from "../models/reviewModel";
+import AppError from "../utils/AppError";
+import Reply from "../models/replyModel";
+
+//TODO: Data sanitization
 
 export const getReply = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -25,10 +30,23 @@ export const getPostReplies = catchAsync(
 
 export const createReply = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
+    const { reviewId } = req.params;
+    const userId = res.locals.user.id;
+    const { reply: replyText } = req.body;
+
+    const validReview = await Review.findById(reviewId);
+    if (!validReview) return next(new AppError("Review not found", 404));
+
+    const reply = await Reply.create({
+      user: userId,
+      review: reviewId,
+      reply: replyText,
+    });
+
     res.status(200).json({
       status: "success",
       data: {
-        message: "Create reply",
+        reply,
       },
     });
   }
