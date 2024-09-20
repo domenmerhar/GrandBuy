c552db91476e24211326b2816d0d3078cfdaf1b1
Domen Merhar <domen.merhar@students.vegova.si>
2024-09-20 18:51:39 +0200

    Update requestModel.ts

--- server/src/controllers/requestController.ts
+++ server/src/controllers/requestController.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import catchAsync from "../utils/catchAsync";
+import requestModel from "../models/requestModel";
+import AppError from "../utils/AppError";
 
 export const getRequest = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -12,9 +14,25 @@ export const getRequest = catchAsync(
 
 export const createRequest = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    res.status(500).json({
-      status: "error",
-      message: "This route is not yet defined!",
+    const userId = res.locals.user._id;
+
+    const existingRequest = await requestModel.findOne({
+      user: userId,
+      status: "pending",
+    });
+
+    if (existingRequest)
+      return next(new AppError("Request already exists", 400));
+
+    const newRequest = await requestModel.create({
+      user: userId,
+    });
+
+    res.status(201).json({
+      status: "succcess",
+      data: {
+        request: newRequest,
+      },
     });
   }
 );
