383efb14e686b6e562daa70ddfe217e174c2f101
Domen Merhar <domen.merhar@students.vegova.si>
2024-09-20 08:14:13 +0200

    Update notificationRouter.ts

--- server/src/controllers/notificationController.ts
+++ server/src/controllers/notificationController.ts
@@ -1,11 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 import catchAsync from "../utils/catchAsync";
+import User from "../models/userModel";
+import AppError from "../utils/AppError";
+import Notification from "../models/notificationModel";
 
-export const getYourNotifications = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {}
+export const getAdminNotifications = catchAsync(
+  (req: Request, res: Response, next: NextFunction) => {}
 );
 
-export const getNotificationCount = catchAsync(
+export const getYourNotifications = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {}
 );
 
@@ -13,6 +16,31 @@ export const getNotification = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {}
 );
 
-export const createNotification = catchAsync(
+export const getUnreadNotificationCount = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {}
+);
+
+export const createNotification = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { userId } = req.params;
+    const { type, message } = req.body;
+    const createdBy = res.locals.user._id;
+
+    const user = await User.findById(userId);
+    if (!user) return next(new AppError("User not found", 404));
+
+    const notifcation = await Notification.create({
+      user: userId,
+      createdBy,
+      type,
+      message,
+    });
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        notifcation,
+      },
+    });
+  }
 );
