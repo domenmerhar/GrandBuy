90808e81cf674367bc7905a14e873f0905d82b45
Domen Merhar <domen.merhar@students.vegova.si>
2024-09-21 10:12:39 +0200

    Add banModel.ts

---
+++ server/src/models/banModel.ts
@@ -0,0 +1,54 @@
+import mongoose, { Schema } from "mongoose";
+
+//TODO: Add Indexes
+//TODO: response structure
+
+const banSchema = new Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+
+  validUntil: {
+    type: Date,
+    validate: {
+      validator: function (val: Date) {
+        return val > new Date(Date.now());
+      },
+      message: "The validUntil date must be in the future.",
+    },
+  },
+
+  message: {
+    type: String,
+    required: true,
+    minLength: [1, "Please provide a message with at least 1 character."],
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+banSchema.set("toJSON", {
+  versionKey: false,
+
+  virtuals: true,
+  transform: function (doc, ret) {
+    delete ret.id;
+    return ret;
+  },
+});
+
+banSchema.set("toObject", {
+  versionKey: false,
+  virtuals: true,
+  transform: function (doc, ret) {
+    delete ret.id;
+    return ret;
+  },
+});
+
+export default mongoose.model("Ban", banSchema);
