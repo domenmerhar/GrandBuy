ea9240aaa6870a063ae7e04506edf5e79be804e3
Domen Merhar <domen.merhar@students.vegova.si>
2024-10-03 19:53:46 +0200

    Update refundRouter.ts

---
+++ server/src/router/refundRouter.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import { protect, restrictTo } from "../controllers/authController";
+import { requestRefund } from "../controllers/refundController";
+
+const refundRouter = express.Router();
+
+refundRouter.use(protect);
+
+refundRouter
+  .route("/product/:id")
+  .post(restrictTo("user", "admin"), requestRefund);
+
+// refundRouter.route("/my").get(restrictTo("user", "admin"), getMyRefunds);
+// refundRouter
+//   .route("/:id")
+//   .get(restrictTo("user", "admin"), getRefund)
+//   .delete(restrictTo("user", "admin"), cancelRefund);
+
+// refundRouter.route("/:id/respond").post(restrictTo("seller"), respondRefund);
+// // refundRouter.route("/:id/accept").post(restrictTo("seller"), acceptRefund);
+// // refundRouter.route("/:id/cancel").post(restrictTo("seller"), acceptRefund);
+
+// refundRouter.route("/seller").get(restrictTo("seller"), getSellerRefunds);
+
+// refundRouter.route("/").get(restrictTo("admin"), getAllRefunds);
+
+export default refundRouter;
