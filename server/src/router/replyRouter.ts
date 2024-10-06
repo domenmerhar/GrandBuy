b57e6b7d55f252674799cd699f27d963b8926011
Domen Merhar <domen.merhar@students.vegova.si>
2024-10-06 20:43:47 +0200

    Update replyRouter.ts

--- server/src/router/replyRouter.ts
+++ server/src/router/replyRouter.ts
@@ -7,16 +7,30 @@ import {
   getReviewReplies,
   getUserReplies,
 } from "../controllers/replyController";
+import { validate } from "../utils/validate";
+import { body, param } from "express-validator";
 
 const replyRouter = express.Router();
 
-replyRouter.route("/:id").get(getReply).delete(protect, deleteReply);
+replyRouter
+  .route("/:id")
+  .get(validate([param("id").isMongoId().notEmpty()]), getReply)
+  .delete(protect, deleteReply);
 
 replyRouter
   .route("/review/:reviewId")
-  .get(getReviewReplies)
-  .post(protect, createReply);
+  .get(validate([param("reviewId").isMongoId().notEmpty()]), getReviewReplies)
+  .post(
+    validate([
+      param("reviewId").isMongoId().notEmpty(),
+      body("reply").isString().notEmpty(),
+    ]),
+    protect,
+    createReply
+  );
 
-replyRouter.route("/user/:userId").get(getUserReplies);
+replyRouter
+  .route("/user/:userId")
+  .get(validate([param("userId").isMongoId().notEmpty()]), getUserReplies);
 
 export default replyRouter;
