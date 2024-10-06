68cec4159ef96d4abc8a3d5d147aadc8a45d9922
Domen Merhar <domen.merhar@students.vegova.si>
2024-10-06 20:44:10 +0200

    Update userRouter.ts

--- server/src/router/userRouter.ts
+++ server/src/router/userRouter.ts
@@ -19,6 +19,8 @@ import { getAll } from "../controllers/handlerFactory";
 import User from "../models/userModel";
 import fileUpload from "express-fileupload";
 import { fileExtLimiterOne } from "../controllers/fileController";
+import { validate } from "../utils/validate";
+import { body, param } from "express-validator";
 //TODO: NOT EMPTY CHECK
 //TODO: CHECK EVERY ROUTER FOR AUTH MIDDLEWARE
 //TODO: NORMALIZE RESPONE FORMAT
@@ -27,27 +29,79 @@ import { fileExtLimiterOne } from "../controllers/fileController";
 
 const userRouter = Router();
 
-userRouter.route("/login").post(login);
+userRouter
+  .route("/login")
+  .post(
+    validate([
+      body("email").isEmail(),
+      body("password").isString().isLength({ min: 6 }),
+    ]),
+    login
+  );
 
-userRouter.route("/signup").post(signup);
+userRouter
+  .route("/signup")
+  .post(
+    validate([
+      body("username").isString(),
+      body("email").isEmail(),
+      body("password").isString().isLength({ min: 6 }),
+      body("confirmPassword").isString().isLength({ min: 6 }),
+    ]),
+    signup
+  );
 
-userRouter.route("/forgot-password").patch(forgotPassword);
-userRouter.route("/confirm-forgot-password").patch(confirmForgotPassword);
+userRouter
+  .route("/forgot-password")
+  .patch(validate([body("email").isEmail()]), forgotPassword);
+userRouter
+  .route("/confirm-forgot-password")
+  .patch(
+    validate([
+      body("email").isEmail(),
+      body("verificationCode").isString(),
+      body("password").isString().isLength({ min: 6 }),
+      body("confirmPassword").isString().isLength({ min: 6 }),
+    ]),
+    confirmForgotPassword
+  );
 
-userRouter.route("/confirm-email/:verificationCode").patch(confirmEmail);
+userRouter
+  .route("/confirm-email/:verificationCode")
+  .patch(
+    validate([param("verificationCode").isString(), body("email").isEmail()]),
+    confirmEmail
+  );
 
 userRouter.use(protect);
 userRouter
   .route("/me")
   .get(getMe)
   .patch(
+    validate([
+      body("firstName").isString().optional(),
+      body("lastName").isString().optional(),
+      body("street").isString().optional(),
+      body("city").isString().optional(),
+      body("zipCode").isString().optional(),
+      body("country").isString().optional(),
+      body("phoneNumber").isMobilePhone("any").optional(),
+    ]),
     fileUpload({ createParentPath: true }),
     fileExtLimiterOne("image", [".png", ".jpg", ".jpeg"], true),
     updateMe
   );
 
 userRouter.route("/logout").post(logout);
-userRouter.route("/change-password").patch(changePassword);
+userRouter
+  .route("/change-password")
+  .patch(
+    validate([
+      body("password").isString().isLength({ min: 6 }),
+      body("confirmPassword").isString().isLength({ min: 6 }),
+    ]),
+    changePassword
+  );
 
 userRouter.use(restrictTo("admin"), restrictPrivileges("admin"));
 userRouter.route("/").get(getAll(User));
@@ -55,11 +109,24 @@ userRouter.route("/").get(getAll(User));
 //TODO: ADD PATCH ROUTE FOR UPDATING PRIVELLEGES
 userRouter
   .route("/:userId")
-  .delete(deleteUser)
+  .delete(validate([param("userId").isMongoId()]), deleteUser)
   .patch(
+    validate([
+      param("userId").isMongoId(),
+      body("privileges").isArray({ max: 5 }),
+    ]),
     restrictPrivileges("ban", "coupon", "notification", "request"),
     updatePrivileges
   );
-userRouter.route("/:userId/role/:role").patch(updateRole);
+
+userRouter
+  .route("/:userId/role/:role")
+  .patch(
+    validate([
+      param("userId").isMongoId(),
+      param("role").isIn(["user", "seller", "admin"]),
+    ]),
+    updateRole
+  );
 
 export default userRouter;
