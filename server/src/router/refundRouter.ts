a3753f59f01924009b63e74b9eb42a0bb0455eaa
Domen Merhar <domen.merhar@students.vegova.si>
2024-10-07 09:36:40 +0200

    Update refundRouter.ts

--- server/src/router/refundRouter.ts
+++ server/src/router/refundRouter.ts
@@ -19,58 +19,74 @@ refundRouter.use(protect);
 
 refundRouter.route("/seller").get(restrictTo("seller"), getSellerRefunds);
 
-refundRouter
-  .route("/product/:id")
-  .post(
-    validate([
-      param("id").isMongoId().withMessage("Please provide a valid ID."),
-      body("reason")
-        .isString()
-        .withMessage("Please provide a valid reason.")
-        .notEmpty()
-        .withMessage("Please provide a reason."),
-    ]),
-    restrictTo("user", "admin"),
-    requestRefund
-  );
+refundRouter.route("/product/:id").post(
+  validate([
+    param("id").isMongoId().withMessage("Please provide a valid ID."),
+    body("reason")
+      .trim()
+      .isString()
+      .withMessage("Please provide a valid reason.")
+      .notEmpty()
+      .withMessage("Please provide a reason."),
+  ]),
+
+  restrictTo("user", "admin"),
+  requestRefund
+);
 
 refundRouter.route("/my").get(restrictTo("user", "admin"), getMyRefunds);
 refundRouter
   .route("/:id")
   .get(
     validate([
-      param("id").isMongoId().withMessage("Please provide a valid ID."),
+      param("id")
+        .isMongoId()
+        .withMessage("Please provide a valid ID.")
+        .notEmpty()
+        .withMessage("Please provide an ID."),
     ]),
+
     restrictTo("user", "admin"),
     getRefund
   )
   .delete(
     validate([
-      param("id").isMongoId().withMessage("Please provide a valid ID."),
+      param("id")
+        .isMongoId()
+        .withMessage("Please provide a valid ID.")
+        .notEmpty()
+        .withMessage("Please provide an ID."),
     ]),
+
     restrictTo("user", "admin"),
     cancelRefund
   );
 
-refundRouter
-  .route("/:id/respond")
-  .patch(
-    validate([
-      param("id").isMongoId().withMessage("Please provide a valid ID."),
-      body("status")
-        .isIn(["accepted", "rejected"])
-        .withMessage("Status must be accepted or rejected.")
-        .notEmpty()
-        .withMessage("Please provide a status."),
-      body("resolvedMessage")
-        .isString()
-        .withMessage("Please provide a resolvedMessage.")
-        .notEmpty()
-        .withMessage("Please provide a reslovedMessage."),
-    ]),
-    restrictTo("seller"),
-    respondToRefund
-  );
+refundRouter.route("/:id/respond").patch(
+  validate([
+    param("id")
+      .isMongoId()
+      .withMessage("Please provide a valid ID.")
+      .notEmpty()
+      .withMessage("Please provide an ID."),
+
+    body("status")
+      .isIn(["accepted", "rejected"])
+      .withMessage("Status must be accepted or rejected.")
+      .notEmpty()
+      .withMessage("Please provide a status."),
+
+    body("resolvedMessage")
+      .trim()
+      .isString()
+      .withMessage("Please provide a resolvedMessage.")
+      .notEmpty()
+      .withMessage("Please provide a reslovedMessage."),
+  ]),
+
+  restrictTo("seller"),
+  respondToRefund
+);
 
 refundRouter
   .route("/")
@@ -82,18 +98,21 @@ refundRouter
       { path: "cartItemId" },
     ])
   );
-refundRouter
-  .route("/admin/:id")
-  .get(
-    validate([
-      param("id").isMongoId().withMessage("Please provide a valid ID."),
-    ]),
-    restrictTo("admin"),
-    getOne(Refund, [
-      { path: "user", select: "name email" },
-      { path: "seller", select: "name email" },
-      { path: "cartItemId" },
-    ])
-  );
+refundRouter.route("/admin/:id").get(
+  validate([
+    param("id")
+      .isMongoId()
+      .withMessage("Please provide a valid ID.")
+      .notEmpty()
+      .withMessage("Please provide an ID."),
+  ]),
+
+  restrictTo("admin"),
+  getOne(Refund, [
+    { path: "user", select: "name email" },
+    { path: "seller", select: "name email" },
+    { path: "cartItemId" },
+  ])
+);
 
 export default refundRouter;
