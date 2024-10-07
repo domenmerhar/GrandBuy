fef4dc4de76935a007cc6643c1407a852b11fcbe
Domen Merhar <domen.merhar@students.vegova.si>
2024-10-07 09:15:17 +0200

    Update userRouter.ts

--- server/src/router/userRouter.ts
+++ server/src/router/userRouter.ts
@@ -29,49 +29,131 @@ import { body, param } from "express-validator";
 
 const userRouter = Router();
 
-userRouter
-  .route("/login")
-  .post(
-    validate([
-      body("email").isEmail(),
-      body("password").isString().isLength({ min: 6 }),
-    ]),
-    login
-  );
+userRouter.route("/login").post(
+  validate([
+    body("email")
+      .isEmail()
+      .withMessage("Please provide a valid email.")
+      .notEmpty()
+      .withMessage("Please provide an email."),
+    body("password")
+      .isString()
+      .withMessage("Please provide a valid password.")
+      .isLength({ min: 6 })
+      .withMessage(
+        "Please provide a password that is longer than 6 characters."
+      )
+      .notEmpty()
+      .withMessage("Please provide a password."),
+  ]),
 
-userRouter
-  .route("/signup")
-  .post(
-    validate([
-      body("username").isString(),
-      body("email").isEmail(),
-      body("password").isString().isLength({ min: 6 }),
-      body("confirmPassword").isString().isLength({ min: 6 }),
-    ]),
-    signup
-  );
+  login
+);
 
-userRouter
-  .route("/forgot-password")
-  .patch(validate([body("email").isEmail()]), forgotPassword);
-userRouter
-  .route("/confirm-forgot-password")
-  .patch(
-    validate([
-      body("email").isEmail(),
-      body("verificationCode").isString(),
-      body("password").isString().isLength({ min: 6 }),
-      body("confirmPassword").isString().isLength({ min: 6 }),
-    ]),
-    confirmForgotPassword
-  );
+userRouter.route("/signup").post(
+  validate([
+    body("username")
+      .isString()
+      .withMessage("Please provide a valid username.")
+      .notEmpty()
+      .withMessage("Please provide a username."),
 
-userRouter
-  .route("/confirm-email/:verificationCode")
-  .patch(
-    validate([param("verificationCode").isString(), body("email").isEmail()]),
-    confirmEmail
-  );
+    body("email")
+      .isEmail()
+      .withMessage("Please provide a valid email.")
+      .notEmpty()
+      .withMessage("Please provide an email."),
+
+    body("password")
+      .isString()
+      .withMessage("Please provide a valid password")
+      .isLength({ min: 6 })
+      .withMessage(
+        "Please provide a password that is longer than 6 characters."
+      )
+      .notEmpty()
+      .withMessage("Please provide a password."),
+
+    body("confirmPassword")
+      .isString()
+      .withMessage("Please provide a valid confirmPassword")
+      .isLength({ min: 6 })
+      .withMessage(
+        "Please provide a confirmPassword that is longer than 6 characters."
+      )
+      .notEmpty()
+      .withMessage("Please provide a confirmPassword."),
+  ]),
+
+  signup
+);
+
+userRouter.route("/forgot-password").patch(
+  validate([
+    body("email")
+      .isEmail()
+      .withMessage("Please provide a valid email.")
+      .notEmpty()
+      .withMessage("Please provide an email."),
+  ]),
+
+  forgotPassword
+);
+userRouter.route("/confirm-forgot-password").patch(
+  validate([
+    body("email")
+      .isEmail()
+      .withMessage("Please provide a valid email.")
+      .notEmpty()
+      .withMessage("Please provide an email."),
+    body("verificationCode")
+      .isString()
+      .withMessage("Please provide a valid verificationCode.")
+      .notEmpty()
+      .withMessage("Please provide a verificationCode."),
+
+    body("password")
+      .isString()
+      .withMessage("Please provide a valid password.")
+      .isLength({ min: 6 })
+      .withMessage(
+        "Please provide a password that is longer than 6 characters."
+      )
+      .notEmpty()
+      .withMessage("Please provide a password."),
+
+    body("confirmPassword")
+      .isString()
+      .withMessage("Please provide a valid confirmPassword.")
+      .isLength({ min: 6 })
+      .withMessage(
+        "Please provide a confirmPassword that is longer than 6 characters."
+      )
+      .notEmpty()
+      .withMessage("Please provide a confirmPassword."),
+  ]),
+
+  confirmForgotPassword
+);
+
+userRouter.route("/confirm-email/:verificationCode").patch(
+  validate([
+    param("verificationCode")
+      .isString()
+      .withMessage("Please provide a valid verificationCode.")
+      .notEmpty()
+      .withMessage("Please provide a verificationCode."),
+
+    body("email")
+      .isEmail()
+      .withMessage("Please provide a valid email.")
+      .notEmpty()
+      .withMessage("Please provide an email.")
+      .normalizeEmail(),
+  ]),
+
+  confirmEmail
+);
 
 userRouter.use(protect);
 userRouter
@@ -85,23 +167,43 @@ userRouter
       body("city").isString().optional(),
       body("zipCode").isString().optional(),
       body("country").isString().optional(),
-      body("phoneNumber").isMobilePhone("any").optional(),
+      body("phoneNumber")
+        .isMobilePhone("any")
+        .withMessage("Please provide a valid phoneNumber")
+        .optional(),
     ]),
+
     fileUpload({ createParentPath: true }),
     fileExtLimiterOne("image", [".png", ".jpg", ".jpeg"], true),
     updateMe
   );
 
 userRouter.route("/logout").post(logout);
-userRouter
-  .route("/change-password")
-  .patch(
-    validate([
-      body("password").isString().isLength({ min: 6 }),
-      body("confirmPassword").isString().isLength({ min: 6 }),
-    ]),
-    changePassword
-  );
+userRouter.route("/change-password").patch(
+  validate([
+    body("password")
+      .isString()
+      .withMessage("Please provide a valid password.")
+      .isLength({ min: 6 })
+      .withMessage(
+        "Please provide a password that is longer than 6 characters."
+      )
+      .notEmpty()
+      .withMessage("Please provide a password."),
+
+    body("confirmPassword")
+      .isString()
+      .withMessage("Please provide a valid confirmPassword.")
+      .isLength({ min: 6 })
+      .withMessage(
+        "Please provide a confirmPassword that is longer than 6 characters."
+      )
+      .notEmpty()
+      .withMessage("Please provide a confirmPassword."),
+  ]),
+
+  changePassword
+);
 
 userRouter.use(restrictTo("admin"), restrictPrivileges("admin"));
 userRouter.route("/").get(getAll(User));
@@ -109,24 +211,52 @@ userRouter.route("/").get(getAll(User));
 //TODO: ADD PATCH ROUTE FOR UPDATING PRIVELLEGES
 userRouter
   .route("/:userId")
-  .delete(validate([param("userId").isMongoId()]), deleteUser)
+  .delete(
+    validate([
+      param("userId")
+        .isMongoId()
+        .withMessage("Please provide a valid user ID.")
+        .notEmpty()
+        .withMessage("Please provide a user ID."),
+    ]),
+
+    deleteUser
+  )
   .patch(
     validate([
-      param("userId").isMongoId(),
-      body("privileges").isArray({ max: 5 }),
+      param("userId")
+        .isMongoId()
+        .withMessage("Please provide a user ID.")
+        .notEmpty()
+        .withMessage("Please provide a user ID."),
+
+      body("privileges")
+        .isArray({ max: 5 })
+        .withMessage("Please provide an array of privileges.")
+        .notEmpty()
+        .withMessage("Please provide at least one privilege."),
     ]),
+
     restrictPrivileges("ban", "coupon", "notification", "request"),
     updatePrivileges
   );
 
-userRouter
-  .route("/:userId/role/:role")
-  .patch(
-    validate([
-      param("userId").isMongoId(),
-      param("role").isIn(["user", "seller", "admin"]),
-    ]),
-    updateRole
-  );
+userRouter.route("/:userId/role/:role").patch(
+  validate([
+    param("userId")
+      .isMongoId()
+      .withMessage("Please provide a user ID.")
+      .notEmpty()
+      .withMessage("Please provide a user ID."),
+
+    param("role")
+      .isIn(["user", "seller", "admin"])
+      .withMessage("Role must be user, seller or admin.")
+      .notEmpty()
+      .withMessage("Please provide a role."),
+  ]),
+
+  updateRole
+);
 
 export default userRouter;
