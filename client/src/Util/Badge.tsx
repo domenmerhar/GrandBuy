e0da37155e3ec53f47b23faa54b84d3d129e33b0
Domen Merhar <domen.merhar@students.vegova.si>
2024-10-10 17:36:26 +0200

    Add Badge.tsx

---
+++ client/src/Util/Badge.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+interface BadgeProps {
+  $color: "orange" | "red" | "green";
+}
+
+export const Badge = styled.span<BadgeProps>`
+  ${({ $color }) =>
+    $color === "orange" &&
+    "background-image: linear-gradient(120deg, var(--orange-6), var(--orange-5));"}
+
+  ${({ $color }) => $color === "red" && "background-color: var(--red);"}
+
+  ${({ $color }) => $color === "green" && "background-color: var(--green-6);"}
+
+  color: var(--gray-0);
+  text-transform: uppercase;
+  font-weight: ${({ $color }) => ($color === "red" ? 600 : 500)};
+  letter-spacing: 0.2px;
+
+  padding: 0.6rem 0.9rem;
+
+  border-radius: 2rem;
+`;
