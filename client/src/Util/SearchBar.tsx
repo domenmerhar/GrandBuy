20cf282b198fbf349148023de35e31d5dcbff633
Domen Merhar <domen.merhar@students.vegova.si>
2024-10-11 19:07:36 +0200

    Update NavigationBar.tsx

--- client/src/Util/SearchBar.tsx
+++ client/src/Util/SearchBar.tsx
@@ -5,16 +5,21 @@ import styled from "styled-components";
 const StyledSearchBar = styled.div`
   display: flex;
   align-items: center;
+
   border-radius: 1.5rem;
   overflow: hidden;
-  background-color: var(--gray-0);
+  background-color: var(--gray-1);
 `;
 
 const Input = styled.input`
   border: none;
   padding: 0.5rem 1rem;
+  flex: 10rem 1 1;
+
   box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
   background-color: inherit;
+
+  transition: all 200ms;
 
   &::placeholder {
     color: var(--gray-6);
@@ -22,20 +27,22 @@ const Input = styled.input`
 
   &:focus {
     outline: none;
+    background-color: var(--gray-0);
   }
 `;
 
 const IconHolder = styled.button`
   display: flex;
+  flex: 5rem 0 0;
+
   justify-content: center;
   align-items: center;
   background-color: inherit;
   padding: 0.5rem 1.5rem;
-  border-radius: 1.4rem;
   box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
   border: none;
 
-  transition: transform 0.2s;
+  transition: transform 200ms;
 
   &:hover {
     transform: scale(1.05);
