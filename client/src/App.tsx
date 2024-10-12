a04312d575e1f99aa8b73fc4d26e36fa63e27f39
Domen Merhar <domen.merhar@students.vegova.si>
2024-10-12 11:54:10 +0200

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
 import GlobalStyles from "./GlobalStyles";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <GlobalStyles />
-      <h1>h1</h1>
-    </>
+    </BrowserRouter>
   );
 }
 
