ffe1ac20ad10c6cd4bd8fe9906813421c202e979
Domen Merhar <domen.merhar@students.vegova.si>
2024-10-12 11:32:58 +0200

    Add BurgerMenuNavigationList.tsx

---
+++ client/src/Components/BurgerMenuNavigationList.tsx
@@ -0,0 +1,52 @@
+import {
+  HiOutlineClipboardList,
+  HiOutlineClock,
+  HiOutlinePresentationChartBar,
+  HiOutlineUser,
+} from "react-icons/hi";
+import { NavigationTextButton } from "../Util/NavigationTextButton";
+import { HiOutlineCog8Tooth } from "react-icons/hi2";
+import { BiPackage } from "react-icons/bi";
+import styled from "styled-components";
+
+const NavigationList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+`;
+
+export const BurgerMenuNavigationList = () => {
+  return (
+    <NavigationList>
+      <NavigationTextButton to="account">
+        <HiOutlineUser size={24} />
+        Account
+      </NavigationTextButton>
+
+      <NavigationTextButton to="wishlist">
+        <HiOutlineClipboardList size={24} />
+        Wishlist
+      </NavigationTextButton>
+
+      <NavigationTextButton to="orders" iconColoring="fill">
+        <BiPackage size={24} />
+        Orders
+      </NavigationTextButton>
+
+      <NavigationTextButton to="history">
+        <HiOutlineClock size={24} />
+        History
+      </NavigationTextButton>
+
+      <NavigationTextButton to="settings">
+        <HiOutlineCog8Tooth size={24} />
+        Settings
+      </NavigationTextButton>
+
+      <NavigationTextButton to="dashboard">
+        <HiOutlinePresentationChartBar size={24} />
+        Dashboard
+      </NavigationTextButton>
+    </NavigationList>
+  );
+};
