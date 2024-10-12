f018a3b2e01753f44fa4735d17b62936707119a4
Domen Merhar <domen.merhar@students.vegova.si>
2024-10-12 10:38:19 +0200

    Update Backdrop.tsx

--- client/src/Util/Backdrop.tsx
+++ client/src/Util/Backdrop.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Backdrop = styled.div`
   background-color: rgba(0, 0, 0, 0.5);
   position: fixed;
-  height: 100vw;
-  width: 100vh;
+  height: 100vh;
+  width: 100vw;
   z-index: 0;
 `;
