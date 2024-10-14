5d95f141fce02624d5b7a1efbac606ac8ba4198e
Domen Merhar <domen.merhar@students.vegova.si>
2024-10-14 10:29:19 +0200

    Update Countdown.tsx

--- client/src/Components/Countdown.tsx
+++ client/src/Components/Countdown.tsx
@@ -1,15 +1,32 @@
 import styled from "styled-components";
 import { timeUntilMidnight } from "../functions/timeUntilMidnight";
 import { Clock } from "./Clock";
+import { useEffect, useState } from "react";
 
 const StyledCountdown = styled.div`
   display: flex;
   flex-direction: column;
 `;
 
+interface iTime {
+  hoursUntilMidnight: number;
+  minutesUntilMidnight: number;
+  secondsUntilMidnight: number;
+}
+
 export const Countdown = () => {
-  const { hoursUntilMidnight, minutesUntilMidnight, secondsUntilMidnight } =
-    timeUntilMidnight();
+  const [
+    { hoursUntilMidnight, minutesUntilMidnight, secondsUntilMidnight },
+    setTime,
+  ] = useState<iTime>(timeUntilMidnight());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(timeUntilMidnight());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [hoursUntilMidnight, minutesUntilMidnight, secondsUntilMidnight]);
 
   return (
     <StyledCountdown>
