b21c2808645387e8f32d61fadcdd630b00f08ab5
Domen Merhar <domen.merhar@students.vegova.si>
2024-10-28 13:28:01 +0100

    Add SkewedContainer.tsx

---
+++ client/src/Util/SkewedContainer.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from "react";
+import styled from "styled-components";
+
+interface SkewedContainerProps {
+  children: React.ReactNode | React.ReactNode[];
+}
+
+const StyledSkewedContainer = styled.div`
+  background-color: var(--gray-2);
+  transform: skewY(var(--skew)) translateZ(0);
+  margin-top: -10rem;
+
+  height: 5000px;
+`;
+
+const Content = styled.div`
+  max-width: 1440px;
+  padding: 0 3.2rem;
+  margin: 0 auto;
+
+  transform: skewY(var(--skew-negative)) translateZ(0);
+`;
+
+export const SkewedContainer: FC<SkewedContainerProps> = ({ children }) => {
+  return (
+    <StyledSkewedContainer>
+      <Content>{children}</Content>
+    </StyledSkewedContainer>
+  );
+};
