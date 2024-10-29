f5f8e7ecb17a4d69db72d2a1c38a551e12265c69
Domen Merhar <domen.merhar@students.vegova.si>
2024-10-29 07:29:22 +0100

    Update SkewedContainer.tsx

--- client/src/Util/SkewedContainer.tsx
+++ client/src/Util/SkewedContainer.tsx
@@ -9,13 +9,11 @@ const StyledSkewedContainer = styled.div`
   background-color: var(--gray-2);
   transform: skewY(var(--skew)) translateZ(0);
   margin-top: -10rem;
-
-  height: 5000px;
 `;
 
 const Content = styled.div`
   max-width: 1440px;
-  padding: 0 3.2rem;
+  padding: 6.4rem 3.2rem;
   margin: 0 auto;
   padding-top: 4.8rem;
 
