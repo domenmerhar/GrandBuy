9168a9b8bd564c0a946c6d725d6abd0db7660321
Domen Merhar <domen.merhar@students.vegova.si>
2024-10-29 07:28:27 +0100

    Add Grid.tsx

---
+++ client/src/Util/Grid.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+interface GridProps {
+  $minWidth: string;
+  $maxWidth: string;
+  $colGap: string;
+  $rowGap: string;
+}
+
+export const Grid = styled.div<GridProps>`
+  display: grid;
+  grid-template-columns: repeat(
+    auto-fit,
+    minmax(${({ $minWidth }) => $minWidth}, ${({ $maxWidth }) => $maxWidth})
+  );
+  column-gap: ${({ $colGap }) => $colGap};
+  row-gap: ${({ $rowGap }) => $rowGap};
+  justify-content: space-around;
+`;
