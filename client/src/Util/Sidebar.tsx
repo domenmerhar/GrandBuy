f774ba95565af34a4284ff85c652fb310cdfe012
Domen Merhar <domen.merhar@students.vegova.si>
2024-10-30 11:28:22 +0100

    Add Sidebar.tsx

---
+++ client/src/Util/Sidebar.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import { FC, ReactNode } from "react";
+
+interface SidebarProps {
+  children: ReactNode | ReactNode[];
+}
+
+const StyledSidebar = styled.aside`
+  position: fixed;
+  display: inline-block;
+  min-width: 20rem;
+  width: 32rem;
+  height: 100vh;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  padding: 2.4rem 3.2rem;
+`;
+
+export const Sidebar: FC<SidebarProps> = ({ children }) => {
+  return <StyledSidebar>{children}</StyledSidebar>;
+};
