1694d7c9307e902b55d8410423d32295c763de67
Domen Merhar <domen.merhar@students.vegova.si>
2024-10-30 11:14:36 +0100

    Add HeaderUppercaseBold.tsx

---
+++ client/src/Util/HeaderUppercaseBold.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const HeaderUppercaseBold = styled.h2`
+  font-size: 2rem;
+  color: var(--gray-7);
+  font-weight: 700;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+`;
