873ad9f78d4e5d681067aa8696a0185feafcf35e
Domen Merhar <domen.merhar@students.vegova.si>
2024-10-31 20:41:20 +0100

    Update SliderFilter.tsx

--- client/src/pages/search/SliderFilter.tsx
+++ client/src/pages/search/SliderFilter.tsx
@@ -1,5 +1,13 @@
 import { Slider } from "@mui/material";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { NakedInput } from "../../Util/NakedInput";
+import styled from "styled-components";
+
+const RangeHolder = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.6rem;
+`;
 
 function valuetext(value: number) {
   return `${value}€`;
@@ -7,19 +15,73 @@ function valuetext(value: number) {
 
 export const SliderFilter = () => {
   const [value, setValue] = useState<number[]>([0, 0]);
-  const handleChange = (event: Event, newValue: number | number[]) => {
+
+  const minRef = useRef<HTMLInputElement>(null);
+  const maxRef = useRef<HTMLInputElement>(null);
+
+  const handleChangeSlider = (e: Event, newValue: number | number[]) => {
     setValue(newValue as number[]);
+
+    minRef.current!.value = `$${Math.min(...(newValue as number[]))}`;
+    maxRef.current!.value = `$${Math.max(...(newValue as number[]))}`;
+  };
+
+  // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   setValue((prev) => {
+  //     const newValue = prev;
+  //     if (e.target === minRef.current) {
+  //       newValue[0] = parseInt(inputValue);
+  //     } else {
+  //       newValue[1] = parseInt(inputValue);
+  //     }
+
+  //     minRef.current!.value = `$${Math.min(...(newValue as number[]))}`;
+  //     maxRef.current!.value = `$${Math.max(...(newValue as number[]))}`;
+  //     return newValue;
+  //   });
+
+  //   setInputValue("");
+  // };
+
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    setValue((prev) => {
+      const newValue = prev;
+      newValue[e.target === minRef.current ? 0 : 1] = parseInt(e.target.value);
+      return newValue;
+    });
+
+    minRef.current!.value = `$${Math.min(...(value as number[]))}`;
+    maxRef.current!.value = `$${Math.max(...(value as number[]))}`;
   };
 
   return (
-    <Slider
-      getAriaLabel={() => "Temperature range"}
-      value={value}
-      onChange={handleChange}
-      //valueLabelDisplay="auto"
-      getAriaValueText={valuetext}
-      color="warning"
-      max={1000}
-    />
+    <>
+      <Slider
+        value={value}
+        onChange={handleChangeSlider}
+        getAriaValueText={valuetext}
+        color="warning"
+        //max={Math.max(...value)}
+      />
+      <RangeHolder>
+        <NakedInput
+          $height="3.2rem"
+          $width="6.4rem"
+          $fontSize="1.6rem"
+          onBlur={handleBlur}
+          ref={minRef}
+          defaultValue={`$${value[0]}`}
+        />
+        -
+        <NakedInput
+          $height="3.2rem"
+          $width="6.4rem"
+          $fontSize="1.6rem"
+          onBlur={handleBlur}
+          ref={maxRef}
+          defaultValue={`$${value[1]}`}
+        />
+      </RangeHolder>
+    </>
   );
 };
