8b37612543df5481c63939af151ade2bb87f69f1
Domen Merhar <domen.merhar@students.vegova.si>
2024-11-01 09:48:47 +0100

    Update Logo.tsx

--- client/src/Util/Logo.tsx
+++ client/src/Util/Logo.tsx
@@ -1,7 +1,20 @@
+import { FC } from "react";
 import styled from "styled-components";
 
-const LogoHolder = styled.div`
-  color: var(--gray-0);
+interface LogoProps {
+  $color?: "white" | "orange";
+}
+
+const LogoHolder = styled.div<LogoProps>`
+  ${({ $color }) => $color === "white" && "color: var(--gray-0);"}
+
+  ${({ $color }) =>
+    $color === "orange" &&
+    `background: -webkit-linear-gradient(120deg, var(--orange-4), var(--orange-6));
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;`}
+
   font-family: "Damion", cursive;
 `;
 
@@ -13,9 +26,9 @@ const SpanLarge = styled.span`
   font-size: 4.8rem;
 `;
 
-export const Logo = () => {
+export const Logo: FC<LogoProps> = ({ $color }) => {
   return (
-    <LogoHolder>
+    <LogoHolder $color={$color}>
       <SpanSmall>Grand</SpanSmall>
       <SpanLarge>Buy</SpanLarge>
     </LogoHolder>
