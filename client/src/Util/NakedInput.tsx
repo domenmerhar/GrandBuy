9598046623fadd9d2bc9db5b52026de4fbc3f443
Domen Merhar <domen.merhar@students.vegova.si>
2024-11-01 07:31:13 +0100

    Add NakedInput.tsx

---
+++ client/src/Util/NakedInput.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+interface NakedInputProps {
+  $width?: string;
+  $height?: string;
+  $fontSize?: string;
+  $fontWeight?: string;
+}
+
+export const NakedInput = styled.input<NakedInputProps>`
+  -webkit-appearance: none;
+  -moz-appearance: textfield;
+  appearance: textfield;
+
+  border: none;
+  border-radius: 8px;
+
+  width: ${({ $width }) => $width || "4rem"};
+  height: ${({ $height }) => $height || "4rem"};
+
+  text-align: center;
+
+  font-size: ${({ $fontSize }) => $fontSize || "2rem"};
+  font-weight: ${({ $fontWeight }) => $fontWeight || "500"};
+`;
