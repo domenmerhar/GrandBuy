14cedbd63be0cdd0d52e33ceaff0c9b0db3bf4ad
Domen Merhar <domen.merhar@students.vegova.si>
2024-11-01 20:48:43 +0100

    Update LoginPage.tsx

--- client/src/pages/login/LoginPage.tsx
+++ client/src/pages/login/LoginPage.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Logo } from "../../Util/Logo";
 
 const BackgroundImage = styled.div`
   background-image: url("/src/public/shopping-bag.jpg");
@@ -20,7 +21,7 @@ const BackgroundImage = styled.div`
 `;
 
 const LoginContainer = styled.div`
-  background-color: var(--gray-4);
+  background-color: var(--gray-3);
   height: 100vh;
   width: 60vw;
   left: -200px;
@@ -52,11 +53,69 @@ const ContentHolder = styled.div`
   position: fixed;
 `;
 
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+
+  & > *:first-child {
+    align-self: center;
+  }
+`;
+
+const Input = styled.input`
+  border: none;
+  padding: 0.5rem 1rem;
+  border-radius: 12px;
+
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+  background-color: var(--gray-2);
+
+  transition: all 200ms;
+
+  color: var(--gray-8);
+
+  font-size: 1.6rem;
+
+  &::placeholder {
+    color: var(--gray-6);
+  }
+
+  &:focus {
+    outline: none;
+    background-color: var(--gray-0);
+  }
+
+  &[type="password"] {
+    font-variant: small-caption;
+
+    &::placeholder {
+      font-variant: normal;
+    }
+  }
+`;
+
+const Label = styled.label`
+  color: var(--gray-6);
+  font-weight: 500;
+
+  font-size: 1.6rem;
+`;
+
 export const LoginPage = () => {
   return (
     <>
       <LoginContainer />
-      <ContentHolder></ContentHolder>
+      <ContentHolder>
+        <Form>
+          <Logo $color="orange" />
+
+          <Label htmlFor="username">Username</Label>
+          <Input type="text" placeholder="Username" id="username" />
+
+          <Label htmlFor="password">Password</Label>
+          <Input type="password" placeholder="Password" id="password" />
+        </Form>
+      </ContentHolder>
       <BackgroundImage />
     </>
   );
