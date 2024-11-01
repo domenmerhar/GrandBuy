277e65ac547fc53c3df2a6663d0405cd83c825c2
Domen Merhar <domen.merhar@students.vegova.si>
2024-11-01 07:39:25 +0100

    Update SliderFilter.tsx

--- client/src/pages/search/SliderFilter.tsx
+++ client/src/pages/search/SliderFilter.tsx
@@ -1,8 +1,7 @@
 import { Slider } from "@mui/material";
-import { useRef, useState } from "react";
 import { NakedInput } from "../../Util/NakedInput";
 import styled from "styled-components";
-import { useSearchParams } from "react-router-dom";
+import { useSliderFilter } from "./SliderFilter.hook";
 
 const RangeHolder = styled.div`
   display: flex;
@@ -11,63 +10,12 @@ const RangeHolder = styled.div`
 `;
 
 function valuetext(value: number) {
-  return `${value}€`;
+  return `$${value}`;
 }
 
-const initialMaxValue = 100;
-
 export const SliderFilter = () => {
-  const [value, setValue] = useState<number[]>([0, 0]);
-  const [maxValue, setMaxValue] = useState<number>(initialMaxValue);
-  const [, setSearchParams] = useSearchParams();
-
-  const minRef = useRef<HTMLInputElement>(null);
-  const maxRef = useRef<HTMLInputElement>(null);
-
-  const handleChangeSlider = (_: Event, newValue: number | number[]) => {
-    setValue(newValue as number[]);
-
-    const min = Math.min(...(newValue as number[]));
-    const max = Math.max(...(newValue as number[]));
-
-    minRef.current!.value = `$${min}`;
-    maxRef.current!.value = `$${max}`;
-
-    setMaxValue((prev) => (max * 1.1 > prev ? prev + 5 : prev));
-  };
-
-  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
-    const newValue = parseInt(e.target.value.replace("$", ""));
-    if (isNaN(newValue)) return;
-
-    setValue((prev) => {
-      const updatedValue = [...prev];
-      if (e.target === minRef.current) {
-        updatedValue[0] = newValue;
-      } else {
-        updatedValue[1] = newValue;
-        setMaxValue((prev) => (newValue > initialMaxValue ? newValue : prev));
-      }
-
-      const min = Math.min(...(updatedValue as number[]));
-      const max = Math.max(...(updatedValue as number[]));
-
-      setSearchParams((searchParams) => {
-        if (!min) searchParams.delete("from");
-        else searchParams.set("from", min.toString());
-
-        if (!max) searchParams.delete("to");
-        else searchParams.set("to", max.toString());
-
-        return searchParams;
-      });
-
-      minRef.current!.value = `$${min}`;
-      maxRef.current!.value = `$${max}`;
-
-      return updatedValue;
-    });
-  };
+  const { handleBlur, handleChangeSlider, maxRef, maxValue, minRef, value } =
+    useSliderFilter();
 
   return (
     <>
