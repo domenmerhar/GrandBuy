6e1476d7620a2c6136538c68b8ba51ec5ced9b17
Domen Merhar <domen.merhar@students.vegova.si>
2024-11-02 11:42:17 +0100

    Add BlankCard.tsx

---
+++ client/src/Util/BlankCard.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const BlankCard = styled.div`
+  border-radius: 12px;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+
+  padding: 1.2rem 1.6rem;
+`;
