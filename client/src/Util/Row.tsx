74b208875e7f7f69910f12dbf21750255defee97
Domen Merhar <domen.merhar@students.vegova.si>
2024-11-02 11:40:50 +0100

    Add Row.tsx

---
+++ client/src/Util/Row.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { RowColumnProps } from "./types";
+
+export const Row = styled.div<RowColumnProps>`
+  display: flex;
+
+  ${({ $gap }) => $gap && `gap: ${$gap};`}
+
+  ${({ $justifyContent }) =>
+    $justifyContent && `justify-content: ${$justifyContent};`}
+
+  ${({ $alignItems }) => $alignItems && `align-items: ${$alignItems};`}
+`;
