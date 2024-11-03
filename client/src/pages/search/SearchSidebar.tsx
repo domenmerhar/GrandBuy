a58e66eccdbff39d08d8b455055074961c94e49c
Domen Merhar <domen.merhar@students.vegova.si>
2024-11-03 09:44:11 +0100

    Update SearchSidebar.tsx

--- client/src/pages/search/SearchSidebar.tsx
+++ client/src/pages/search/SearchSidebar.tsx
@@ -1,35 +1,10 @@
-import styled from "styled-components";
-import { HeaderUppercaseBold } from "../../Util/HeaderUppercaseBold";
+import { ProductFilter } from "../../Util/ProductFilter";
 import { Sidebar } from "../../Util/Sidebar";
-import { CheckboxWithText } from "./CheckboxWithText";
-import { RatingInteractive } from "./RatingInteractive";
-import { SliderFilter } from "./SliderFilter";
-
-const ContentHolder = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  & > h2:not(:first-child) {
-    margin-top: 12px;
-  }
-`;
 
 export const SearchSidebar = () => {
   return (
     <Sidebar>
-      <ContentHolder>
-        <HeaderUppercaseBold>Delivery</HeaderUppercaseBold>
-        <CheckboxWithText id="free-shipping" label="Free Shipping" />
-
-        <HeaderUppercaseBold>Discount</HeaderUppercaseBold>
-        <CheckboxWithText id="sale" label="Sale" />
-
-        <HeaderUppercaseBold>Rating</HeaderUppercaseBold>
-        <RatingInteractive />
-
-        <HeaderUppercaseBold>Price</HeaderUppercaseBold>
-        <SliderFilter />
-      </ContentHolder>
+      <ProductFilter />
     </Sidebar>
   );
 };
