eeae278497690ad451e17267e2964af2d60f24f1
Domen Merhar <domen.merhar@students.vegova.si>
2024-11-03 12:55:43 +0100

    Add RefundPage.tsx

---
+++ client/src/pages/refund/RefundPage.tsx
@@ -0,0 +1,10 @@
+import { Content } from "../../Util/Content";
+import { RefundPageHeader } from "./RefundPageHeader";
+
+export const RefundPage = () => {
+  return (
+    <Content>
+      <RefundPageHeader />
+    </Content>
+  );
+};
