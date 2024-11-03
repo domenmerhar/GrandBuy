5d0e2b2b440412104c944ce89653b0eff982b604
Domen Merhar <domen.merhar@students.vegova.si>
2024-11-03 09:44:03 +0100

    Update Sidebar.tsx

--- client/src/Util/Sidebar.tsx
+++ client/src/Util/Sidebar.tsx
@@ -1,19 +1,10 @@
 import styled from "styled-components";
 import { FC, ReactNode } from "react";
+import { StyledSidebar } from "./StyledSidebar";
 
 interface SidebarProps {
   children: ReactNode | ReactNode[];
 }
-
-const StyledSidebar = styled.aside`
-  position: fixed;
-  display: inline-block;
-  min-width: 20rem;
-  width: 32rem;
-  height: 100vh;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  padding: 2.4rem 3.2rem;
-`;
 
 const Placeholder = styled.div`
   display: inline-block;
@@ -25,7 +16,7 @@ const Placeholder = styled.div`
 export const Sidebar: FC<SidebarProps> = ({ children }) => {
   return (
     <>
-      <StyledSidebar>{children}</StyledSidebar>
+      <StyledSidebar $position="fixed">{children}</StyledSidebar>
       <Placeholder />
     </>
   );
