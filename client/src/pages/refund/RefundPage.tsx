d4ea480a391c7ed226fff1f0536f5f2e0f2d4c8c
Domen Merhar <domen.merhar@students.vegova.si>
2024-11-03 20:39:21 +0100

    Update RefundPage.tsx

--- client/src/pages/refund/RefundPage.tsx
+++ client/src/pages/refund/RefundPage.tsx
@@ -1,10 +1,27 @@
+import styled from "styled-components";
 import { Content } from "../../Util/Content";
+import { RefundCard } from "./RefundCard";
 import { RefundPageHeader } from "./RefundPageHeader";
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(40rem, 42rem));
+  justify-content: space-between;
+
+  row-gap: 3.2rem;
+  margin-top: 3.2rem;
+`;
 
 export const RefundPage = () => {
   return (
     <Content>
       <RefundPageHeader />
+      <Grid>
+        <RefundCard />
+        <RefundCard />
+        <RefundCard />
+        <RefundCard />
+      </Grid>
     </Content>
   );
 };
