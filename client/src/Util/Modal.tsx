34db7716f915a2acd59d50a184ec13f56ffa5a77
Domen Merhar <domen.merhar@students.vegova.si>
2024-11-23 12:54:01 +0100

    Update Modal.tsx

--- client/src/Util/Modal.tsx
+++ client/src/Util/Modal.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import styled from "styled-components";
 import { Row } from "./Row";
 import { Button } from "./Button";
@@ -8,6 +8,9 @@ import { Backdrop } from "./Backdrop";
 interface ModalProps {
   title?: string;
   children?: string;
+
+  onSubmit?: (e?: React.MouseEvent<HTMLButtonElement, MouseEvent>) => unknown;
+  onCancel?: (e?: React.MouseEvent<HTMLButtonElement, MouseEvent>) => unknown;
 }
 
 const StyledModal = styled.div`
@@ -39,6 +42,7 @@ const Header = styled.div`
   background: linear-gradient(45deg, var(--orange-6), var(--orange-5));
   border-radius: 10px 10px 0 0;
   overflow: hidden;
+  min-height: 4.8rem;
 
   padding: 0.4rem 0.6rem;
 `;
@@ -57,7 +61,28 @@ const Content = styled.p`
   }
 `;
 
-export const Modal: FC<ModalProps> = ({ title, children }) => {
+export const Modal: FC<ModalProps> = ({
+  title,
+  children,
+  onCancel,
+  onSubmit,
+}) => {
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+
+  const handleCancel = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    if (onCancel) onCancel(e);
+    setIsOpen(false);
+  };
+
+  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    if (onSubmit) onSubmit(e);
+    setIsOpen(true);
+  };
+
+  const handleBackdropClick = () => setIsOpen(false);
+
+  if (!isOpen) return null;
+
   return (
     <>
       <StyledModal>
@@ -69,16 +94,27 @@ export const Modal: FC<ModalProps> = ({ title, children }) => {
           <Content>{children}</Content>
 
           <Row $gap="1.2rem" $alignItems="center">
-            <Button $color="gray" $shape="oval" $size="medium">
+            <Button
+              $color="gray"
+              $shape="oval"
+              $size="medium"
+              onClick={handleCancel}
+            >
               Cancel
             </Button>
-            <Button $color="orange" $shape="oval" $size="medium">
+
+            <Button
+              $color="orange"
+              $shape="oval"
+              $size="medium"
+              onSubmit={handleSubmit}
+            >
               Submit
             </Button>
           </Row>
         </Column>
       </StyledModal>
-      <Backdrop />
+      <Backdrop onClick={handleBackdropClick} />
     </>
   );
 };
