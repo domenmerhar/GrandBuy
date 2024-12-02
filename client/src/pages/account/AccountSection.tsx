91343361071d1c656321506aabd5e49696c84c81
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-02 11:19:49 +0100

    Add AccountSection.tsx

---
+++ client/src/pages/account/AccountSection.tsx
@@ -0,0 +1,72 @@
+import { Button } from "../../Util/Button";
+import { Column } from "../../Util/Column";
+import { HeaderUppercaseBold } from "../../Util/HeaderUppercaseBold";
+import { InputWithLabel } from "../../Util/InputWithLabel";
+import { Row } from "../../Util/Row";
+import { AccountForm } from "./AccountForm";
+
+export const AccountSection = () => {
+  return (
+    <AccountForm>
+      <HeaderUppercaseBold as="h2">Account info</HeaderUppercaseBold>
+      <Row $gap="8px">
+        <Column $gap="4px">
+          <InputWithLabel
+            id="first-name"
+            title="First Name"
+            type="text"
+            placeholder="John"
+          />
+        </Column>
+
+        <Column $gap="4px">
+          <InputWithLabel
+            id="last-name"
+            title="Last Name"
+            type="text"
+            placeholder="Doe"
+          />
+        </Column>
+      </Row>
+
+      <InputWithLabel
+        id="billing"
+        title="Billing Address"
+        type="text"
+        placeholder="21st Jump Street"
+      />
+
+      <InputWithLabel
+        id="city"
+        title="City"
+        type="text"
+        placeholder="New York"
+      />
+
+      <InputWithLabel
+        id="zip"
+        title="Zip or Postal Code"
+        type="text"
+        placeholder="10001"
+      />
+
+      <InputWithLabel
+        id="country"
+        title="Country"
+        type="text"
+        placeholder="Nigeria"
+      />
+
+      <InputWithLabel
+        id="phone"
+        title="Phone Number"
+        type="text"
+        placeholder="+234 123 456 7890"
+      />
+
+      <Button $color="orange" $shape="rectangle" $size="medium">
+        Save Changes
+      </Button>
+    </AccountForm>
+  );
+};
