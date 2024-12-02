709358b9ecd6f4d4d94f4c646e5cbc888e31d879
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-02 11:19:05 +0100

    Add AccountForm.tsx

---
+++ client/src/pages/account/AccountForm.tsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const AccountForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+
+  align-items: stretch;
+  width: 50rem;
+
+  & > h2,
+  & > h3 {
+    align-self: flex-start;
+    margin-bottom: 1.2rem;
+  }
+
+  & > label {
+    margin-top: 8px;
+  }
+
+  & > label:first-of-type {
+    margin-top: 0;
+  }
+
+  & > button {
+    margin-top: 2.4rem;
+  }
+`;
