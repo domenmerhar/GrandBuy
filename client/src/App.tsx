8cd42b759f0fcc60fc0e66049a979df2f6a22e2d
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-04 19:28:17 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
 import GlobalStyles from "./GlobalStyles";
+import { NavigationBar } from "./Components/NavigationBar";
 import { BrowserRouter } from "react-router-dom";
+import { AccountPage } from "./pages/account/AccountPage";
+import { LanguageProvider } from "./Util/LanguageContext";
 
 function App() {
   return (
     <BrowserRouter>
-      <GlobalStyles />
+      <LanguageProvider>
+        <GlobalStyles />
+        <NavigationBar />
+        <AccountPage />
+      </LanguageProvider>
     </BrowserRouter>
   );
 }
