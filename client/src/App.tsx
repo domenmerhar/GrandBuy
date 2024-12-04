cedb0b047e4ed835bfa8f9a72dd7b296363d17f4
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-04 20:02:34 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,36 @@
 import GlobalStyles from "./GlobalStyles";
 import { NavigationBar } from "./Components/NavigationBar";
-import { BrowserRouter } from "react-router-dom";
-import { AccountPage } from "./pages/account/AccountPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { LanguageProvider } from "./Util/LanguageContext";
+import { MainPage } from "./pages/main/MainPage";
+import { AccountPage } from "./pages/account/AccountPage";
+import { CartPage } from "./pages/Cart/CartPage";
+import { HistoryPage } from "./pages/history/HistoryPage";
+import { RefundPage } from "./pages/refund/RefundPage";
+import { SearchPage } from "./pages/search/SearchPage";
+import { SignupPage } from "./pages/signup/Signup";
 
 function App() {
   return (
-    <BrowserRouter>
-      <LanguageProvider>
-        <GlobalStyles />
-        <NavigationBar />
-        <AccountPage />
-      </LanguageProvider>
-    </BrowserRouter>
+    <LanguageProvider>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<NavigationBar />}>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/account" element={<AccountPage />} />
+            <Route path="/history" element={<HistoryPage />} />
+            <Route path="/refund" element={<RefundPage />} />
+            <Route path="/search" element={<SearchPage />} />
+          </Route>
+
+          <Route path="/signup" element={<SignupPage />} />
+
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </LanguageProvider>
   );
 }
 
