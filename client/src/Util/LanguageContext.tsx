ef64959e4aedd3bd36238570da31951193709cb1
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-04 19:27:45 +0100

    Add LangaugeContext.tsx

---
+++ client/src/Util/LanguageContext.tsx
@@ -0,0 +1,33 @@
+import React, { createContext, FC, useState } from "react";
+
+type languages = "en" | "sl";
+
+interface LanguageContextType {
+  currentLanguage: languages;
+  setCurrentLanguage: React.Dispatch<React.SetStateAction<languages>>;
+}
+
+interface LanguageProviderProps {
+  children: React.ReactNode | React.ReactNode[];
+}
+
+const LanguageContext = createContext<LanguageContextType | null>(null);
+
+export const LanguageProvider: FC<LanguageProviderProps> = ({ children }) => {
+  const [currentLanguage, setCurrentLanguage] = useState<languages>("sl");
+
+  return (
+    <LanguageContext.Provider value={{ currentLanguage, setCurrentLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const useLanguage = () => {
+  const context = React.useContext(LanguageContext);
+  if (!context) {
+    throw new Error("useLanguage must be used within a LanguageProvider");
+  }
+
+  return context;
+};
