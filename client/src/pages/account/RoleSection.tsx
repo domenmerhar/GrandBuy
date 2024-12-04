8bc499865d6dfcc22b24d476c679a5e14c6e65fa
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-04 19:28:01 +0100

    Update RoleSelection.tsx

--- client/src/pages/account/RoleSection.tsx
+++ client/src/pages/account/RoleSection.tsx
@@ -1,24 +1,33 @@
 import { Button } from "../../Util/Button";
 import { HeaderUppercaseBold } from "../../Util/HeaderUppercaseBold";
 import { InputWithLabel } from "../../Util/InputWithLabel";
+import { Modal } from "../../Util/Modal";
 import { AccountForm } from "./AccountForm";
 
 export const RoleSection = () => {
+  const handleSubmit = () => {};
   return (
-    <AccountForm>
-      <HeaderUppercaseBold as="h2">Role</HeaderUppercaseBold>
+    <>
+      <AccountForm onSubmit={handleSubmit}>
+        <HeaderUppercaseBold as="h2">Role</HeaderUppercaseBold>
 
-      <InputWithLabel
-        id="role"
-        title="Role"
-        type="text"
-        placeholder="User"
-        disabled={true}
-      />
+        <InputWithLabel
+          id="role"
+          title="Role"
+          type="text"
+          placeholder="User"
+          disabled={true}
+        />
 
-      <Button $color="orange" $shape="rectangle" $size="medium">
-        Request Seller
-      </Button>
-    </AccountForm>
+        <Button $color="orange" $shape="rectangle" $size="medium">
+          Request Seller
+        </Button>
+      </AccountForm>
+
+      <Modal.Window title="Test">
+        After the requests approval you won’t be able to post a review on a
+        product anymore.
+      </Modal.Window>
+    </>
   );
 };
