2827ccb8e3d4f6fde5a27ff28c8b73d867d13b25
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-04 19:44:30 +0100

    Update LanguageSection.tsx

--- client/src/pages/account/LanguageSection.tsx
+++ client/src/pages/account/LanguageSection.tsx
@@ -1,17 +1,30 @@
 import { HeaderUppercaseBold } from "../../Util/HeaderUppercaseBold";
+import { useLanguage } from "../../Util/LanguageContext";
 import { SwitchButtons } from "../../Util/SwitchButtons";
+import { languages } from "../../Util/types";
 import { AccountForm } from "./AccountForm";
 
 export const LanguageSection = () => {
+  const { currentLanguage, setCurrentLanguage } = useLanguage();
+
+  const handleClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    value?: string
+  ) => {
+    if (!value) return;
+
+    setCurrentLanguage(value as languages);
+  };
   return (
     <AccountForm as="div">
       <HeaderUppercaseBold as="h2">Language</HeaderUppercaseBold>
 
       <SwitchButtons
         options={[
-          { name: "EN", value: "en" },
-          { name: "SL", value: "sl" },
+          { name: "EN", value: "en", disabled: currentLanguage === "en" },
+          { name: "SL", value: "sl", disabled: currentLanguage === "sl" },
         ]}
+        onClick={handleClick}
       />
     </AccountForm>
   );
