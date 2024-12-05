e518e331f61d4498f942f6c43f9ff59cb33b870f
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-05 15:09:39 +0100

    Renamed app path and element

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,13 @@ import { NavigationBar } from "./Components/NavigationBar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { LanguageProvider } from "./Util/LanguageContext";
 import { MainPage } from "./pages/main/MainPage";
-import { AccountPage } from "./pages/account/AccountPage";
+import { AccountPage } from "./pages/settings/SettingsPage";
 import { CartPage } from "./pages/Cart/CartPage";
 import { HistoryPage } from "./pages/history/HistoryPage";
 import { RefundPage } from "./pages/refund/RefundPage";
 import { SearchPage } from "./pages/search/SearchPage";
 import { SignupPage } from "./pages/signup/Signup";
+import { UserPage } from "./pages/user/UserPage";
 
 function App() {
   return (
@@ -19,10 +20,11 @@ function App() {
           <Route path="/" element={<NavigationBar />}>
             <Route path="/" element={<MainPage />} />
             <Route path="/cart" element={<CartPage />} />
-            <Route path="/account" element={<AccountPage />} />
+            <Route path="/settings" element={<AccountPage />} />
             <Route path="/history" element={<HistoryPage />} />
             <Route path="/refund" element={<RefundPage />} />
             <Route path="/search" element={<SearchPage />} />
+            {/* <Route path="/user/:userId" element={<UserPage />} /> */}
           </Route>
 
           <Route path="/signup" element={<SignupPage />} />
