6a99067436212db30267aea6db3e92cbc94e29e6
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-10 18:11:18 +0100

    Update ReviewReplyWindow.tsx

--- client/src/pages/account/ReviewReplyWindow.tsx
+++ client/src/pages/account/ReviewReplyWindow.tsx
@@ -1,18 +1,6 @@
-import styled from "styled-components";
-import { ButtonWithNotifcations } from "../../Components/ButtonWithNotifcations";
 import { CardWithHeader } from "../../Util/CardWithHeader";
-import { Row } from "../../Util/Row";
-import { HiDotsVertical } from "react-icons/hi";
-import { ReviewReplyCard } from "./ReviewReplyCard";
-import { ReviewReplyWindowSelect } from "./ReviewReplyWindowSelect";
-import { Column } from "../../Util/Column";
 import { ReviewReplyWindowHeader } from "./ReviewReplyWindowHeader";
-
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-  gap: 3.2rem;
-`;
+import { ReviewReplyWindowBody } from "./ReviewReplyWindowBody";
 
 export const ReviewReplyWindow = () => {
   return (
@@ -20,17 +8,7 @@ export const ReviewReplyWindow = () => {
       <CardWithHeader>
         <ReviewReplyWindowHeader />
 
-        <CardWithHeader.Body>
-          <Column $gap="3.2rem">
-            <ReviewReplyWindowSelect />
-            <Grid>
-              <ReviewReplyCard />
-              <ReviewReplyCard />
-              <ReviewReplyCard />
-              <ReviewReplyCard />
-            </Grid>
-          </Column>
-        </CardWithHeader.Body>
+        <ReviewReplyWindowBody />
       </CardWithHeader>
     </>
   );
