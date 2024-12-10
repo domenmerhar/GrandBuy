fa99fc6cab128116e9496be7d07c5ee47b903e28
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-10 17:59:50 +0100

    Update ReviewReplyCard.tsx

--- client/src/pages/account/ReviewReplyCard.tsx
+++ client/src/pages/account/ReviewReplyCard.tsx
@@ -1,44 +1,50 @@
 import styled from "styled-components";
-import { ButtonWithNotifcations } from "../../Components/ButtonWithNotifcations";
-import { CardWithHeader } from "../../Util/CardWithHeader";
+import { BlankCard } from "../../Util/BlankCard";
+import { Column } from "../../Util/Column";
 import { Row } from "../../Util/Row";
-import { HiDotsVertical } from "react-icons/hi";
-import { NavLink } from "react-router-dom";
 
-const StyledNavLink = styled(NavLink)`
-  text-decoration: none;
-  color: var(--gray-1);
-
-  &.active {
-    font-weight: 600;
-  }
+const ProfileImage = styled.img`
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
 `;
 
-const ButtonHolder = styled.div`
-  margin-left: auto;
+const User = styled.span`
+  color: var(--gray-7);
+`;
+
+const Date = styled.span`
+  font-size: 1.4rem;
+  color: var(--gray-6);
+`;
+
+const P = styled.p`
+  color: var(--gray-8);
+  max-width: 50ch;
 `;
 
 export const ReviewReplyCard = () => {
   return (
-    <>
-      <CardWithHeader>
-        <CardWithHeader.Header>
-          <Row $gap="1.6rem" $alignItems="center">
-            <StyledNavLink to="account">Reviews</StyledNavLink>
-            <StyledNavLink to="replies">Replies</StyledNavLink>
-
-            <ButtonHolder>
-              <ButtonWithNotifcations>
-                <HiDotsVertical />
-              </ButtonWithNotifcations>
-            </ButtonHolder>
+    <BlankCard>
+      <Column $gap="2rem">
+        <Column $gap="8px">
+          <Row $justifyContent="space-between" $alignItems="center">
+            <Row $gap="12px" $alignItems="center">
+              <ProfileImage src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSA4_6SCuZdyzZD0pgNR1ncralqCAz_8w8R_g&s" />
+              <User>John Doe</User>
+            </Row>
           </Row>
-        </CardWithHeader.Header>
-        <CardWithHeader.Body>
-          <p>Reviews</p>
-          <p>Replies</p>
-        </CardWithHeader.Body>
-      </CardWithHeader>
-    </>
+
+          <Date>22. 5. 2025</Date>
+        </Column>
+
+        <P>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam fugit
+          laudantium aliquid labore et deleniti distinctio. Recusandae libero
+          minus laudantium, incidunt magnam deserunt ipsam fugiat perspiciatis
+          ut ea dolore obcaecati!
+        </P>
+      </Column>
+    </BlankCard>
   );
 };
