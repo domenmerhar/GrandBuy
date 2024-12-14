4c2ce07b258315de763767a04eecf98cb4533f9e
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-14 12:06:04 +0100

    Update cartItemModel.ts

--- server/src/models/cartItemModel.ts
+++ server/src/models/cartItemModel.ts
@@ -1,36 +1,41 @@
-import mongoose from "mongoose";
+import mongoose, { Document, Schema } from "mongoose";
 
-const CartItemSchema = new mongoose.Schema({
+interface ICartItem extends Document {
+  user: mongoose.Schema.Types.ObjectId;
+  product: mongoose.Schema.Types.ObjectId;
+  quantity: number;
+  createdAt: Date;
+  discount: number;
+  status: "pending" | "cancelled" | "shipped" | "delivered" | "refunded";
+  ordered: boolean;
+}
+
+const CartItemSchema = new Schema<ICartItem>({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     required: true,
   },
-
   product: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Product",
     required: true,
   },
-
   quantity: {
     type: Number,
     min: [1, "Quantity must be at least 1"],
     required: true,
   },
-
   createdAt: {
     type: Date,
     default: Date.now,
   },
-
   discount: {
     type: Number,
     min: [0, "Discount must be at least 0"],
     max: [100, "Discount cannot exceed 100"],
     default: 0,
   },
-
   status: {
     type: String,
     enum: {
@@ -39,15 +44,16 @@ const CartItemSchema = new mongoose.Schema({
     },
     default: "pending",
   },
-
   ordered: Boolean,
 });
 
-//CartItemSchema.index({ user: 1, product: 1 }, { unique: true });
 CartItemSchema.index({ user: 1, createdAt: 1 });
 
 CartItemSchema.pre(/^find/, function (next) {
-  this.populate({ path: "user", select: "name email image" });
+  (this as mongoose.Query<any, ICartItem>).populate({
+    path: "user",
+    select: "name email image",
+  });
   next();
 });
 
@@ -59,4 +65,4 @@ CartItemSchema.set("toObject", {
   versionKey: false,
 });
 
-export default mongoose.model("CartItem", CartItemSchema);
+export default mongoose.model<ICartItem>("CartItem", CartItemSchema);
