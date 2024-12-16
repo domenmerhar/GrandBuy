6ae39f94617700186b5e7bbd1ca531b172e7793c
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-16 09:31:47 +0100

    Update ArrowButton.tsx

---
+++ client/src/Util/ArrowButton.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+interface ArrowButtonProps {
+  $size: "medium" | "large";
+}
+
+export const ArrowButton = styled.button<ArrowButtonProps>`
+  cursor: pointer;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  box-shadow: 0 0 0.8rem rgba(0, 0, 0, 0.2);
+
+  user-select: none;
+  border: none;
+  border-radius: 50%;
+  background-color: var(--gray-1);
+
+  &:disabled {
+    cursor: not-allowed;
+    opacity: 0.5;
+  }
+
+  ${({ $size }) => $size === "medium" && `height: 4.8rem; width: 4.8rem;`}
+  ${({ $size }) => $size === "large" && `height: 6.4rem; width: 6.4rem;`}
+  margin: auto 0;
+
+  transition: all 200ms;
+
+  & svg {
+    ${({ $size }) => $size === "medium" && `height: 4.2rem; width: 4.2rem;`}
+    ${({ $size }) => $size === "large" && `height: 6.6rem; width: 5.6rem;`}
+    fill: var(--gray-8);
+  }
+`;
