d87720d065cc826a35628b6f91508b595a005c41
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-17 18:25:16 +0100

    Update ProductPage.tsx

--- client/src/pages/product/ProductPage.tsx
+++ client/src/pages/product/ProductPage.tsx
@@ -1,11 +1,19 @@
 import { Content } from "../../Util/Content";
 import { ImageCarousel } from "./ImageCarousel";
 import { MoreFromSellerSection } from "./MoreFromSellerSection";
+import { StyledSidebar } from "../../Util/StyledSidebar";
+import { ProductFilter } from "../../Util/ProductFilter";
+import { Row } from "../../Util/Row";
 
 export const ProductPage = () => {
   return (
     <Content>
-      <ImageCarousel />
+      <Row $gap="2.8rem">
+        <ImageCarousel />
+        <StyledSidebar $position="sticky" $height="80vh">
+          <ProductFilter />
+        </StyledSidebar>
+      </Row>
       <MoreFromSellerSection />
     </Content>
   );
