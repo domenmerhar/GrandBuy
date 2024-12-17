754f53664be3940636dcb347c08822003f3552f7
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-17 19:20:24 +0100

    Update ProductInfo.tsx

--- client/src/pages/product/ProductInfo.tsx
+++ client/src/pages/product/ProductInfo.tsx
@@ -3,26 +3,49 @@ import { Column } from "../../Util/Column";
 import { useParams } from "react-router-dom";
 import { HeaderUppercaseBold } from "../../Util/HeaderUppercaseBold";
 import { ProductInfoParagraph } from "./ProductInfoParagraph";
+import { Stepper } from "../../Util/Stepper";
+import { Button } from "../../Util/Button";
 
 const StyledProductInfo = styled(Column)`
-  min-width: 30rem;
+  min-width: 25rem;
+  height: 100%;
+`;
 
+const Info = styled(Column)`
   font-size: 2rem;
   & > :first-child {
     font-size: 3.6rem;
     margin-bottom: 0.8rem;
   }
+
+  /* & div:nth-of-type(1) {
+    margin-top: 1.6rem;
+  } */
 `;
 
 export const ProductInfo = () => {
   const { productId } = useParams();
 
   return (
-    <StyledProductInfo $gap=".6rem">
-      <HeaderUppercaseBold>{productId}</HeaderUppercaseBold>
-      <ProductInfoParagraph title="Price" value="$299.99" />
-      <ProductInfoParagraph title="Shipping" value="$20.99" />
-      <ProductInfoParagraph title="Average Rating" value="3" />
+    <StyledProductInfo $gap="2.4rem">
+      <Info $gap=".6rem">
+        <HeaderUppercaseBold>{productId}</HeaderUppercaseBold>
+        <ProductInfoParagraph title="Price" value="$299.99" />
+        <ProductInfoParagraph title="Shipping" value="$20.99" />
+        <ProductInfoParagraph title="Average Rating" value="3" />
+      </Info>
+
+      <Stepper searchParamName="quantity" />
+
+      <Column $gap=".8rem">
+        <Button $color="orange" $shape="oval" $size="medium">
+          Add to Cart
+        </Button>
+
+        <Button $color="gray" $shape="oval" $size="medium">
+          Buy now
+        </Button>
+      </Column>
     </StyledProductInfo>
   );
 };
