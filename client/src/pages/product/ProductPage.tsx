ba992716ec92de98a176cdec8c9fdd7c24d248f6
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-18 19:41:21 +0100

    Update ProductPage.tsx

--- client/src/pages/product/ProductPage.tsx
+++ client/src/pages/product/ProductPage.tsx
@@ -1,25 +1,22 @@
 import { Content } from "../../Util/Content";
-import { ImageCarousel } from "./ImageCarousel";
 import { MoreFromSellerSection } from "./MoreFromSellerSection";
-import { StyledSidebar } from "../../Util/StyledSidebar";
-import { Row } from "../../Util/Row";
 import { SaleSection } from "../main/SaleSection";
-import { ProductInfo } from "./ProductInfo";
-import styled from "styled-components";
-
-const ProducInfoHolder = styled(StyledSidebar)`
-  align-self: flex-start;
-`;
+import { BlankCard } from "../../Util/BlankCard";
+import { SliderInfoRow } from "./SliderInfoRow";
 
 export const ProductPage = () => {
   return (
     <Content>
-      <Row $gap="2.8rem" $alignItems="center">
-        <ImageCarousel />
-        <ProducInfoHolder $position="sticky" $height="400px">
-          <ProductInfo />
-        </ProducInfoHolder>
-      </Row>
+      <SliderInfoRow />
+
+      <BlankCard>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique
+          aliquam tempora nemo repellat! Quasi, aliquid, magnam ratione illo,
+          provident hic repudiandae sit vel modi animi asperiores aliquam ab
+          sequi ullam.
+        </p>
+      </BlankCard>
 
       <MoreFromSellerSection />
 
