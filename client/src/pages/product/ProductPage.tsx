ed933df8d3027a8a47ae3c28fd22c1bff8c5ef90
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-18 19:36:53 +0100

    Update ProductPage.tsx

--- client/src/pages/product/ProductPage.tsx
+++ client/src/pages/product/ProductPage.tsx
@@ -5,15 +5,20 @@ import { StyledSidebar } from "../../Util/StyledSidebar";
 import { Row } from "../../Util/Row";
 import { SaleSection } from "../main/SaleSection";
 import { ProductInfo } from "./ProductInfo";
+import styled from "styled-components";
+
+const ProducInfoHolder = styled(StyledSidebar)`
+  align-self: flex-start;
+`;
 
 export const ProductPage = () => {
   return (
     <Content>
       <Row $gap="2.8rem" $alignItems="center">
         <ImageCarousel />
-        <StyledSidebar $position="sticky" $height="75vh">
+        <ProducInfoHolder $position="sticky" $height="400px">
           <ProductInfo />
-        </StyledSidebar>
+        </ProducInfoHolder>
       </Row>
 
       <MoreFromSellerSection />
