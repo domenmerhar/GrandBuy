7635a03f344520acd6adf02692bdaa59eabcaa68
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-20 19:13:56 +0100

    Add ProductDescription.tsx

---
+++ client/src/pages/product/ProductDescription.tsx
@@ -0,0 +1,14 @@
+import { BlankCard } from "../../Util/BlankCard";
+
+export const ProductDescription = () => {
+  return (
+    <BlankCard>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique
+        aliquam tempora nemo repellat! Quasi, aliquid, magnam ratione illo,
+        provident hic repudiandae sit vel modi animi asperiores aliquam ab sequi
+        ullam.
+      </p>
+    </BlankCard>
+  );
+};
