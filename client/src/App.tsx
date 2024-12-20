696486aababcdcd06e74cd0848d9815e527558c3
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-20 19:27:53 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,11 +7,12 @@ import { CartPage } from "./pages/Cart/CartPage";
 import { HistoryPage } from "./pages/history/HistoryPage";
 import { RefundPage } from "./pages/refund/RefundPage";
 import { SearchPage } from "./pages/search/SearchPage";
-import { SignupPage } from "./pages/signup/Signup";
+import { SignupPage } from "./pages/signup/SignupPage";
 import { AccountPage } from "./pages/account/AccountPage";
 import { SettingsPage } from "./pages/settings/SettingsPage";
 import { ProductPage } from "./pages/product/ProductPage";
 import { MyProducts } from "./pages/myProducts/myProducts";
+import { LoginPage } from "./pages/login/LoginPage";
 
 function App() {
   return (
@@ -37,6 +38,7 @@ function App() {
           </Route>
 
           <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
 
           <Route path="*" element={<h1>404 Not Found</h1>} />
         </Routes>
