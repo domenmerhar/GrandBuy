b495fd4758995e7231cb90862267674679b5d5e5
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-21 12:18:21 +0100

    Add FilterSortHeader.tsx

---
+++ client/src/Util/FilterSortHeader.tsx
@@ -0,0 +1,36 @@
+import { Header } from "./Header";
+import { Row } from "./Row";
+import { Filter } from "./Filter";
+import { IOption } from "./types";
+import { Select } from "./Select";
+import { FC } from "react";
+
+interface RefundPageHeaderProps {
+  headerText: string;
+  selectOptions?: IOption[];
+  filterOptions?: IOption[];
+}
+
+export const FilterSortHeader: FC<RefundPageHeaderProps> = ({
+  headerText,
+  filterOptions,
+  selectOptions,
+}) => {
+  return (
+    <Row
+      $justifyContent="space-between"
+      $alignItems="center"
+      $flexWrap="wrap"
+      $gap="2rem"
+    >
+      <Header as="h2" $size="medium" $color="orange">
+        {headerText}
+      </Header>
+
+      <Row $gap="12px" $alignItems="center">
+        {filterOptions && <Filter options={filterOptions} />}
+        {selectOptions && <Select options={selectOptions} />}
+      </Row>
+    </Row>
+  );
+};
