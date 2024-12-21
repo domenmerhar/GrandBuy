cd84d1261b9b28504f9cbfe0fbe330dcbffe4a76
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-21 10:50:20 +0100

    Update WishlistPage.tsx

--- client/src/pages/wishlist/WishlistPage.tsx
+++ client/src/pages/wishlist/WishlistPage.tsx
@@ -1,14 +1,24 @@
 import { Content } from "../../Util/Content";
-import { Header } from "../../Util/Header";
-import { SaleSection } from "../main/SaleSection";
+import { Stepper } from "../../Util/Stepper";
+import { CardFilterGrid } from "../../Util/CardFilterGrid";
+import { StyledSidebar } from "../../Util/StyledSidebar";
+import { ProductFilter } from "../../Util/ProductFilter";
+import { ProductsCard } from "../../Util/ProductsCard";
+import { CartItem } from "../Cart/CartItem";
 
 export const WishlistPage = () => {
   return (
     <Content>
-      <Header as={"h1"} $color="orange" $size="medium">
-        Wishlist
-      </Header>
-      <SaleSection />
+      <CardFilterGrid>
+        <ProductsCard title="Wishlist" items={2}>
+          <CartItem />
+        </ProductsCard>
+
+        <StyledSidebar $position="sticky" $width="auto" $height="80vh">
+          <ProductFilter />
+        </StyledSidebar>
+        <Stepper searchParamName="page" max={10} />
+      </CardFilterGrid>
     </Content>
   );
 };
