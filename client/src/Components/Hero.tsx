be7b1be22df4905f5ec5ce27891daf1dd6a7d6b9
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-21 12:31:10 +0100

    Update Hero.tsx

--- client/src/Components/Hero.tsx
+++ client/src/Components/Hero.tsx
@@ -7,7 +7,7 @@ const StyledHero = styled.div`
   position: relative;
   background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
     url("https://images.unsplash.com/photo-1483985988355-763728e1935b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
-  height: 110vh;
+  height: 115vh;
   background-position: 0% 50%;
   background-size: 120%;
 
