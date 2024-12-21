3d847509ed8be50aa92f283accc48dc3f5d5c808
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-21 10:50:26 +0100

    UpdateCartPage.tsx

--- client/src/pages/Cart/CartPage.tsx
+++ client/src/pages/Cart/CartPage.tsx
@@ -1,40 +1,20 @@
-import styled from "styled-components";
 import { Content } from "../../Util/Content";
 import { CartCard } from "./CartCard";
 import { StyledSidebar } from "../../Util/StyledSidebar";
 import { ProductFilter } from "../../Util/ProductFilter";
 import { Stepper } from "../../Util/Stepper";
-
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(
-    auto-fit,
-    minmax(60rem, 1fr) minmax(25rem, 35rem)
-  );
-
-  grid-template-rows: min-content 1fr;
-  align-items: start;
-  gap: 2.8rem;
-
-  & > *:nth-child(2) {
-    grid-row: span 2;
-  }
-
-  & > *:last-child {
-    margin-bottom: auto;
-  }
-`;
+import { CardFilterGrid } from "../../Util/CardFilterGrid";
 
 export const CartPage = () => {
   return (
     <Content>
-      <Grid>
+      <CardFilterGrid>
         <CartCard />
         <StyledSidebar $position="sticky" $width="auto" $height="80vh">
           <ProductFilter />
         </StyledSidebar>
         <Stepper searchParamName="page" max={10} />
-      </Grid>
+      </CardFilterGrid>
     </Content>
   );
 };
