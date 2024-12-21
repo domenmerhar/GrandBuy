2d0d2ee77b75f1fbbc251d83e3bd6e5bd53784c0
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-21 10:59:20 +0100

    Add OrdersPage.tsx

---
+++ client/src/pages/orders/OrdersPage.tsx
@@ -0,0 +1,24 @@
+import { CardFilterGrid } from "../../Util/CardFilterGrid";
+import { Content } from "../../Util/Content";
+import { ProductFilter } from "../../Util/ProductFilter";
+import { ProductsCard } from "../../Util/ProductsCard";
+import { Stepper } from "../../Util/Stepper";
+import { CartItem } from "../Cart/CartItem";
+import { StyledSidebar } from "../../Util/StyledSidebar";
+
+export const OrdersPage = () => {
+  return (
+    <Content>
+      <CardFilterGrid>
+        <ProductsCard title="Orders" itemCount={2}>
+          <CartItem />
+        </ProductsCard>
+
+        <StyledSidebar $position="sticky" $width="auto" $height="80vh">
+          <ProductFilter />
+        </StyledSidebar>
+        <Stepper searchParamName="page" max={10} />
+      </CardFilterGrid>
+    </Content>
+  );
+};
