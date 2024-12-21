52d4040bf4d742a5c8e6ab43d18da8d0e2fa40e5
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-21 10:50:08 +0100

    Add CardFilterGrid.tsx

---
+++ client/src/Util/CardFilterGrid.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const CardFilterGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(
+    auto-fit,
+    minmax(60rem, 1fr) minmax(25rem, 35rem)
+  );
+
+  grid-template-rows: min-content 1fr;
+  align-items: start;
+  gap: 2.8rem;
+
+  & > *:nth-child(2) {
+    grid-row: span 2;
+  }
+
+  & > *:last-child {
+    margin-bottom: auto;
+  }
+`;
