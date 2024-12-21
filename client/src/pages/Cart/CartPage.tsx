6a9da09d947b079fcf79a3b93d586370e0666e89
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-21 10:53:28 +0100

    Update CartPage.tsx

--- client/src/pages/Cart/CartPage.tsx
+++ client/src/pages/Cart/CartPage.tsx
@@ -1,15 +1,20 @@
 import { Content } from "../../Util/Content";
-import { CartCard } from "./CartCard";
 import { StyledSidebar } from "../../Util/StyledSidebar";
 import { ProductFilter } from "../../Util/ProductFilter";
 import { Stepper } from "../../Util/Stepper";
 import { CardFilterGrid } from "../../Util/CardFilterGrid";
+import { ProductsCard } from "../../Util/ProductsCard";
+import { CartItem } from "./CartItem";
 
 export const CartPage = () => {
   return (
     <Content>
       <CardFilterGrid>
-        <CartCard />
+        <ProductsCard title="Shopping Cart" itemCount={2}>
+          <CartItem />
+          <CartItem />
+        </ProductsCard>
+
         <StyledSidebar $position="sticky" $width="auto" $height="80vh">
           <ProductFilter />
         </StyledSidebar>
