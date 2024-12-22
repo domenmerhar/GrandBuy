5d80d7f99b3a10fa9285ad39e48615940f6b7e33
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-22 07:57:50 +0100

    Add SidebarLayout.tsx

---
+++ client/src/Util/SidebarLayout.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const SidebarLayout = styled.div`
+  display: flex;
+  gap: 6.4rem;
+  padding-right: 6.4rem;
+
+  & > *:nth-child(3) {
+    padding-top: 2.4rem;
+  }
+`;
