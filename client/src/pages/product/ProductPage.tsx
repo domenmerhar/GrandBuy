146ca6b6e1fb418469f7efaf5225c876161eeab0
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-25 20:51:39 +0100

    Update ProductPage.tsx

--- client/src/pages/product/ProductPage.tsx
+++ client/src/pages/product/ProductPage.tsx
@@ -4,6 +4,7 @@ import { SaleSection } from "../main/SaleSection";
 import { SliderInfoRow } from "./SliderInfoRow";
 import { ProductDescription } from "./ProductDescription";
 import { Column } from "../../Util/Column";
+import { ReviewSection } from "./ReviewSection";
 
 export const ProductPage = () => {
   return (
@@ -11,6 +12,7 @@ export const ProductPage = () => {
       <Column $gap="6.4rem">
         <SliderInfoRow />
         <ProductDescription markdownSrc="http://localhost:3000/files/test.md" />
+        <ReviewSection />
         <MoreFromSellerSection />
         <SaleSection />
       </Column>
