3a8012cf1c0bae791a04163aa998bfe13b82595b
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-26 20:30:58 +0100

    Update InputWithLabel.tsx

--- client/src/Util/InputWithLabel.tsx
+++ client/src/Util/InputWithLabel.tsx
@@ -1,6 +1,6 @@
-import styled from "styled-components";
 import { Input } from "./Input";
 import { FC } from "react";
+import { Label } from "./Label";
 
 interface InputWithLabelProps {
   id: string;
@@ -9,13 +9,6 @@ interface InputWithLabelProps {
   title: string;
   disabled?: boolean;
 }
-
-const Label = styled.label`
-  color: var(--gray-6);
-  font-weight: 600;
-
-  font-size: 1.4rem;
-`;
 
 export const InputWithLabel: FC<InputWithLabelProps> = ({
   id,
