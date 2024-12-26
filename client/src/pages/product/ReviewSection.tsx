53513934fd2a9224c1fe3be927c7654c98579e19
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-26 11:16:15 +0100

    Update ReviewSection.tsx

--- client/src/pages/product/ReviewSection.tsx
+++ client/src/pages/product/ReviewSection.tsx
@@ -1,6 +1,8 @@
+import { useSearchParams } from "react-router-dom";
 import { BlankCard } from "../../Util/BlankCard";
 import { Column } from "../../Util/Column";
 import { HeaderUppercaseBold } from "../../Util/HeaderUppercaseBold";
+import { Modal } from "../../Util/Modal";
 import { Row } from "../../Util/Row";
 import { Select } from "../../Util/Select";
 import { IOption } from "../../Util/types";
@@ -16,25 +18,47 @@ const selectOptions: IOption[] = [
 ];
 
 export const ReviewSection = () => {
+  const [, setSearchParams] = useSearchParams();
+
+  const handleModalClose = () => {
+    setSearchParams((searchParams) => {
+      searchParams.delete("reply");
+      return searchParams;
+    });
+  };
+
   return (
-    <BlankCard>
-      <Column $gap="1.2rem">
-        <Row $justifyContent="space-between" $flexWrap="wrap">
-          <HeaderUppercaseBold>Reviews</HeaderUppercaseBold>
-          <Select options={selectOptions} searchParam="sort" />
-        </Row>
+    <Modal>
+      <BlankCard>
+        <Column $gap="1.2rem">
+          <Row $justifyContent="space-between" $flexWrap="wrap">
+            <HeaderUppercaseBold>Reviews</HeaderUppercaseBold>
+            <Select options={selectOptions} searchParam="sort" />
+          </Row>
 
-        <AverageRating rating={2.3} />
+          <AverageRating rating={2.3} />
 
-        <Row $gap="3.2rem">
-          <RatingBreakdown />
-          <Column $gap="3.2rem">
-            {Array.from({ length: 3 }).map((_, i) => (
-              <Review key={i} />
-            ))}
-          </Column>
-        </Row>
-      </Column>
-    </BlankCard>
+          <Row $gap="3.2rem">
+            <RatingBreakdown />
+            <Column $gap="3.2rem">
+              {Array.from({ length: 3 }).map((_, i) => (
+                <Review key={i} />
+              ))}
+            </Column>
+          </Row>
+        </Column>
+      </BlankCard>
+
+      <Modal.Window
+        title="Reply"
+        onCancelReject={handleModalClose}
+        onBackdropClick={handleModalClose}
+      >
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita
+        possimus nesciunt eveniet at vitae officiis, illum architecto animi
+        atque nemo qui, ab commodi. Beatae, quod mollitia optio tenetur
+        voluptatem molestias.
+      </Modal.Window>
+    </Modal>
   );
 };
