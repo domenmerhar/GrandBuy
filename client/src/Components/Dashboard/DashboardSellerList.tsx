98c3d62d32c24fbad49c86eab541d5a5fad5bcf4
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-27 19:38:24 +0100

    Update DashboardSellerList.tsx

--- client/src/Components/Dashboard/DashboardSellerList.tsx
+++ client/src/Components/Dashboard/DashboardSellerList.tsx
@@ -1,51 +1,34 @@
-import { HiOutlineEye, HiOutlineMicrophone } from "react-icons/hi";
 import { Column } from "../../Util/Column";
-import { NavigationTextButton } from "../../Util/NavigationTextButton";
-import { HiArrowUturnLeft } from "react-icons/hi2";
-import { BiPackage } from "react-icons/bi";
+import styled from "styled-components";
+import { Button } from "../../Util/Button";
+import { SellerNavigationButtons } from "./SellerNavigationButtons";
+import { Modal } from "../../Util/Modal";
+import { AddProductModal } from "./AddProductModal";
+
+const AddProductButton = styled(Button)`
+  align-self: center;
+  margin-top: 2.4rem;
+`;
 
 export const DashboardSellerList = () => {
+  const { setIsOpen } = Modal.useModalContext();
+
+  const handleClick = () => setIsOpen(true);
+
   return (
     <Column $gap="1.2rem">
-      <NavigationTextButton to="overview" variant="dark">
-        <HiOutlineEye size={24} />
-        Overview
-      </NavigationTextButton>
+      <SellerNavigationButtons />
 
-      <NavigationTextButton to="reviews" variant="dark">
-        <HiOutlineMicrophone size={24} />
-        Reviews
-      </NavigationTextButton>
+      <AddProductButton
+        $color="orange"
+        $shape="oval"
+        $size="medium"
+        onClick={handleClick}
+      >
+        Add Product
+      </AddProductButton>
 
-      <NavigationTextButton to="orders" variant="dark">
-        <BiPackage size={24} />
-        Orders
-      </NavigationTextButton>
-
-      <NavigationTextButton to="refund" iconColoring="stroke" variant="dark">
-        <HiArrowUturnLeft size={24} />
-        Refunds
-      </NavigationTextButton>
-
-      <NavigationTextButton to="coupons" variant="dark" iconColoring="stroke">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="size-6"
-          height={24}
-          width={24}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 0 1 0 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 0 1 0-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375Z"
-          />
-        </svg>
-        Coupons
-      </NavigationTextButton>
+      <AddProductModal />
     </Column>
   );
 };
