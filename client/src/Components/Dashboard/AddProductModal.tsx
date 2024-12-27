c7b6f33892e1f56607b686961a8f8a982697d4ed
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-27 19:37:23 +0100

    Add AddProductModal.tsx

---
+++ client/src/Components/Dashboard/AddProductModal.tsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+import { Column } from "../../Util/Column";
+import { InputWithLabel } from "../../Util/InputWithLabel";
+import { Modal } from "../../Util/Modal";
+import { useRef } from "react";
+
+const Content = styled(Column)`
+  & label {
+    margin-bottom: -8px;
+  }
+`;
+
+export const AddProductModal = () => {
+  const productRef = useRef<HTMLInputElement>(null);
+  const priceRef = useRef<HTMLInputElement>(null);
+  const shippingRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <Modal.Window title="Add Product">
+      <Content $gap="1.2rem">
+        <InputWithLabel
+          id="product-name"
+          placeholder="guitar"
+          title="Product name"
+          type="text"
+          ref={productRef}
+        />
+
+        <InputWithLabel
+          id="price"
+          placeholder="10.99"
+          title="Price"
+          type="number"
+          ref={priceRef}
+        />
+
+        <InputWithLabel
+          id="shipping"
+          placeholder="2.99"
+          title="Shipping"
+          type="number"
+          ref={shippingRef}
+        />
+      </Content>
+    </Modal.Window>
+  );
+};
