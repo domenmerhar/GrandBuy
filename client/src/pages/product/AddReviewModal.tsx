6aac33521d8ca442bdee510257b5ee6cccd5ddfe
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-27 11:53:33 +0100

    Update AddReviewModal.tsx

--- client/src/pages/product/AddReviewModal.tsx
+++ client/src/pages/product/AddReviewModal.tsx
@@ -1,24 +1,9 @@
-import styled from "styled-components";
 import { Column } from "../../Util/Column";
-import { Label } from "../../Util/Label";
 import { Modal } from "../../Util/Modal";
 import { RatingInteractive } from "../search/RatingInteractive";
 import { useSearchParams } from "react-router-dom";
 import { useRef } from "react";
-
-const TextareaLabel = styled(Label)`
-  margin-bottom: -4px;
-`;
-
-const Textarea = styled.textarea`
-  border-radius: 15px;
-  border: none;
-  padding: 1rem 1.5rem;
-  font-weight: 400;
-  resize: vertical;
-  min-height: 10rem;
-  max-height: 80%;
-`;
+import { TextareaWithLabel } from "../../Components/TextareaWithLabel";
 
 export const AddReviewModal = () => {
   const { setIsOpen } = Modal.useModalContext();
@@ -46,11 +31,11 @@ export const AddReviewModal = () => {
       <Column $gap="8px" as="form">
         <RatingInteractive size={28} fontSize="1.6rem" />
 
-        <TextareaLabel htmlFor="review">Review</TextareaLabel>
-        <Textarea
-          id="review"
-          placeholder="Write your review here"
-          ref={textAreaRef}
+        <TextareaWithLabel
+          id="content"
+          label="Content"
+          textAreaRef={textAreaRef}
+          placeholder="Beautiful product. I love it!"
         />
       </Column>
     </Modal.Window>
