30bf565cc9be0e58e7e648ec04ca64e5a506877a
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-28 11:02:11 +0100

    Add ProductInfoCard.tsx

---
+++ client/src/pages/addProduct/ProductInfoCard.tsx
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+import { StyledSidebar } from "../../Util/StyledSidebar";
+import { ProductInfo } from "../../Components/ProductInfo";
+import { ButtonWithNotifcations } from "../../Components/ButtonWithNotifcations";
+import { HiPencil } from "react-icons/hi";
+import { Modal } from "../../Util/Modal";
+import { useSearchParams } from "react-router-dom";
+import { ProductInfoModal } from "./ProductInfoModal";
+
+const ProductInfoHolder = styled(StyledSidebar)`
+  align-self: flex-start;
+  position: relative;
+
+  & > div:nth-of-type(1) {
+    position: absolute;
+    top: 2.4rem;
+    right: 2.4rem;
+    display: inline-block;
+
+    & > button {
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+  }
+`;
+
+const createdBy = "test";
+
+export const ProductInfoCard = () => {
+  const [searchParams] = useSearchParams();
+  const { setIsOpen } = Modal.useModalContext();
+
+  const handleClick = () => {
+    setIsOpen(true);
+  };
+
+  return (
+    <>
+      <ProductInfoHolder $position="sticky" $height="500px" $rounded>
+        <ButtonWithNotifcations onClick={handleClick}>
+          <HiPencil size={24} />
+        </ButtonWithNotifcations>
+
+        <ProductInfo
+          title={searchParams.get("title") || ""}
+          price={searchParams.get("price") || ""}
+          shipping={searchParams.get("shipping") || ""}
+          averageRating="0"
+          unitsSold="0"
+          createdBy={createdBy}
+          uploaded={new Date().toLocaleDateString()}
+        />
+      </ProductInfoHolder>
+
+      <ProductInfoModal />
+    </>
+  );
+};
