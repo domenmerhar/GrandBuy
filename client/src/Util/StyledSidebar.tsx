a4f23964a4de7253c76019e75caf3d3f86d377b8
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-28 11:00:36 +0100

    Update StyledSidebar.sx

--- client/src/Util/StyledSidebar.tsx
+++ client/src/Util/StyledSidebar.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 interface StyledSidebarProps {
   $position: string;
+  $rounded?: boolean;
   $width?: string;
   $height?: string;
 }
@@ -13,4 +14,6 @@ export const StyledSidebar = styled.aside<StyledSidebarProps>`
   height: ${({ $height }) => $height || "100vh"};
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   padding: 2.4rem 3.2rem;
+
+  ${({ $rounded }) => $rounded && "border-radius: 15px;"}
 `;
