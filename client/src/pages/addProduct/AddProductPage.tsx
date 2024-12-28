ed6bc4da1fb3fcca9d4610dc764dadc6eac2e633
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-28 19:17:01 +0100

    Update ProductPage.tsx

--- client/src/pages/addProduct/AddProductPage.tsx
+++ client/src/pages/addProduct/AddProductPage.tsx
@@ -6,18 +6,27 @@ import { useState } from "react";
 import { CarouselUpload } from "./CarouselUpload";
 import { MarkdownFileUploader } from "./MarkdownFileUploader";
 import styled from "styled-components";
+import { Button } from "../../Util/Button";
 
 const CarouselCardRow = styled(Row)`
   margin-bottom: 6.4rem;
 `;
 
+const CreateButton = styled(Button)`
+  margin-left: auto;
+`;
+
+const AddProductPageContent = styled(Content)`
+  display: flex;
+  flex-direction: column;
+  gap: 6.4rem;
+`;
+
 export const AddProductPage = () => {
   const [selectedImages, setSelectedImages] = useState<string[]>([]);
 
-  console.log(selectedImages);
-
   return (
-    <Content>
+    <AddProductPageContent>
       <CarouselCardRow $gap="2.8rem" $alignItems="center">
         <CarouselUpload
           selectedImages={selectedImages}
@@ -30,6 +39,9 @@ export const AddProductPage = () => {
       </CarouselCardRow>
 
       <MarkdownFileUploader />
-    </Content>
+      <CreateButton $shape="oval" $color="orange" $size="large">
+        Create
+      </CreateButton>
+    </AddProductPageContent>
   );
 };
