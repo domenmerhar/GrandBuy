025863bf5f01e2a5fa57fe880c8cc5d5f503bab8
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-28 11:33:21 +0100

    Update AddProductPage.tsx

--- client/src/pages/addProduct/AddProductPage.tsx
+++ client/src/pages/addProduct/AddProductPage.tsx
@@ -3,10 +3,10 @@ import { Row } from "../../Util/Row";
 import { ImageCarousel } from "../product/ImageCarousel";
 import { Modal } from "../../Util/Modal";
 import { ProductInfoCard } from "./ProductInfoCard";
-import { ImagePicker } from "../../Components/ImagePicker";
 import { Column } from "../../Util/Column";
 import { useState } from "react";
 import styled from "styled-components";
+import { FilePicker } from "../../Components/FilePicker";
 
 const ImagesColumn = styled(Column)`
   width: 100%;
@@ -20,7 +20,7 @@ export const AddProductPage = () => {
       <Row $gap="2.8rem" $alignItems="center">
         <ImagesColumn $gap="1.2rem">
           <ImageCarousel images={selectedImages} />
-          <ImagePicker setSelectedImages={setSelectedImages} />
+          <FilePicker setSelectedFiles={setSelectedImages} multiple />
         </ImagesColumn>
 
         <Modal>
