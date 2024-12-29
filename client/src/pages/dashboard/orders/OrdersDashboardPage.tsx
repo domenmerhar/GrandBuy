94ce7521db739b421344b03bca4146af06f18c1b
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-29 10:44:06 +0100

    Update OrdersDashboardPage.tsx

--- client/src/pages/dashboard/orders/OrdersDashboardPage.tsx
+++ client/src/pages/dashboard/orders/OrdersDashboardPage.tsx
@@ -1,5 +1,8 @@
+import { Button } from "../../../Util/Button";
 import { FilterSortHeader } from "../../../Util/FilterSortHeader";
 import { Stepper } from "../../../Util/Stepper";
+import { Table } from "../../../Components/Table";
+import { Badge } from "../../../Util/Badge";
 
 const filterOptions = [
   { value: "all", name: "All" },
@@ -15,6 +18,8 @@ const selectOptions = [
   { value: "newest", name: "Sort by age (newest)" },
 ];
 
+const headers = ["Username", "Quantity", "Product", "Status", ""];
+
 export const OrdersDashboardPage = () => {
   return (
     <>
@@ -23,6 +28,44 @@ export const OrdersDashboardPage = () => {
         filterOptions={filterOptions}
         selectOptions={selectOptions}
       />
+
+      <Table headers={headers}>
+        <Table.Row>
+          <td>John Doe</td>
+          <td>2</td>
+          <td>Product 1</td>
+          <td>
+            <Badge $color="green" $size="small">
+              Shipped
+            </Badge>
+          </td>
+          <td></td>
+        </Table.Row>
+
+        <Table.Row>
+          <td>John Doe</td>
+          <td>2</td>
+          <td>Product 1</td>
+          <td>
+            <Badge $color="red" $size="small">
+              Cancelled
+            </Badge>
+          </td>
+          <td></td>
+        </Table.Row>
+
+        <Table.Row>
+          <td>John Doe</td>
+          <td>2</td>
+          <td>Product 1</td>
+          <td>Pending</td>
+          <td>
+            <Button $color="orange" $shape="oval" $size="small">
+              Respond
+            </Button>
+          </td>
+        </Table.Row>
+      </Table>
 
       <Stepper searchParamName="page" />
     </>
