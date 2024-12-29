2929c92742cda26f664129a4e07f60c6f11dcf69
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-29 10:16:24 +0100

    Update OrdersDashboardPage.tsx

--- client/src/pages/dashboard/orders/OrdersDashboardPage.tsx
+++ client/src/pages/dashboard/orders/OrdersDashboardPage.tsx
@@ -1,5 +1,30 @@
-import React from "react";
+import { FilterSortHeader } from "../../../Util/FilterSortHeader";
+import { Stepper } from "../../../Util/Stepper";
+
+const filterOptions = [
+  { value: "all", name: "All" },
+  { value: "pending", name: "Pending" },
+  { value: "shipped", name: "Shipped" },
+  { value: "cancelled", name: "Cancelled" },
+  { value: "delivered", name: "Delivered" },
+];
+
+const selectOptions = [
+  { value: "all", name: "All" },
+  { value: "oldest", name: "Sort by age (oldest)" },
+  { value: "newest", name: "Sort by age (newest)" },
+];
 
 export const OrdersDashboardPage = () => {
-  return <div>OrdersDashboardPage</div>;
+  return (
+    <>
+      <FilterSortHeader
+        headerText="Orders"
+        filterOptions={filterOptions}
+        selectOptions={selectOptions}
+      />
+
+      <Stepper searchParamName="page" />
+    </>
+  );
 };
