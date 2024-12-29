9fc027e068767ed73f3d919062c14647f35ae881
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-29 20:06:11 +0100

    Add DashboardRefunds.tsx

---
+++ client/src/pages/dashboard/refund/DashboardRefunds.tsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { Modal } from "../../../Util/Modal";
+import { Row } from "../../../Util/Row";
+import { RefundCard } from "../../refund/RefundCard";
+import { RefundModal } from "./RefundModal";
+export const DashboardRefunds = () => {
+  const { setIsOpen } = Modal.useModalContext();
+
+  useEffect(() => {
+    setIsOpen(true);
+  }, [setIsOpen]);
+
+  return (
+    <Row
+      $gap="3.2rem"
+      $justifyContent="center"
+      $alignItems="flex-start"
+      $flexWrap="wrap"
+    >
+      <RefundCard />
+      <RefundCard />
+      <RefundCard />
+      <RefundCard />
+
+      <RefundModal />
+    </Row>
+  );
+};
