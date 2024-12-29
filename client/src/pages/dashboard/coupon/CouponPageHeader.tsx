d4d56cd6ae2391810908eb3f55717163b943388c
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-29 20:20:40 +0100

    Add CouponPageHeader.tsx

---
+++ client/src/pages/dashboard/coupon/CouponPageHeader.tsx
@@ -0,0 +1,36 @@
+import { HiOutlinePlus } from "react-icons/hi";
+import { Header } from "../../../Util/Header";
+import { Row } from "../../../Util/Row";
+import { Select } from "../../../Util/Select";
+import { SquareButton } from "../../../Util/SquareButton";
+
+const selectOptions = [
+  { value: "newest", name: "Sort by age (newest)" },
+  { value: "oldest", name: "Sort by age (oldest)" },
+  { value: "highest", name: "Sort by highest discount" },
+  { value: "lowest", name: "Sort by lowest discount" },
+  { value: "all", name: "All" },
+];
+
+export const CouponPageHeader = () => {
+  return (
+    <>
+      <Row
+        $justifyContent="space-between"
+        $flexWrap="wrap"
+        $alignItems="center"
+      >
+        <Header as="h1" $color="orange" $size="medium">
+          Coupons
+        </Header>
+
+        <Row $gap="1.6rem" $alignItems="center" $flexWrap="wrap">
+          <Select searchParam="sort" options={selectOptions} />
+          <SquareButton $color="orange" $size="medium">
+            <HiOutlinePlus size={32} color="#f1f3f5" />
+          </SquareButton>
+        </Row>
+      </Row>
+    </>
+  );
+};
