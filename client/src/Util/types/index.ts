ce9b91b7dc6331b86e6223326fb70fe7ebd7c9a2
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-29 11:06:25 +0100

    Update types/index.ts

--- client/src/Util/types/index.ts
+++ client/src/Util/types/index.ts
@@ -22,3 +22,12 @@ export interface BadgeProps {
   $color: "orange" | "red" | "green" | "yellow";
   $size?: "medium" | "small";
 }
+
+export type OrderStatus = "shipped" | "cancelled" | "pending";
+export interface IOrderTable {
+  username: string;
+  orderID: string;
+  quantity: number;
+  product: string;
+  status: OrderStatus;
+}
