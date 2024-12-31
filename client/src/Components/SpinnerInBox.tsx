ae365d204af82aed987b01ef230b91cb0ad92fdb
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-31 10:20:00 +0100

    Add SpinnerInBox.tsx

--- client/src/Components/SpinnerInBox.tsx
+++ client/src/Components/SpinnerInBox.tsx
@@ -11,8 +11,7 @@ const StyledSpinnerInBox = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 100%;
-  height: 100%;
+  height: 80dvh;
 `;
 
 export const SpinnerInBox: FC<SpinnerInBoxProps> = ({
