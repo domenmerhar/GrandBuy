0dcc8233ed34ab143f72bf8efe9b7ac0698871c2
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-31 10:06:36 +0100

    Update ProductPage.tsx

--- client/src/pages/product/ProductPage.tsx
+++ client/src/pages/product/ProductPage.tsx
@@ -5,17 +5,74 @@ import { SliderInfoRow } from "./SliderInfoRow";
 import { ProductDescription } from "./ProductDescription";
 import { Column } from "../../Util/Column";
 import { ReviewSection } from "./ReviewSection";
+import { useProduct } from "./useProduct";
+import { SpinnerInBox } from "../../Components/SpinnerInBox";
+import { toPrice } from "../../functions/toPrice";
+import { toDate } from "../../functions/toDate";
+import { toApiFilesPath } from "../../functions/toApiFilesPath";
+
+interface IProductDetails {
+  isSelling: boolean;
+  _id: string;
+  user: string;
+  name: string;
+  coverImage: string;
+  images: string[];
+  price: number;
+  shipping: number;
+  discount: number;
+  description: string;
+  lastChanged: string;
+  totalPrice: number;
+  orders: number;
+}
 
 export const ProductPage = () => {
+  const { data, isLoading, error } = useProduct();
+
+  if (isLoading)
+    return (
+      <Content>
+        <SpinnerInBox />
+      </Content>
+    );
+
+  const {
+    description,
+    images,
+    lastChanged,
+    name,
+    orders,
+    price,
+    shipping,
+    user,
+  } = data.data.product as IProductDetails;
+
+  const markdownPath = toApiFilesPath(description);
+  const imagesPath = images.map((image) => toApiFilesPath(image));
+
+  console.log(imagesPath);
+
   return (
-    <Content>
-      <Column $gap="6.4rem">
-        <SliderInfoRow />
-        <ProductDescription markdownSrc="http://localhost:3000/files/test.md" />
-        <ReviewSection />
-        <MoreFromSellerSection />
-        <SaleSection />
-      </Column>
-    </Content>
+    <>
+      <Content>
+        <Column $gap="6.4rem">
+          <SliderInfoRow
+            images={imagesPath}
+            title={name}
+            averageRating="N/A"
+            createdBy={user}
+            uploaded={toDate(lastChanged)}
+            price={toPrice(price, "USD")}
+            shipping={toPrice(shipping, "USD")}
+            unitsSold={String(orders)}
+          />
+          <ProductDescription markdownSrc={markdownPath} />
+          <ReviewSection />
+          <MoreFromSellerSection />
+          <SaleSection />
+        </Column>
+      </Content>
+    </>
   );
 };
