09c1793da6c2059ebb7d60dc573e18cc29db7db2
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-31 10:47:54 +0100

    Add getSellerProducts.ts

---
+++ client/src/api/getSellerProducts.ts
@@ -0,0 +1,9 @@
+import { toApiPath } from "../functions/toApiPath";
+
+export const getSellerProducts = async (id: string, page = 1) => {
+  const response = await fetch(
+    toApiPath(`/product/seller/${id}?limit=20&page=${page}`)
+  );
+  const data = await response.json();
+  return data;
+};
