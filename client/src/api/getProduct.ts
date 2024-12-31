29a67ecd4a42af8b36ba619482a494a5d6d2626c
Domen Merhar <domen.merhar@students.vegova.si>
2024-12-31 10:36:33 +0100

    Update getProduct.ts

--- client/src/api/getProduct.ts
+++ client/src/api/getProduct.ts
@@ -1,7 +1,7 @@
+import { toApiPath } from "../functions/toApiPath";
+
 export const getProduct = async (id: string) => {
-  const response = await fetch(
-    `${import.meta.env.VITE_API_LINK}/product/${id}`
-  );
+  const response = await fetch(toApiPath(`product/${id}`));
   const data = await response.json();
   return data;
 };
