92142b40e589a05c22b69f66d00b440915269995
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-01 13:58:16 +0100

    Update getProducts.ts

--- client/src/api/getProducts.ts
+++ client/src/api/getProducts.ts
@@ -1,8 +1,35 @@
 import { toApiPath } from "../functions/toApiPath";
 
-export const getProducts = async (query: string, page: number) => {
+export const getProducts = async ({
+  query,
+  page,
+  freeShipping,
+  sale,
+  from,
+  to,
+  rating,
+}: {
+  query: string;
+  page: number;
+  freeShipping?: boolean;
+  sale?: boolean;
+  rating?: number;
+  from?: number;
+  to?: number;
+}) => {
+  const queryParamsStr = [
+    freeShipping && "shipping[lte]=0",
+    sale && "discount[gt]=0",
+    from && `totalPrice[gte]=${from}`,
+    to && `totalPrice[lte]=${to}`,
+  ]
+    .filter((param) => !!param)
+    .join("&");
+
+  const queryParams = queryParamsStr ? `&${queryParamsStr}` : "";
+
   const response = await fetch(
-    toApiPath(`product?search=${query}&page=${page}`)
+    toApiPath(`product?search=${query}&page=${page}${queryParams}`)
   );
 
   const data = await response.json();
