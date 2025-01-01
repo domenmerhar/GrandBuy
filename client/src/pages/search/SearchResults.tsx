7ebf89ba44a1cc4bfee13e3251addb3f15167a9f
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-01 13:58:36 +0100

    Update SearchResults.tsx

--- client/src/pages/search/SearchResults.tsx
+++ client/src/pages/search/SearchResults.tsx
@@ -3,17 +3,36 @@ import { ProductCard } from "../../Util/ProductCard";
 import { IProductShort } from "../../Util/types";
 import { getProducts } from "../../api/getProducts";
 import { useQuery } from "@tanstack/react-query";
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import { SpinnerInBox } from "../../Components/SpinnerInBox";
 import { ErrorBox } from "../../Components/ErrorBox";
 import { toApiFilesPath } from "../../functions/toApiFilesPath";
 
 export const SearchResults = () => {
   const { query } = useParams();
+  const [searchParams] = useSearchParams();
+
+  const [from, to, freeShipping, sale, rating] = [
+    Number(searchParams.get("from")),
+    Number(searchParams.get("to")),
+    searchParams.get("free-shipping") === "true",
+    searchParams.get("sale") === "true",
+    Number(searchParams.get("rating")),
+  ];
 
   const { data, isLoading, error } = useQuery({
-    queryKey: ["products", query],
-    queryFn: () => getProducts(query!, 1),
+    queryKey: ["products", query, 1, from, to, freeShipping, sale, rating],
+    queryFn: () => {
+      return getProducts({
+        query: String(query),
+        page: 1,
+        from,
+        to,
+        freeShipping,
+        sale,
+        rating,
+      });
+    },
   });
 
   if (isLoading) return <SpinnerInBox fullPage={false} />;
