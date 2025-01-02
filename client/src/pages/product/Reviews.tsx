f402df542838f71a71b9990e19f199e40f6a38d1
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-02 13:18:15 +0100

    Update Reviews.tsx

--- client/src/pages/product/Reviews.tsx
+++ client/src/pages/product/Reviews.tsx
@@ -1,32 +1,16 @@
 import styled from "styled-components";
 import { Column } from "../../Util/Column";
 import { Review } from "./Review";
-import { useParams, useSearchParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
-import { getReviews } from "../../functions/getReviews";
 import { SpinnerInBox } from "../../Components/SpinnerInBox";
 import { ErrorBox } from "../../Components/ErrorBox";
-import { ReviewSort } from "../../Util/types";
+import { useReviews } from "./useReviews";
 
 const StyledReviews = styled(Column)`
   overflow-y: auto;
 `;
 
-const page = 1;
-
 export const Reviews = () => {
-  const { productId } = useParams();
-  const [searchParams] = useSearchParams();
-
-  const { data, isLoading, error } = useQuery({
-    queryKey: ["reviews", productId, page, searchParams.get("sort")],
-    queryFn: () =>
-      getReviews({
-        productId: productId!,
-        page,
-        sort: (searchParams.get("sort") as ReviewSort) || "-likesCount",
-      }),
-  });
+  const { data, isLoading, error } = useReviews();
 
   if (isLoading) return <SpinnerInBox size="large" />;
   if (error) return <ErrorBox fullPage={false} />;
