33ee4a5112a0c60b81bf0d72e75f49e410dc4226
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-02 13:17:39 +0100

    Add useReviews.ts

---
+++ client/src/pages/product/useReviews.ts
@@ -0,0 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import { useParams, useSearchParams } from "react-router-dom";
+import { ReviewSort } from "../../Util/types";
+import { getReviews } from "../../functions/getReviews";
+
+export const useReviews = () => {
+  const { productId } = useParams();
+  const [searchParams] = useSearchParams();
+
+  const page = Number(searchParams.get("page")) || 1;
+
+  const sort = (searchParams.get("sort") as ReviewSort) || "-likesCount";
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["reviews", productId, page, sort],
+    queryFn: () =>
+      getReviews({
+        productId: productId!,
+        page,
+        sort,
+      }),
+  });
+
+  return { data, isLoading, error };
+};
