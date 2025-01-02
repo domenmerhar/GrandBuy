5fdcc484e3159fb5090aa46b8d52cb1e5bcb37da
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-02 11:06:56 +0100

    Update RatingBreakdown.tsx

--- client/src/pages/product/RatingBreakdown.tsx
+++ client/src/pages/product/RatingBreakdown.tsx
@@ -1,31 +1,48 @@
 import { Column } from "../../Util/Column";
 import { ProgressWithLabel } from "../../Components/ProgressWithLabel";
 import { AverageRating } from "./AverageRating";
+import { SpinnerInBox } from "../../Components/SpinnerInBox";
+import { ErrorBox } from "../../Components/ErrorBox";
+import { useReviewStats } from "./useReviewStats";
+
+interface IRatingBreakdown {
+  rating: number;
+  count: number;
+  percentage: number;
+}
 
 export const RatingBreakdown = () => {
+  const { data, isLoading, error } = useReviewStats();
+
+  if (isLoading) return <SpinnerInBox size="large" />;
+
+  if (error) return <ErrorBox fullPage={false} />;
+
+  const averageRating = data.data.overallStats.avgRating;
+
+  const ratingBreakdowns: IRatingBreakdown[] = Array.from({ length: 5 })
+    .map(
+      (_, i) =>
+        data.data.ratingBreakdown.find(
+          (ratingBreakdown: IRatingBreakdown) =>
+            ratingBreakdown.rating === i + 1
+        ) || { rating: i + 1, count: 0, percentage: 0 }
+    )
+    .reverse();
+
   return (
     <Column $gap=".8rem">
-      <AverageRating rating={2.3} />
+      <AverageRating rating={averageRating} />
 
-      <ProgressWithLabel value={10} max={100}>
-        5 stars (10%)
-      </ProgressWithLabel>
-
-      <ProgressWithLabel value={30} max={100}>
-        4 stars (30%)
-      </ProgressWithLabel>
-
-      <ProgressWithLabel value={30} max={100}>
-        3 stars (30%)
-      </ProgressWithLabel>
-
-      <ProgressWithLabel value={20} max={100}>
-        2 stars (20%)
-      </ProgressWithLabel>
-
-      <ProgressWithLabel value={10} max={100}>
-        1 stars (10%)
-      </ProgressWithLabel>
+      {ratingBreakdowns.map((ratingBreakdown) => (
+        <ProgressWithLabel
+          key={ratingBreakdown.rating}
+          value={ratingBreakdown.percentage}
+          max={100}
+        >
+          {`${ratingBreakdown.rating} stars ${ratingBreakdown.percentage}%`}
+        </ProgressWithLabel>
+      ))}
     </Column>
   );
 };
