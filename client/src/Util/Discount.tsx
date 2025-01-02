96ae19bc9713c1bb7b22bce7563b6719ea7baa6c
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-02 10:37:11 +0100

    Add Discount.tsx

---
+++ client/src/Util/Discount.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Discount = styled.p`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  position: absolute;
+  right: -25px;
+  top: -25px;
+
+  background-color: var(--red);
+  color: var(--gray-0);
+
+  rotate: 30deg;
+  height: 5rem;
+  width: 5rem;
+  border-radius: 50px;
+
+  font-weight: 600;
+`;
