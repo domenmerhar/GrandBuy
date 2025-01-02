f6a9c5c9636e04e78198424541c66ad34e1de991
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-02 11:06:26 +0100

    Add getProductReviewStats.ts

---
+++ client/src/api/getProductReviewStats.ts
@@ -0,0 +1,8 @@
+import { toApiPath } from "../functions/toApiPath";
+
+export const getProductReviewStats = async (productId: string) => {
+  const response = await fetch(toApiPath(`review/product/${productId}/stats`));
+  const data = await response.json();
+
+  return data;
+};
