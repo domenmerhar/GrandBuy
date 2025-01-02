3b6089e8cb75b5d8138032dd945176e739a6cdbd
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-02 11:08:41 +0100

    Update RatingBreakdown.tsx

--- client/src/pages/product/RatingBreakdown.tsx
+++ client/src/pages/product/RatingBreakdown.tsx
@@ -5,30 +5,13 @@ import { SpinnerInBox } from "../../Components/SpinnerInBox";
 import { ErrorBox } from "../../Components/ErrorBox";
 import { useReviewStats } from "./useReviewStats";
 
-interface IRatingBreakdown {
-  rating: number;
-  count: number;
-  percentage: number;
-}
-
 export const RatingBreakdown = () => {
-  const { data, isLoading, error } = useReviewStats();
+  const { isLoading, error, averageRating, ratingBreakdowns } =
+    useReviewStats();
 
   if (isLoading) return <SpinnerInBox size="large" />;
 
   if (error) return <ErrorBox fullPage={false} />;
-
-  const averageRating = data.data.overallStats.avgRating;
-
-  const ratingBreakdowns: IRatingBreakdown[] = Array.from({ length: 5 })
-    .map(
-      (_, i) =>
-        data.data.ratingBreakdown.find(
-          (ratingBreakdown: IRatingBreakdown) =>
-            ratingBreakdown.rating === i + 1
-        ) || { rating: i + 1, count: 0, percentage: 0 }
-    )
-    .reverse();
 
   return (
     <Column $gap=".8rem">
