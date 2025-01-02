3bd1c2e3a9562b145bd0743e8202211d9872c6cc
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-02 20:30:59 +0100

    Update Reply.tsx

--- client/src/pages/product/Reply.tsx
+++ client/src/pages/product/Reply.tsx
@@ -1,22 +1,24 @@
 import styled from "styled-components";
 import { Column } from "../../Util/Column";
 import { UsernameIcon } from "./UsernameIcon";
+import { FC } from "react";
 
 const StyledReply = styled(Column)`
   margin-left: 2rem;
   margin-top: -8px;
 `;
 
-export const Reply = () => {
+interface ReplyProps {
+  username: string;
+  icon: string;
+  content: string;
+}
+
+export const Reply: FC<ReplyProps> = ({ username, icon, content }) => {
   return (
     <StyledReply $gap="8px">
-      <UsernameIcon />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
-        accusamus, fugit consequuntur aliquid vitae illum. Fuga sed minima,
-        delectus cumque laborum a commodi. Natus nisi, sequi sunt praesentium ad
-        nemo.
-      </p>
+      <UsernameIcon username={username} icon={icon} />
+      <p>{content}</p>
     </StyledReply>
   );
 };
