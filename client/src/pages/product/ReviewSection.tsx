23cebe1b18e5c53b196f5a187618146233088814
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-02 11:40:45 +0100

    Update ReviewSection.tsx

--- client/src/pages/product/ReviewSection.tsx
+++ client/src/pages/product/ReviewSection.tsx
@@ -1,23 +1,18 @@
 import { BlankCard } from "../../Util/BlankCard";
-import { Column } from "../../Util/Column";
 import { Modal } from "../../Util/Modal";
 import { Row } from "../../Util/Row";
-import { Review } from "./Review";
 import { RatingBreakdown } from "./RatingBreakdown";
 import styled from "styled-components";
 import { AddReviewButton } from "./AddReviewButton";
 import { ReviewSectionHeader } from "./ReviewSectionHeader";
 import { ReplyModal } from "./ReplyModal";
+import { Reviews } from "./Reviews";
 
 const StyledReviewSection = styled(BlankCard)`
   height: 800px;
   display: flex;
   flex-direction: column;
   gap: 1.2rem;
-`;
-
-const Reviews = styled(Column)`
-  overflow-y: auto;
 `;
 
 const RatingReviewHolder = styled(Row)`
@@ -35,11 +30,7 @@ export const ReviewSection = () => {
           <RatingBreakdown />
 
           <Modal>
-            <Reviews $gap="3.2rem">
-              {Array.from({ length: 5 }).map((_, i) => (
-                <Review key={i} />
-              ))}
-            </Reviews>
+            <Reviews />
 
             <ReplyModal />
           </Modal>
