620860db57c5feae011910b6e68b1399e77d6554
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-03 21:09:15 +0100

    Add Under50.tsx

---
+++ client/src/pages/main/Under50.tsx
@@ -0,0 +1,46 @@
+import { useQuery } from "@tanstack/react-query";
+import { getProducts } from "../../api/getProducts";
+import { SpinnerInBox } from "../../Components/SpinnerInBox";
+import { ErrorBox } from "../../Components/ErrorBox";
+import { CardMultipleItems } from "./CardMultipleItems";
+import { IProductShort } from "../../Util/types/index";
+import { toPrice } from "../../functions/toPrice";
+import { toApiFilesPath } from "../../functions/toApiFilesPath";
+
+export const Under50 = () => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["products-free-shipping"],
+    queryFn: () =>
+      getProducts({
+        query: "",
+        page: 1,
+        to: 50,
+        sort: "-totalPrice",
+        limit: 2,
+      }),
+  });
+
+  if (isLoading) return <SpinnerInBox fullPage={false} />;
+  if (error) return <ErrorBox fullPage={false} />;
+  if (data?.status === "fail") return null;
+
+  return (
+    <CardMultipleItems title="Under $50">
+      {data?.data?.products?.map(
+        ({ _id, coverImage, discount, name, totalPrice }: IProductShort) => (
+          <CardMultipleItems.Product
+            key={_id}
+            id={_id}
+            productName={name}
+            imageSrc={toApiFilesPath(coverImage)}
+            priceAfterDiscount={toPrice(totalPrice, "USD")}
+            priceBeforeDiscount={toPrice(
+              totalPrice / ((100 - discount) / 100),
+              "USD"
+            )}
+          />
+        )
+      )}
+    </CardMultipleItems>
+  );
+};
