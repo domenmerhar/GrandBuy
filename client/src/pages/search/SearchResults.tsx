3910e712a6da76dacfca81645f27c89216822603
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-03 09:09:44 +0100

    Update SearchResults.tsx

--- client/src/pages/search/SearchResults.tsx
+++ client/src/pages/search/SearchResults.tsx
@@ -2,15 +2,24 @@ import { Grid } from "../../Util/Grid";
 import { ProductCard } from "../../Util/ProductCard";
 import { IProductShort } from "../../Util/types";
 import { getProducts } from "../../api/getProducts";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { useParams, useSearchParams } from "react-router-dom";
 import { SpinnerInBox } from "../../Components/SpinnerInBox";
 import { ErrorBox } from "../../Components/ErrorBox";
 import { toApiFilesPath } from "../../functions/toApiFilesPath";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+import styled from "styled-components";
+
+const Div = styled.div`
+  height: 200px;
+`;
 
 export const SearchResults = () => {
   const { query } = useParams();
   const [searchParams] = useSearchParams();
+
+  const { ref, inView } = useInView({ threshold: 0.5 });
 
   const [from, to, freeShipping, sale, rating] = [
     Number(searchParams.get("from")),
@@ -20,56 +29,78 @@ export const SearchResults = () => {
     Number(searchParams.get("rating")),
   ];
 
-  const { data, isLoading, error } = useQuery({
-    queryKey: [
-      "products-search",
-      query,
-      1,
-      from,
-      to,
-      freeShipping,
-      sale,
-      rating,
-    ],
-    queryFn: () => {
-      return getProducts({
-        query: String(query),
-        page: 1,
+  const { data, isLoading, error, fetchNextPage, isFetching } =
+    useInfiniteQuery({
+      queryKey: [
+        "products-search",
+        query,
         from,
         to,
         freeShipping,
         sale,
         rating,
-      });
-    },
-  });
+      ],
+      queryFn: ({ pageParam }) => {
+        if (pageParam === null) return;
+
+        return getProducts({
+          query: String(query),
+          page: Number(pageParam),
+          from,
+          to,
+          freeShipping,
+          sale,
+          rating,
+        });
+      },
+      getNextPageParam: (lastPage) => lastPage?.nextItem || null,
+      initialPageParam: 1,
+    });
+
+  useEffect(() => {
+    if (inView && !isFetching) {
+      console.log("fetching next page");
+      fetchNextPage();
+    }
+  }, [ref, inView, fetchNextPage, isFetching]);
 
   if (isLoading) return <SpinnerInBox fullPage={false} />;
   if (error) return <ErrorBox fullPage={false} />;
 
   return (
-    <Grid
-      $maxWidth="27rem"
-      $minWidth="27rem"
-      $colGap="3.2rem"
-      $rowGap="3.2rem"
-      $margin="4.8rem 0"
-    >
-      {!isLoading &&
-        !error &&
-        data.data &&
-        data.data.products.map(
-          ({ _id, name, coverImage, discount, totalPrice }: IProductShort) => (
-            <ProductCard
-              key={_id}
-              id={_id}
-              title={name}
-              image={toApiFilesPath(coverImage)}
-              discount={discount}
-              price={totalPrice}
-            />
-          )
-        )}
-    </Grid>
+    <>
+      <Grid
+        $maxWidth="27rem"
+        $minWidth="27rem"
+        $colGap="3.2rem"
+        $rowGap="3.2rem"
+        $margin="4.8rem 0"
+      >
+        {!isLoading &&
+          !error &&
+          data?.pages?.map((page) =>
+            page?.data?.products?.map(
+              ({
+                _id,
+                name,
+                coverImage,
+                discount,
+                totalPrice,
+              }: IProductShort) => (
+                <ProductCard
+                  key={_id}
+                  id={_id}
+                  title={name}
+                  image={toApiFilesPath(coverImage)}
+                  discount={discount}
+                  price={totalPrice}
+                />
+              )
+            )
+          )}
+      </Grid>
+      {isFetching && <SpinnerInBox fullPage={false} />}
+      <Div ref={ref}></Div>
+    </>
   );
 };
