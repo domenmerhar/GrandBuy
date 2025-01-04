e0e4809750f2dd05e36cd6bad8af74addc6ec1ea
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-04 09:09:25 +0100

    Add ProductGrid.tsx

--- client/src/Util/ProductGrid.tsx
+++ client/src/Util/ProductGrid.tsx
@@ -5,8 +5,6 @@ export const ProductGrid = styled.div`
   grid-template-columns: repeat(auto-fit, minmax(27rem, 27rem));
   justify-content: space-around;
 
-  max-width: 27rem;
-  min-width: 27rem;
   column-gap: 3.2rem;
   row-gap: 3.2rem;
   margin: 4.8rem 0;
