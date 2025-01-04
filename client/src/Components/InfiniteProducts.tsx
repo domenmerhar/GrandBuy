395ffb410d676325901c89d20e621155434efdf1
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-04 09:10:15 +0100

    Add InfiniteProducts.tsx

---
+++ client/src/Components/InfiniteProducts.tsx
@@ -0,0 +1,54 @@
+import { forwardRef } from "react";
+import { SpinnerInBox } from "./SpinnerInBox";
+import { ErrorBox } from "./ErrorBox";
+import { ProductGrid } from "../Util/ProductGrid";
+import { IProductShort } from "../Util/types";
+import { ProductCard } from "../Util/ProductCard";
+import { toApiFilesPath } from "../functions/toApiFilesPath";
+import { InfiniteDiv } from "../Util/InfiniteDiv";
+
+interface InfiniteProductsProps {
+  data: unknown;
+  isLoading: boolean;
+  error: unknown;
+  isFetching: boolean;
+}
+
+export const InfiniteProducts = forwardRef<
+  HTMLDivElement,
+  InfiniteProductsProps
+>(({ data, isLoading, isFetching, error }, ref) => {
+  if (isLoading) return <SpinnerInBox fullPage={false} />;
+  if (error) return <ErrorBox fullPage={false} />;
+
+  return (
+    <>
+      <ProductGrid>
+        {!isLoading &&
+          !error &&
+          data?.pages?.map((page) =>
+            page?.data?.products?.map(
+              ({
+                _id,
+                name,
+                coverImage,
+                discount,
+                totalPrice,
+              }: IProductShort) => (
+                <ProductCard
+                  key={_id}
+                  id={_id}
+                  title={name}
+                  image={toApiFilesPath(coverImage)}
+                  discount={discount}
+                  price={totalPrice}
+                />
+              )
+            )
+          )}
+      </ProductGrid>
+      {isFetching && <SpinnerInBox fullPage={false} />}
+      <InfiniteDiv ref={ref}></InfiniteDiv>
+    </>
+  );
+});
