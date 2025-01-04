5f28d4e2585b021111cb80da3ca40f2f69587cd1
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-04 10:23:15 +0100

    Update SearchResults.tsx

--- client/src/pages/search/SearchResults.tsx
+++ client/src/pages/search/SearchResults.tsx
@@ -6,13 +6,16 @@ export const SearchResults = () => {
   const { query } = useParams();
   const [searchParams] = useSearchParams();
 
-  const [from, to, freeShipping, sale, rating] = [
+  const [from, to, freeShipping, sale, rating, sort] = [
     Number(searchParams.get("from")),
     Number(searchParams.get("to")),
     searchParams.get("free-shipping") === "true",
     searchParams.get("sale") === "true",
     Number(searchParams.get("rating")),
+    searchParams.get("sort") || "-orders",
   ];
+
+  console.log(searchParams.get("sort"));
 
   const { data, isLoading, isFetching, error, ref } = useProductsInfinite({
     queryName: "products-search",
@@ -22,6 +25,7 @@ export const SearchResults = () => {
     freeShipping,
     sale,
     rating,
+    sort,
   });
 
   return (
