0534c187e96f3fe93af38781d405de4839715bf4
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-04 07:50:50 +0100

    Add LatestProducts.tsx

---
+++ client/src/pages/main/LatestProducts.tsx
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import { getProducts } from "../../api/getProducts";
+import { SpinnerInBox } from "../../Components/SpinnerInBox";
+import { ErrorBox } from "../../Components/ErrorBox";
+import HorizontalProducts from "./HorizontalProducts";
+
+export const LatestProducts = () => {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["products-latest"],
+    queryFn: () =>
+      getProducts({
+        query: "",
+        page: 1,
+        limit: 20,
+        sort: "-createdAt",
+      }),
+  });
+
+  if (isLoading) return <SpinnerInBox fullPage={false} />;
+  if (error) return <ErrorBox fullPage={false} />;
+  if (data?.status === "fail") return null;
+
+  return <HorizontalProducts products={data?.data?.products} />;
+};
