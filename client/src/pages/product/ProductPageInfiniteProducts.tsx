f38054937e0a19ba95301f370b2504eeef4f293c
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-04 09:50:42 +0100

    Add ProductPageInfiniteProducts.tsx

---
+++ client/src/pages/product/ProductPageInfiniteProducts.tsx
@@ -0,0 +1,43 @@
+import { getProducts } from "../../api/getProducts";
+import { useProduct } from "./useProduct";
+import { useInfinite } from "../../hooks/useInfinite";
+import { InfiniteProducts } from "../../Components/InfiniteProducts";
+
+const queryFn =
+  (to: number) =>
+  ({ pageParam }) => {
+    if (pageParam === null) return;
+
+    return getProducts({
+      query: "",
+      page: Number(pageParam),
+      to,
+    });
+  };
+
+export const ProductPageInfiniteProducts = () => {
+  const { data } = useProduct();
+
+  const {
+    data: products,
+    error,
+    isFetching,
+    isLoading,
+    ref,
+  } = useInfinite({
+    queryKey: ["products-recommended", data?.data?.product?._id],
+    queryFn: queryFn(
+      data?.data?.products?.price + data?.data?.products?.shipping
+    ),
+  });
+
+  return (
+    <InfiniteProducts
+      data={products}
+      error={error}
+      isFetching={isFetching}
+      isLoading={isLoading}
+      ref={ref}
+    />
+  );
+};
