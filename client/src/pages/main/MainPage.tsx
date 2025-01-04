a416086ee57d2b5be89647075b3141ea129059ba
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-04 07:54:21 +0100

    Update MainPage.tsx

--- client/src/pages/main/MainPage.tsx
+++ client/src/pages/main/MainPage.tsx
@@ -1,10 +1,8 @@
 import { Hero } from "../../Components/Hero";
 import { SaleSection } from "./SaleSection";
-import HorizontalProducts from "./HorizontalProducts.tsx";
 import { SkewedContainer } from "./SkewedContainer";
-import { Under50 } from "./Under50.tsx";
-import { Row } from "../../Util/Row.tsx";
-import { FreeShipping } from "./FreeShipping.tsx";
+import { LatestProducts } from "./LatestProducts.tsx";
+import { ForYouSection } from "./ForYouSection.tsx";
 
 export const MainPage = () => {
   return (
@@ -13,11 +11,9 @@ export const MainPage = () => {
 
       <SkewedContainer>
         <SaleSection />
-        <Row $gap="3.2rem" $flexWrap="wrap">
-          <Under50 />
-          <FreeShipping />
-        </Row>
-        {/* <HorizontalProducts /> */}
+        <ForYouSection />
+
+        <LatestProducts />
       </SkewedContainer>
     </>
   );
