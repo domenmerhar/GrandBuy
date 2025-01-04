5f20b15b215c727ac4701eec95c772ce5b30afa3
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-04 08:56:05 +0100

    Add useInfinte.ts

---
+++ client/src/hooks/useInfinite.ts
@@ -0,0 +1,28 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+
+interface useInfiniteProps {
+  queryKey: unknown[];
+  queryFn: ({ pageParam }: { pageParam: unknown }) => unknown;
+}
+
+export const useInfinite = ({ queryKey, queryFn }: useInfiniteProps) => {
+  const { ref, inView } = useInView({ threshold: 0.5 });
+
+  const { data, isLoading, error, fetchNextPage, isFetching } =
+    useInfiniteQuery({
+      queryKey,
+      queryFn,
+      getNextPageParam: (lastPage) => lastPage?.nextItem || null,
+      initialPageParam: 1,
+    });
+
+  useEffect(() => {
+    if (inView && !isFetching) {
+      fetchNextPage();
+    }
+  }, [ref, inView, fetchNextPage, isFetching]);
+
+  return { data, isLoading, error, isFetching, ref };
+};
