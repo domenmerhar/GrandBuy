9ddf62aab1869b7681212a4136ea1b4dc0302f70
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-05 09:44:19 +0100

    Add getUser.ts

---
+++ client/src/api/getUser.ts
@@ -0,0 +1,8 @@
+import { toApiPath } from "../functions/toApiPath";
+
+export const getUser = async (id: string, role: "user" | "seller") => {
+  const response = await fetch(toApiPath(`user/${id}?role=${role}`));
+  const data = await response.json();
+
+  return data;
+};
