0deea98eb5af5ac0e2bd38c1a32d15811765cbda
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-05 08:36:26 +0100

    Update RouteProtector.tsx

--- client/src/Components/RouteProtector.tsx
+++ client/src/Components/RouteProtector.tsx
@@ -1,17 +1,22 @@
-import { FC } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { FC, ReactNode } from "react";
 import { useAuthContext } from "../contexts/AuthContext";
+import { Navigate } from "react-router-dom";
 type UserRole = "admin" | "user" | "seller" | "";
 
 interface RouteProtectorProps {
   allowedRoles: UserRole[];
+  children?: ReactNode | ReactNode[];
 }
 
-export const RouteProtector: FC<RouteProtectorProps> = ({ allowedRoles }) => {
-  const navigate = useNavigate();
+export const RouteProtector: FC<RouteProtectorProps> = ({
+  allowedRoles,
+  children,
+}) => {
   const [{ role }] = useAuthContext();
 
-  if (!allowedRoles.includes(role)) navigate("/", { replace: true });
-
-  return <Outlet />;
+  return !allowedRoles.includes(role) ? (
+    <Navigate to={"/"} replace relative="path" />
+  ) : (
+    children
+  );
 };
