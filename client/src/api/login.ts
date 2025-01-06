69a781ae4e11f685bac40f5d953a353353c48129
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-06 09:47:59 +0100

    Add login.ts

---
+++ client/src/api/login.ts
@@ -0,0 +1,20 @@
+import { toApiPath } from "../functions/toApiPath";
+
+export const login = async ({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) => {
+  const response = await fetch(toApiPath("user/login"), {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  const data = await response.json();
+  return data;
+};
