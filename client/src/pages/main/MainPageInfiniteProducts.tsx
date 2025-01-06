878e0a9f9eeefa924d8c847417f75136328923d9
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-06 10:20:08 +0100

    Update MainPageInfiniteProducts.tsx

--- client/src/pages/main/MainPageInfiniteProducts.tsx
+++ client/src/pages/main/MainPageInfiniteProducts.tsx
@@ -1,7 +1,5 @@
 import { getProducts } from "../../api/getProducts";
-import { ErrorBox } from "../../Components/ErrorBox";
 import { InfiniteProducts } from "../../Components/InfiniteProducts";
-import { SpinnerInBox } from "../../Components/SpinnerInBox";
 import { useInfinite } from "../../hooks/useInfinite";
 
 const queryFn = ({ pageParam }: unknown) => {
@@ -15,21 +13,10 @@ const queryFn = ({ pageParam }: unknown) => {
 };
 
 export const MainPageInfiniteProducts = () => {
-  const { data, error, isLoading, isFetching, ref } = useInfinite({
+  const data = useInfinite({
     queryKey: ["main-page-infinite-products"],
     queryFn,
   });
 
-  if (isLoading) return <SpinnerInBox fullPage={false} />;
-  if (error) return <ErrorBox fullPage={false} />;
-
-  return (
-    <InfiniteProducts
-      data={data}
-      isFetching={isFetching}
-      ref={ref}
-      error={error}
-      isLoading={isLoading}
-    />
-  );
+  return <InfiniteProducts {...data} />;
 };
