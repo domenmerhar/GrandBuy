6935abe128d206bb008390dd668d07c68754b071
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-09 19:27:15 +0100

    Add getHistory.ts

---
+++ client/src/api/getHistory.ts
@@ -0,0 +1,22 @@
+import { toApiPath } from "../functions/toApiPath";
+
+export const getHistory = async ({
+  JWT,
+  page,
+}: {
+  JWT: string;
+  page: number;
+}) => {
+  const limit = Number(import.meta.env.VITE_PRODUCT_PAGE_SIZE);
+
+  const res = await fetch(toApiPath(`history?page=${page}&limit=${limit}`), {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${JWT}`,
+    },
+  });
+
+  const data = await res.json();
+
+  return data;
+};
