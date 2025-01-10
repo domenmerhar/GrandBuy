3ed6fdbdaade709d5301ce935b7073f84845ca2a
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-10 18:18:24 +0100

    Update useProduct.tsx

--- client/src/pages/product/useProduct.tsx
+++ client/src/pages/product/useProduct.tsx
@@ -1,15 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { getProduct } from "../../api/getProduct";
+import { useAuthContext } from "../../contexts/AuthContext";
 
 export const useProduct = () => {
   const { productId } = useParams();
+  const [{ JWT }] = useAuthContext();
 
   if (!productId) throw new Error("No productId found in URL");
 
   const { data, isLoading, error } = useQuery({
     queryKey: ["product", productId],
-    queryFn: () => getProduct(productId!),
+    queryFn: () => getProduct(productId!, JWT),
   });
 
   return { data, isLoading, error };
