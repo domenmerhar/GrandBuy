f6273c4416b5d2316ec5975d2a3eca5749079fd7
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-10 18:55:15 +0100

    Update HistoryPageInfiniteProducts.tsx

--- client/src/pages/history/HistoryPageInfiniteProducts.tsx
+++ client/src/pages/history/HistoryPageInfiniteProducts.tsx
@@ -5,6 +5,7 @@ import { InfiniteProducts } from "../../Components/InfiniteProducts";
 import { IProductShort } from "../../Util/types";
 import { ProductCard } from "../../Util/ProductCard";
 import { toApiFilesPath } from "../../functions/toApiFilesPath";
+import { ProductGrid } from "../../Util/ProductGrid";
 
 const queryFn =
   (JWT: string) =>
@@ -22,28 +23,26 @@ export const HistoryPageInfiniteProducts = () => {
     queryFn: queryFn(JWT),
   });
 
-  return (
-    <InfiniteProducts
-      {...data}
-      renderFn={(page) =>
-        page?.data?.historyItems?.map(
-          ({ product }: { _id: string; product: IProductShort }) => {
-            if (!product) return null;
-            const { _id, name, coverImage, discount, totalPrice } = product;
+  const renderFn = (page) =>
+    page?.data?.historyItems?.map(
+      ({ product }: { _id: string; product: IProductShort }) => {
+        if (!product) return null;
+        const { _id, name, coverImage, discount, totalPrice } = product;
 
-            return (
-              <ProductCard
-                key={_id}
-                id={_id}
-                title={name}
-                image={toApiFilesPath(coverImage)}
-                discount={discount}
-                price={totalPrice}
-              />
-            );
-          }
-        )
+        return (
+          <ProductCard
+            key={_id}
+            id={_id}
+            title={name}
+            image={toApiFilesPath(coverImage)}
+            discount={discount}
+            price={totalPrice}
+          />
+        );
       }
-    />
+    );
+
+  return (
+    <InfiniteProducts {...data} container={ProductGrid} renderFn={renderFn} />
   );
 };
