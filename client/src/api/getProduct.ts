31129ada2cdd51035716a57e7766a6631465fc4d
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-10 18:15:17 +0100

    Update getProduct.ts

--- client/src/api/getProduct.ts
+++ client/src/api/getProduct.ts
@@ -1,7 +1,15 @@
 import { toApiPath } from "../functions/toApiPath";
 
-export const getProduct = async (id: string) => {
-  const response = await fetch(toApiPath(`product/${id}`));
+export const getProduct = async (id: string, JWT?: string) => {
+  const Authorization = JWT ? `Bearer ${JWT}` : "";
+
+  const response = await fetch(toApiPath(`product/${id}`), {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization,
+    },
+  });
   const data = await response.json();
   return data;
 };
