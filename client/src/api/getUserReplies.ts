78af0f6d3118eff2201bafd3ca75e8db1250d167
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-11 10:11:16 +0100

    Add getUserReplies.ts

---
+++ client/src/api/getUserReplies.ts
@@ -0,0 +1,27 @@
+import { toApiPath } from "../functions/toApiPath";
+
+export const getUserReplies = async ({
+  userId,
+  page,
+  sort,
+}: {
+  userId: string;
+  page: number;
+  sort: "-createdAt" | "+createdAt";
+}) => {
+  const limit = Number(import.meta.env.VITE_USER_REVIEWS_PAGE_SIZE);
+
+  const response = await fetch(
+    toApiPath(`reply/user/${userId}?page=${page}&limit=${limit}&sort=${sort}`)
+  );
+
+  console.log(
+    toApiPath(`reply/user/${userId}?page=${page}&limit=${limit}&sort=${sort}`)
+  );
+
+  const data = await response.json();
+
+  const nextItem = data.data.replies.length === limit ? page + 1 : null;
+
+  return { ...data, nextItem };
+};
