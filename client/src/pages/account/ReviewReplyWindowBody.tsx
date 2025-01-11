4c0fcb248335d1da488b20fbe9edb8015c065472
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-11 10:11:27 +0100

    Update ReviewReplyWindowBody.tsx

--- client/src/pages/account/ReviewReplyWindowBody.tsx
+++ client/src/pages/account/ReviewReplyWindowBody.tsx
@@ -3,6 +3,7 @@ import { CardWithHeader } from "../../Util/CardWithHeader";
 import { Column } from "../../Util/Column";
 import { ReviewReplyWindowSelect } from "./ReviewReplyWindowSelect";
 import { UserReviews } from "./UserReviews";
+import { UserReplies } from "./UserReplies";
 
 export const ReviewReplyWindowBody = () => {
   const [searchParams] = useSearchParams();
@@ -12,7 +13,11 @@ export const ReviewReplyWindowBody = () => {
       <Column $gap="3.2rem">
         <ReviewReplyWindowSelect />
 
-        {searchParams.get("location") === "replies" ? null : <UserReviews />}
+        {searchParams.get("location") === "replies" ? (
+          <UserReplies />
+        ) : (
+          <UserReviews />
+        )}
       </Column>
     </CardWithHeader.Body>
   );
