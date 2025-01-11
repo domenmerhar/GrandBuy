606234c71130eab7eefebf6c7d5c25baa59ce5d1
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-11 10:11:49 +0100

    Add UserReplies.tsx

---
+++ client/src/pages/account/UserReplies.tsx
@@ -0,0 +1,50 @@
+import { useParams, useSearchParams } from "react-router-dom";
+import { useInfinite } from "../../hooks/useInfinite";
+import { getUserReplies } from "../../api/getUserReplies";
+import { Reply } from "../../Util/types";
+import { ReviewReplyCard } from "./ReviewReplyCard";
+import { toDate } from "../../functions/toDate";
+import { InfiniteProducts } from "../../Components/InfiniteProducts";
+import styled from "styled-components";
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 3.2rem;
+`;
+
+export const UserReplies = () => {
+  const { userId } = useParams();
+  const [searchParams] = useSearchParams();
+  const sort =
+    searchParams.get("sort") === "-createdAt" ? "-createdAt" : "+createdAt";
+
+  const data = useInfinite({
+    queryKey: ["userReplies", searchParams.get("sort"), userId],
+    queryFn: ({ pageParam }) => {
+      if (pageParam === null) return;
+
+      return getUserReplies({
+        userId: String(userId),
+        page: Number(pageParam),
+        sort,
+      });
+    },
+  });
+
+  const renderFn = ({ data }: { data: { replies: Reply[] } }) => {
+    if (!data?.replies) return null;
+
+    return data.replies.map(({ _id, reply, createdAt }) => (
+      <ReviewReplyCard
+        key={_id}
+        content={reply}
+        date={toDate(createdAt)}
+        profileImage=""
+        username=""
+      />
+    ));
+  };
+
+  return <InfiniteProducts {...data} renderFn={renderFn} container={Grid} />;
+};
