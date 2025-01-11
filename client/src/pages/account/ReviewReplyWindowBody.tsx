19d88877409d43c4d7f1977e35f4d460b610d07c
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-11 09:51:31 +0100

    Updat ReviewReplyWIndowBody.tsx

--- client/src/pages/account/ReviewReplyWindowBody.tsx
+++ client/src/pages/account/ReviewReplyWindowBody.tsx
@@ -1,64 +1,18 @@
-import styled from "styled-components";
+import { useSearchParams } from "react-router-dom";
 import { CardWithHeader } from "../../Util/CardWithHeader";
 import { Column } from "../../Util/Column";
 import { ReviewReplyWindowSelect } from "./ReviewReplyWindowSelect";
-import { useParams } from "react-router-dom";
-import { useInfinite } from "../../hooks/useInfinite";
-import { getUserReviews } from "../../api/getUserReviews";
-import { InfiniteProducts } from "../../Components/InfiniteProducts";
-import { Review } from "../../Util/types";
-import { ReviewReplyCard } from "./ReviewReplyCard";
-import { toDate } from "../../functions/toDate";
-import { useUser } from "../../hooks/useUser";
-import { toApiFilesPath } from "../../functions/toApiFilesPath";
-
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-  gap: 3.2rem;
-`;
+import { UserReviews } from "./UserReviews";
 
 export const ReviewReplyWindowBody = () => {
-  const { userId } = useParams();
-
-  const { data: dataUser, isLoading } = useUser();
-
-  const data = useInfinite({
-    queryKey: ["userReviews", userId],
-    queryFn: ({ pageParam }) => {
-      if (pageParam === null) return;
-
-      return getUserReviews(String(userId), Number(pageParam));
-    },
-  });
-
-  const renderFn = (page: { data?: { doc: Review[] } }) => {
-    if (!page?.data) return;
-
-    return page.data.doc.map(({ _id, review, createdAt }) => (
-      <ReviewReplyCard
-        key={_id}
-        content={review}
-        date={toDate(createdAt)}
-        profileImage={
-          dataUser?.data?.image ? toApiFilesPath(dataUser?.data?.image) : ""
-        }
-        username={dataUser?.data?.username}
-      />
-    ));
-  };
+  const [searchParams] = useSearchParams();
 
   return (
     <CardWithHeader.Body>
       <Column $gap="3.2rem">
         <ReviewReplyWindowSelect />
 
-        <InfiniteProducts
-          {...data}
-          container={Grid}
-          renderFn={renderFn}
-          isLoading={isLoading || data.isLoading}
-        />
+        {searchParams.get("location") === "replies" ? null : <UserReviews />}
       </Column>
     </CardWithHeader.Body>
   );
