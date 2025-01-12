a47c496b4957dfd78ee1d576446ee148b027045f
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-12 09:14:17 +0100

    Add getMe.ts

---
+++ client/src/api/getMe.ts
@@ -0,0 +1,14 @@
+import { toApiPath } from "../functions/toApiPath";
+
+export const getMe = async (JWT: string) => {
+  const res = await fetch(toApiPath(`user/me`), {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${JWT}`,
+    },
+  });
+
+  const data = await res.json();
+
+  return data;
+};
