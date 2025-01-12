b364af54ab594840806391b6bfed84fc3f8c4ba0
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-12 11:34:30 +0100

    Update PasswordSection.tsx

--- client/src/pages/settings/PasswordSection.tsx
+++ client/src/pages/settings/PasswordSection.tsx
@@ -1,11 +1,38 @@
+import { FormEvent, useRef } from "react";
 import { Button } from "../../Util/Button";
 import { HeaderUppercaseBold } from "../../Util/HeaderUppercaseBold";
 import { InputWithLabel } from "../../Util/InputWithLabel";
 import { SettingsForm } from "./SettingsForm";
+import toast from "react-hot-toast";
+import { useChangePassword } from "../../hooks/useChangePassword";
+import { useAuthContext } from "../../contexts/AuthContext";
 
 export const PasswordSection = () => {
+  const [{ JWT }] = useAuthContext();
+  const { mutate } = useChangePassword();
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const confirmPasswordRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log(passwordRef.current?.value, confirmPasswordRef.current?.value);
+
+    if (!passwordRef.current?.value || !confirmPasswordRef.current?.value)
+      return toast.error("Please fill in all fields", { id: "changePassword" });
+
+    if (passwordRef.current.value !== confirmPasswordRef.current.value)
+      return toast.error("Passwords do not match", { id: "changePassword" });
+
+    mutate({
+      JWT,
+      password: passwordRef.current.value,
+      confirmPassword: confirmPasswordRef.current.value,
+    });
+  };
+
   return (
-    <SettingsForm>
+    <SettingsForm onSubmit={handleSubmit}>
       <HeaderUppercaseBold as="h2">Password</HeaderUppercaseBold>
 
       <InputWithLabel
@@ -13,6 +40,7 @@ export const PasswordSection = () => {
         title="Password"
         type="password"
         placeholder="********"
+        ref={passwordRef}
       />
 
       <InputWithLabel
@@ -20,6 +48,7 @@ export const PasswordSection = () => {
         title="Confirm Password"
         type="password"
         placeholder="********"
+        ref={confirmPasswordRef}
       />
 
       <Button $color="orange" $shape="rectangle" $size="medium">
