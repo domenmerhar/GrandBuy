51d75ff1f9ab1e7d5c0a7e67e8b73b1d3dd6b904
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-12 09:14:22 +0100

    Add useMe.ts

---
+++ client/src/hooks/useMe.ts
@@ -0,0 +1,14 @@
+import { getMe } from "../api/getMe";
+import { useQuery } from "@tanstack/react-query";
+import { useAuthContext } from "../contexts/AuthContext";
+
+export const useMe = () => {
+  const [{ userId, JWT }] = useAuthContext();
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["user-settings", userId],
+    queryFn: () => getMe(JWT),
+  });
+
+  return { data, error, isLoading };
+};
