e332507f6ff7be908205fb9eafb8b9d050dc3109
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-17 08:35:10 +0100

    Add useLogout.ts

---
+++ client/src/hooks/useLogout.ts
@@ -0,0 +1,21 @@
+import { useMutation } from "@tanstack/react-query";
+import { logout } from "../api/auth/logout";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+export const useLogout = () => {
+  const navigate = useNavigate();
+  return useMutation({
+    mutationFn: logout,
+
+    onMutate: () => {
+      toast.loading("Logging out...", { id: "logout" });
+    },
+
+    onSuccess: (data) => {
+      console.log(data);
+      toast.success("Logged out", { id: "logout" });
+      navigate("/login");
+    },
+  });
+};
