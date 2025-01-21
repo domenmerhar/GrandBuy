d90c4831a052590759dc0a00198af70327e62f09
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-21 20:12:18 +0100

    Update AddButton.tsx

--- client/src/Util/AddButton.tsx
+++ client/src/Util/AddButton.tsx
@@ -1,9 +1,23 @@
 import { HiPlus } from "react-icons/hi";
 import { SquareButton } from "./SquareButton";
+import { useAddProductToCard } from "../hooks/cart/useAddProductToCard";
+import { useAuthContext } from "../contexts/AuthContext";
+import { FC } from "react";
 
-export const AddButton = () => {
+interface AddButtonProps {
+  productId: string;
+}
+
+export const AddButton: FC<AddButtonProps> = ({ productId }) => {
+  const [{ JWT }] = useAuthContext();
+  const { mutate } = useAddProductToCard();
+
+  const handleClick = () => {
+    mutate({ JWT, productId, quantity: 1 });
+  };
+
   return (
-    <SquareButton $size="small" $color="orange">
+    <SquareButton $size="small" $color="orange" onClick={handleClick}>
       <HiPlus color="#f8f9fa" size={24} />
     </SquareButton>
   );
