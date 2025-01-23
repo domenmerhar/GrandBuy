917d06d96190ac40b22af4b5269cda59338d3b8f
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-23 17:23:12 +0100

    Update CartPage.tsx

--- client/src/pages/Cart/CartPage.tsx
+++ client/src/pages/Cart/CartPage.tsx
@@ -4,8 +4,8 @@ import { ProductFilter } from "../../Util/ProductFilter";
 import { Stepper } from "../../Util/Stepper";
 import { CardFilterGrid } from "../../Util/CardFilterGrid";
 import { ProductsCard } from "../../Util/ProductsCard";
-import { CartItem } from "./CartItem";
 import { useCartItemsCount } from "../../hooks/cart/useCartItemsCount";
+import { CartItems } from "./CartItems";
 
 const itemsPerPage = import.meta.env.VITE_PRODUCTS_PER_STEPPER;
 
@@ -13,18 +13,14 @@ export const CartPage = () => {
   const { data, isLoading, isError } = useCartItemsCount();
 
   const itemCount =
-    !isLoading && !isError && data?.data?.cartItems
-      ? data.data.cartItems.length
-      : 0;
-
+    !isLoading && !isError && data?.data?.cartItems ? data.data.cartItems : 0;
   const max = itemCount ? Math.ceil(itemCount / itemsPerPage) : 1;
 
   return (
     <Content>
       <CardFilterGrid>
         <ProductsCard title="Shopping Cart" itemCount={itemCount}>
-          <CartItem />
-          <CartItem />
+          <CartItems />
         </ProductsCard>
 
         <StyledSidebar $position="sticky" $width="auto" $height="80vh">
