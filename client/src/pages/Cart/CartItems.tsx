c196dbba4a06437f160296c3028e88f2c94e2227
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-23 17:23:05 +0100

    Add CartItems.tsx

---
+++ client/src/pages/Cart/CartItems.tsx
@@ -0,0 +1,26 @@
+import { useCartItems } from "../../hooks/cart/useCartItems";
+import { SpinnerInBox } from "../../Components/SpinnerInBox";
+import { ErrorBox } from "../../Components/ErrorBox";
+import { CartItem } from "./CartItem";
+import { CartItemInterface } from "../../Util/types";
+import { toPrice } from "../../functions/toPrice";
+import { toApiFilesPath } from "../../functions/toApiFilesPath";
+
+export const CartItems = () => {
+  const { data, isLoading, error } = useCartItems();
+
+  if (isLoading) return <SpinnerInBox fullPage={false} />;
+  if (error) return <ErrorBox fullPage={false} />;
+
+  return data?.data?.cartItems?.map(
+    ({ product: { coverImage, name, totalPrice, _id } }: CartItemInterface) => (
+      <CartItem
+        image={toApiFilesPath(coverImage)}
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat animi nam aperiam earum explicabo ratione reiciendis, vel, a veritatis voluptatum molestias quaerat cumque, aut natus deleniti architecto iusto fugiat dignissimos."
+        name={name}
+        price={toPrice(totalPrice, "USD")}
+        key={_id}
+      />
+    )
+  );
+};
