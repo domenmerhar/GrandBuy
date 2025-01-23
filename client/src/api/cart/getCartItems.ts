b57cbb5dbcd4a937013c118f8a4d6f8c5a2f3e8a
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-23 17:22:35 +0100

    Add getCartItems.ts

---
+++ client/src/api/cart/getCartItems.ts
@@ -0,0 +1,41 @@
+import { toApiPath } from "../../functions/toApiPath";
+
+export const getCartItems = async ({
+  JWT,
+  page,
+  from,
+  to,
+  sale,
+  freeShipping,
+}: {
+  JWT: string;
+  page: number;
+  sale?: boolean;
+  from?: number;
+  to?: number;
+  freeShipping?: boolean;
+}) => {
+  const limit = Number(import.meta.env.VITE_PRODUCTS_PER_STEPPER);
+
+  const queryParamsStr = [
+    freeShipping && "shipping[lte]=0",
+    limit && `limit=${limit}`,
+    page && `page=${page}`,
+    sale && "discount[gt]=0",
+    from && `totalPrice[gte]=${from}`,
+    to && `totalPrice[lte]=${to}`,
+  ]
+    .filter((param) => !!param)
+    .join("&");
+
+  const res = await fetch(toApiPath(`cart?${queryParamsStr}`), {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${JWT}`,
+    },
+  });
+
+  const data = await res.json();
+
+  return data;
+};
