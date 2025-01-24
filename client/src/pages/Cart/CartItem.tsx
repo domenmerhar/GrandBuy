c1de937e9442ce536b6ade996c103343333df445
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-24 19:51:35 +0100

    Update CartItem.tsx

--- client/src/pages/Cart/CartItem.tsx
+++ client/src/pages/Cart/CartItem.tsx
@@ -1,7 +1,12 @@
 import styled from "styled-components";
-import { AddButton } from "../../Util/AddButton";
 import { Row } from "../../Util/Row";
 import { FC } from "react";
+import { Column } from "../../Util/Column";
+import { SquareButton } from "../../Util/SquareButton";
+import { HiOutlineTrash } from "react-icons/hi";
+import { StepperRaw } from "../../Util/StepperRaw";
+import { useAddProductToCard } from "../../hooks/cart/useAddProductToCard";
+import { useAuthContext } from "../../contexts/AuthContext";
 
 const Image = styled.img`
   width: 14rem;
@@ -12,23 +17,16 @@ const ProductInfoHolder = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1rem;
+`;
 
-  & + button {
-    margin-left: auto;
-  }
+const ButtonHolder = styled(Column)`
+  margin-left: auto;
 `;
 
 const Product = styled.h2`
   font-size: 1.6rem;
   font-weight: 600;
   color: var(--gray-7);
-`;
-
-const Description = styled.p`
-  max-width: 50ch;
-  font-size: 1.4rem;
-
-  text-overflow: ellipsis;
 `;
 
 const Price = styled.p`
@@ -38,27 +36,50 @@ const Price = styled.p`
 `;
 
 interface CartItemProps {
+  productId: string;
   image: string;
   name: string;
   description: string;
   price: string;
+  quantity: number;
 }
 
 export const CartItem: FC<CartItemProps> = ({
+  productId,
   image,
   name,
   description,
   price,
+  quantity,
 }) => {
+  const [{ JWT }] = useAuthContext();
+  const { mutate, isPending } = useAddProductToCard();
+
   return (
     <Row $gap="2rem">
       <Image src={image} />
       <ProductInfoHolder>
         <Product>{name}</Product>
-        <Description>{description}</Description>
         <Price>{price}</Price>
       </ProductInfoHolder>
-      <AddButton />
+
+      <ButtonHolder $alignItems="flex-end" $justifyContent="space-around">
+        <SquareButton $color="red" $size="small">
+          <HiOutlineTrash />
+        </SquareButton>
+
+        <StepperRaw
+          color="orange"
+          placeholder={String(quantity)}
+          min={0}
+          handleNextPage={() =>
+            mutate({ JWT, productId, quantity: quantity + 1 })
+          }
+          disabledLeft={isPending}
+          disabledInput
+          disabledRight={isPending}
+        />
+      </ButtonHolder>
     </Row>
   );
 };
