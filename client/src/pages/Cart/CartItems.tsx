e48e070a4c8ff9c22a9dd41feb5a605b3c5028ce
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-25 07:27:50 +0100

    Update CartItems.tsx

--- client/src/pages/Cart/CartItems.tsx
+++ client/src/pages/Cart/CartItems.tsx
@@ -1,10 +1,10 @@
 import { useCartItems } from "../../hooks/cart/useCartItems";
 import { SpinnerInBox } from "../../Components/SpinnerInBox";
 import { ErrorBox } from "../../Components/ErrorBox";
-import { CartItem } from "./CartItem";
 import { CartItemInterface } from "../../Util/types";
 import { toPrice } from "../../functions/toPrice";
 import { toApiFilesPath } from "../../functions/toApiFilesPath";
+import { CartItem } from "./CartItem";
 
 export const CartItems = () => {
   const { data, isLoading, error } = useCartItems();
@@ -13,13 +13,23 @@ export const CartItems = () => {
   if (error) return <ErrorBox fullPage={false} />;
 
   return data?.data?.cartItems?.map(
-    ({ _id, image, name, totalPrice }: CartItemInterface) => (
+    ({
+      _id,
+      image,
+      name,
+      totalPrice,
+      quantity,
+      product,
+    }: CartItemInterface) => (
       <CartItem
         image={toApiFilesPath(image)}
         description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat animi nam aperiam earum explicabo ratione reiciendis, vel, a veritatis voluptatum molestias quaerat cumque, aut natus deleniti architecto iusto fugiat dignissimos."
         name={name}
         price={toPrice(totalPrice, "USD")}
+        quantity={quantity}
         key={_id}
+        productId={product}
+        cartItemId={_id}
       />
     )
   );
