2476b6efa0dc7951236bd54c2daf05c2b3c58ba6
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-25 12:33:19 +0100

    Add useGetCartItemsSummary.ts

---
+++ client/src/hooks/cart/useGetCartItemsSummary.ts
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import { useAuthContext } from "../../contexts/AuthContext";
+import { useSearchParams } from "react-router-dom";
+import { getCartItemsSummary } from "../../api/cart/getCartItemsSummary";
+
+export const useGetCartItemsSummary = () => {
+  const [{ JWT, userId }] = useAuthContext();
+  const [searchParams] = useSearchParams();
+
+  const cartItems = searchParams.get("products")?.split(",") || [];
+
+  return useQuery({
+    queryKey: ["cartItemsSummary", userId, cartItems],
+    queryFn: () =>
+      getCartItemsSummary({
+        JWT,
+        cartItems,
+      }),
+  });
+};
