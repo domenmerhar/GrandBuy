8ccd146b4846e707445a4cc6be500bdc4045f0e2
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-25 19:59:36 +0100

    Update CouponInput.tsx

--- client/src/pages/Cart/CouponInput.tsx
+++ client/src/pages/Cart/CouponInput.tsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import { BlankCard } from "../../Util/BlankCard";
 import { Input } from "../../Util/Input";
+import { useAuthContext } from "../../contexts/AuthContext";
+import { useApplyCoupon } from "../../hooks/cart/useApplyCoupon";
 
 const InputHolder = styled(BlankCard)`
   display: flex;
@@ -8,9 +10,20 @@ const InputHolder = styled(BlankCard)`
 `;
 
 export const CouponInput = () => {
+  const [{ JWT }] = useAuthContext();
+  const { mutate } = useApplyCoupon();
+
   return (
     <InputHolder>
-      <Input placeholder="Coupon Code" />
+      <Input
+        placeholder="Coupon Code"
+        onBlur={(e) => {
+          console.log(e.target.value);
+          if (!e.target.value) return;
+
+          mutate({ JWT, couponCode: e.target.value });
+        }}
+      />
     </InputHolder>
   );
 };
