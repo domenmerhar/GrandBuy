c295814cca7a0d0d95c58d96430e07468be26d80
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-25 20:28:13 +0100

    Update CartPage.tsx

--- client/src/pages/Cart/CartPage.tsx
+++ client/src/pages/Cart/CartPage.tsx
@@ -8,15 +8,33 @@ import { Column } from "../../Util/Column";
 import { OrderSummary } from "./OrderSummary";
 import { Button } from "../../Util/Button";
 import { CouponInput } from "./CouponInput";
+import { useSearchParams } from "react-router-dom";
+import toast from "react-hot-toast";
+import { useAddOrder } from "../../hooks/order/useAddOrder";
+import { useAuthContext } from "../../contexts/AuthContext";
 
 const itemsPerPage = import.meta.env.VITE_PRODUCTS_PER_STEPPER;
 
 export const CartPage = () => {
   const { data, isLoading, isError } = useCartItemsCount();
+  const [{ JWT }] = useAuthContext();
+  const [searchParams] = useSearchParams();
+  const { mutate } = useAddOrder();
 
   const itemCount =
     !isLoading && !isError && data?.data?.cartItems ? data.data.cartItems : 0;
   const max = itemCount ? Math.ceil(itemCount / itemsPerPage) : 1;
+
+  const handleOrder = () => {
+    const cartItems = searchParams.get("products")?.split(",");
+
+    console.log({ cartItems });
+
+    if (!cartItems?.length)
+      return toast.error("Please add items to cart first", { id: "add-order" });
+
+    mutate({ JWT, cartItems });
+  };
 
   return (
     <Content>
@@ -30,7 +48,12 @@ export const CartPage = () => {
 
           <CouponInput />
 
-          <Button $color="orange" $shape="oval" $size="medium">
+          <Button
+            $color="orange"
+            $shape="oval"
+            $size="medium"
+            onClick={handleOrder}
+          >
             Order
           </Button>
         </Column>
