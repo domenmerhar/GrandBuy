5ec9f199f220a1ac84f7c652e4f919fd518aa26b
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-25 12:33:00 +0100

    Add getCartItemsSummary.ts

---
+++ client/src/api/cart/getCartItemsSummary.ts
@@ -0,0 +1,34 @@
+import { toApiPath } from "../../functions/toApiPath";
+
+export const getCartItemsSummary = async ({
+  JWT,
+  cartItems,
+}: {
+  JWT: string;
+  cartItems: string[];
+}) => {
+  if (cartItems.length === 0)
+    return {
+      status: "success",
+      data: {
+        items: 0,
+        shipping: 0,
+        discount: 0,
+        total: 0,
+      },
+    };
+
+  const cartItemsStr = cartItems.sort().join(",");
+  console.log(cartItemsStr);
+
+  const res = await fetch(toApiPath(`cart/summary/${cartItemsStr}`), {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${JWT}`,
+    },
+  });
+
+  const data = await res.json();
+
+  return data;
+};
