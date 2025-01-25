35ae1d1db8aa7d3cd66a340c925d986e3a2af1eb
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-25 12:33:32 +0100

    Add CartItemButton.tsx

---
+++ client/src/pages/Cart/CartItemButton.tsx
@@ -0,0 +1,47 @@
+import { FC } from "react";
+import { useSearchParams } from "react-router-dom";
+import styled from "styled-components";
+import { CircleButton } from "../../Util/CircleButton";
+
+const AlignedCircleButton = styled(CircleButton)`
+  align-self: center;
+`;
+
+interface CartItemButtonProps {
+  cartItemId: string;
+}
+
+export const CartItemButton: FC<CartItemButtonProps> = ({ cartItemId }) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleCirlceButton = () => {
+    const products = searchParams.get("products");
+
+    if (!products)
+      return setSearchParams((prev) => {
+        prev.append("products", cartItemId);
+        return prev;
+      });
+
+    if (!products.includes(cartItemId))
+      return setSearchParams((prev) => {
+        prev.set("products", `${products},${cartItemId}`);
+        return prev;
+      });
+
+    const newProducts = products.split(",").filter((id) => id !== cartItemId);
+    setSearchParams((prev) => {
+      if (newProducts.length === 0) prev.delete("products");
+      else prev.set("products", newProducts.join(","));
+
+      return prev;
+    });
+  };
+
+  return (
+    <AlignedCircleButton
+      $active={Boolean(searchParams.get("products")?.includes(cartItemId))}
+      onClick={handleCirlceButton}
+    />
+  );
+};
