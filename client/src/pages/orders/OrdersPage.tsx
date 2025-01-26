6f682efe34503c8a55586c3b8e0701a09c942d97
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-26 11:33:39 +0100

    Update OrdersPage.tsx

--- client/src/pages/orders/OrdersPage.tsx
+++ client/src/pages/orders/OrdersPage.tsx
@@ -3,21 +3,29 @@ import { Content } from "../../Util/Content";
 import { ProductFilter } from "../../Util/ProductFilter";
 import { ProductsCard } from "../../Util/ProductsCard";
 import { Stepper } from "../../Util/Stepper";
-import { CartItem } from "../Cart/CartItem";
 import { StyledSidebar } from "../../Util/StyledSidebar";
+import { useGetUserOrdersCount } from "../../hooks/order/useGetUserOrdersCount";
+import { Orders } from "./Orders";
+
+const itemsPerPage = import.meta.env.VITE_ORDERS_PER_PAGE;
 
 export const OrdersPage = () => {
+  const { data } = useGetUserOrdersCount();
+
+  const itemCount = data?.data?.ordersCount || 0;
+  const max = itemCount ? Math.ceil(itemCount / itemsPerPage) : 1;
+
   return (
     <Content>
       <CardFilterGrid>
-        <ProductsCard title="Orders" itemCount={2}>
-          <CartItem />
+        <ProductsCard title="Orders" itemCount={itemCount}>
+          <Orders />
         </ProductsCard>
 
         <StyledSidebar $position="sticky" $width="auto" $height="80vh">
           <ProductFilter freeShipping price sale />
         </StyledSidebar>
-        <Stepper searchParamName="page" max={10} />
+        <Stepper searchParamName="page" max={max} />
       </CardFilterGrid>
     </Content>
   );
