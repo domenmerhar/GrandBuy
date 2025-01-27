12f0d0b39e4646e0943eeabc3a21bdd06aa45c5c
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-27 09:21:18 +0100

    Update OrderItem.tsx

--- client/src/pages/orders/OrderItem.tsx
+++ client/src/pages/orders/OrderItem.tsx
@@ -1,8 +1,20 @@
 import styled from "styled-components";
 import { Row } from "../../Util/Row";
-import { AddButton } from "../../Util/AddButton";
 import { Link } from "react-router-dom";
 import { FC } from "react";
+import { ButtonWithNotifcations } from "../../Components/ButtonWithNotifcations";
+import { HiDotsVertical, HiOutlineCheck } from "react-icons/hi";
+import ExpandingList from "../../Components/ExpandingList";
+import { HiArrowUturnLeft } from "react-icons/hi2";
+import { useConfirmOrder } from "../../hooks/order/useConfirmOrderDelivery";
+import { useAuthContext } from "../../contexts/AuthContext";
+import { ItemStatus } from "../../Util/types";
+
+const StyledOrderItem = styled(Row)`
+  & div:nth-child(3) {
+    margin-left: auto;
+  }
+`;
 
 const Image = styled.img`
   width: 14rem;
@@ -12,6 +24,7 @@ const Image = styled.img`
 const ProductInfoHolder = styled.div`
   display: flex;
   flex-direction: column;
+
   gap: 0.6rem;
 
   & + button {
@@ -34,35 +47,68 @@ const Price = styled.p`
 
 const Quantity = styled.p``;
 
-interface CartItemProps {
+interface OrderItemProps {
+  orderId: string;
   image: string;
   name: string;
   price: string;
   productId: string;
   quantity: number;
+  status: ItemStatus;
+  delivered?: boolean;
 }
 
-export const OrderItem: FC<CartItemProps> = ({
+export const OrderItem: FC<OrderItemProps> = ({
+  orderId,
   image,
   name,
   price,
   productId,
   quantity,
+  delivered,
+  status,
 }) => {
+  const [{ JWT }] = useAuthContext();
+  const { mutate } = useConfirmOrder();
+
   return (
-    <Row $gap="2rem">
-      <Link to={`/product/${productId}?quantity=1&page=1&sort=-likesCount`}>
-        <Image src={image} />
-      </Link>
+    <ExpandingList>
+      <StyledOrderItem $gap="2rem" $alignItems="flex-start">
+        <Link to={`/product/${productId}?quantity=1&page=1&sort=-likesCount`}>
+          <Image src={image} />
+        </Link>
 
-      <ProductInfoHolder>
-        <Product>{name}</Product>
+        <ProductInfoHolder>
+          <Product>{name}</Product>
 
-        <Price>Total price: {price}</Price>
-        <Quantity>Quantity: {quantity}</Quantity>
-      </ProductInfoHolder>
+          <Price>Total price: {price}</Price>
+          <Quantity>Quantity: {quantity}</Quantity>
+        </ProductInfoHolder>
 
-      <AddButton productId={productId} />
-    </Row>
+        {delivered && status === "refunded" ? null : (
+          <ExpandingList.Button>
+            <ButtonWithNotifcations>
+              <HiDotsVertical />
+            </ButtonWithNotifcations>
+          </ExpandingList.Button>
+        )}
+
+        <ExpandingList.List>
+          <ExpandingList.Ul>
+            {!delivered ? (
+              <ExpandingList.Li onClick={() => mutate({ JWT, orderId })}>
+                <HiOutlineCheck />
+                Confirm order delivery
+              </ExpandingList.Li>
+            ) : status !== "refunded" ? (
+              <ExpandingList.Li>
+                <HiArrowUturnLeft />
+                Refund item
+              </ExpandingList.Li>
+            ) : null}
+          </ExpandingList.Ul>
+        </ExpandingList.List>
+      </StyledOrderItem>
+    </ExpandingList>
   );
 };
