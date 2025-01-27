6f27902384cf1238b622202f276e865954d7763d
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-27 10:59:03 +0100

    Update useDecrementCartItem.tsx

--- client/src/hooks/cart/useDecrementCartItem.ts
+++ client/src/hooks/cart/useDecrementCartItem.ts
@@ -8,17 +8,11 @@ export const useDecrementCartItem = () => {
   return useMutation({
     mutationFn: decrementCartItem,
 
-    onMutate: () => {
-      toast.loading("Decrementing quantity...", { id: "decrement-cart-item" });
-    },
-
     onSuccess: (data) => {
       if (data.status !== "success" || data?.errors?.length > 0)
         return toast.error("Failed to decrement quantity", {
           id: "decrement-cart-item",
         });
-
-      toast.success("Decremented quantity", { id: "decrement-cart-item" });
 
       client.invalidateQueries({
         predicate: (query) => query.queryKey[0].includes("cartItems"),
