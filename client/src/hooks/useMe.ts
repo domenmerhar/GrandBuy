73fd8f092483b9263060bd954c8c92cca6a3b221
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-29 17:52:20 +0100

    Update useMe.ts

--- client/src/hooks/useMe.ts
+++ client/src/hooks/useMe.ts
@@ -3,12 +3,14 @@ import { useQuery } from "@tanstack/react-query";
 import { useAuthContext } from "../contexts/AuthContext";
 
 export const useMe = () => {
-  const [{ userId, JWT }] = useAuthContext();
+  const { JWT, clearAuthInfo } = useAuthContext();
 
   const { data, error, isLoading } = useQuery({
-    queryKey: ["user-settings", userId],
+    queryKey: ["user-settings", JWT],
     queryFn: () => getMe(JWT),
   });
+
+  if (data?.status === "error") clearAuthInfo();
 
   return { data, error, isLoading };
 };
