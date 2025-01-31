b6852dc6ca3d9ea60b5a8f2e1741c6a0daa87d95
Domen Merhar <domen.merhar@students.vegova.si>
2025-01-31 15:11:29 +0100

    Add SignupConfirmPage.tsx

---
+++ client/src/pages/signupConfirm/SignUpConfirmPage.tsx
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+import { Logo } from "../../Util/Logo";
+import { Button } from "../../Util/Button";
+import { AuthContainer } from "../../Util/AuthContainer";
+import { NakedInput } from "../../Util/NakedInput";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useConfirmEmail } from "../../hooks/auth/useConfirmEmail";
+import { useParams } from "react-router-dom";
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+
+  width: 35rem;
+  gap: 1.2rem;
+
+  & > *:first-child {
+    align-self: center;
+  }
+
+  & > input {
+    width: 100%;
+  }
+
+  & > label {
+    margin-top: 0.8rem;
+  }
+
+  & > button {
+    margin-top: 1.2rem;
+  }
+`;
+
+const P = styled.p`
+  max-width: 50ch;
+`;
+
+export const SignupConfirmPage = () => {
+  const [code, setCode] = useState<string>("");
+  const { mutate } = useConfirmEmail();
+  const { email } = useParams<{ email: string }>();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCode(e.target.value.slice(0, 9));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const verificationCode = (e?.currentTarget.elements[0] as HTMLInputElement)
+      ?.value;
+    if (!verificationCode)
+      return toast.error("Please enter a code.", { id: "confirmEmail" });
+
+    mutate({ email: email!, verificationCode });
+  };
+
+  return (
+    <AuthContainer>
+      <Form onSubmit={handleSubmit}>
+        <Logo $color="orange" />
+        <P>Please enter the verification code sent to your email address.</P>
+
+        <NakedInput
+          placeholder="12345678"
+          $height="3.6rem"
+          type="number"
+          max={999999999}
+          min={111111111}
+          maxLength={9}
+          onChange={handleChange}
+          value={code}
+        />
+
+        <Button $color="orange" $shape="oval" $size="medium">
+          Confirm
+        </Button>
+      </Form>
+    </AuthContainer>
+  );
+};
