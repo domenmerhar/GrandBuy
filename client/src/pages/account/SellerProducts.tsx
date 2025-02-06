aad7afe79b2195510256273a779ab945b3a45b2c
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-06 18:07:32 +0100

    Add SellerProducts.tsx

---
+++ client/src/pages/account/SellerProducts.tsx
@@ -0,0 +1,29 @@
+import { useParams } from "react-router-dom";
+import { getSellerProducts } from "../../api/product/getSellerProducts";
+import { useInfinite } from "../../hooks/useInfinite";
+import { InfiniteProducts } from "../../Components/InfiniteProducts";
+import { renderProduct } from "../../Util/renderProduct";
+import { ProductGrid } from "../../Util/ProductGrid";
+
+export const SellerProducts = () => {
+  const { userId } = useParams();
+
+  const queryFn = ({ pageParam }: { pageParam: unknown }) => {
+    if (pageParam === null) return;
+
+    return getSellerProducts(userId!, Number(pageParam));
+  };
+
+  const data = useInfinite({
+    queryFn,
+    queryKey: ["seller-page-products", userId],
+  });
+
+  return (
+    <InfiniteProducts
+      {...data}
+      renderFn={renderProduct}
+      container={ProductGrid}
+    />
+  );
+};
