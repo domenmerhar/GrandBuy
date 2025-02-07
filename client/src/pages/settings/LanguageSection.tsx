b0cd284d06fe64a99c731ee1f6b390061f74a553
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-07 18:34:01 +0100

    Update LanguageSection.tsx

--- client/src/pages/settings/LanguageSection.tsx
+++ client/src/pages/settings/LanguageSection.tsx
@@ -1,31 +1,18 @@
+import styled from "styled-components";
+import { LanguageButton } from "../../Components/LanguageButton";
 import { HeaderUppercaseBold } from "../../Util/HeaderUppercaseBold";
-import { useLanguage } from "../../Util/LanguageContext";
-import { SwitchButtons } from "../../Util/SwitchButtons";
-import { languages } from "../../Util/types";
 import { SettingsForm } from "./SettingsForm";
 
+const StyledLanguageSection = styled(SettingsForm)`
+  align-items: flex-start;
+`;
+
 export const LanguageSection = () => {
-  const { currentLanguage, setCurrentLanguage } = useLanguage();
-
-  const handleClick = (
-    _: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-    value?: string
-  ) => {
-    if (!value) return;
-
-    setCurrentLanguage(value as languages);
-  };
   return (
-    <SettingsForm as="div">
+    <StyledLanguageSection as="div">
       <HeaderUppercaseBold as="h2">Language</HeaderUppercaseBold>
 
-      <SwitchButtons
-        options={[
-          { name: "EN", value: "en", disabled: currentLanguage === "en" },
-          { name: "SL", value: "sl", disabled: currentLanguage === "sl" },
-        ]}
-        onClick={handleClick}
-      />
-    </SettingsForm>
+      <LanguageButton />
+    </StyledLanguageSection>
   );
 };
