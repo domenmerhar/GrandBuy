7de7ceaad6cb31228cf3516b9cb66f5aefee3f3d
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-07 18:33:24 +0100

    Add ThemeButton.tsx

---
+++ client/src/Components/ThemeButton.tsx
@@ -0,0 +1,13 @@
+import { IconNav } from "../Util/IconNav";
+import { HiOutlineMoon, HiOutlineSun } from "react-icons/hi";
+import { useDarkMode } from "../contexts/DarkModeContext";
+
+export const ThemeButton = () => {
+  const { darkMode, toggleDarkMode } = useDarkMode();
+
+  return (
+    <IconNav onClick={toggleDarkMode}>
+      {darkMode ? <HiOutlineSun /> : <HiOutlineMoon />}
+    </IconNav>
+  );
+};
