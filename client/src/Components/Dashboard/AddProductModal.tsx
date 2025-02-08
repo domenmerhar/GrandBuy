57ae37d9ae416ded3e4f481d6c51e5f74a607e38
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-08 08:59:31 +0100

    Update AddProductModal.tsx

--- client/src/Components/Dashboard/AddProductModal.tsx
+++ client/src/Components/Dashboard/AddProductModal.tsx
@@ -3,7 +3,11 @@ import { Column } from "../../Util/Column";
 import { InputWithLabel } from "../../Util/InputWithLabel";
 import { Modal } from "../../Util/Modal";
 import { useRef, useState } from "react";
-import { FilePicker } from "../FilePicker";
+import { FilePicker } from "../Files/FilePicker";
+import { useJWT } from "../../hooks/useJWT";
+import { useAddProduct } from "../../hooks/products/useAddProduct";
+import { FilePickerDisplay } from "../Files/FilePickerDIsplay";
+import toast from "react-hot-toast";
 
 const Content = styled(Column)`
   & label {
@@ -12,15 +16,50 @@ const Content = styled(Column)`
 `;
 
 export const AddProductModal = () => {
-  const [selectedImages, setSelectedImages] = useState<string[]>([]);
+  const { JWT } = useJWT();
+  const { mutate: addProduct } = useAddProduct();
+
+  const [selectedImages, setSelectedImages] = useState<File[]>([]);
+  const [selectedDescription, setSelectedDescription] = useState<File[]>([]);
+  const [selectedCover, setSelectedCover] = useState<File[]>([]);
 
   const productRef = useRef<HTMLInputElement>(null);
   const priceRef = useRef<HTMLInputElement>(null);
   const shippingRef = useRef<HTMLInputElement>(null);
+  const discountRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = () => {
+    const name = productRef.current?.value;
+    const discount = Number(discountRef.current?.value);
+    const price = Number(priceRef.current?.value);
+    const shipping = Number(shippingRef.current?.value);
+
+    if (
+      !name ||
+      !discount ||
+      !price ||
+      !shipping ||
+      !selectedImages.length ||
+      !selectedDescription.length ||
+      !selectedCover.length
+    )
+      return toast.error("Please enter all fields.", { id: "add-product" });
+
+    addProduct({
+      JWT,
+      name: productRef.current?.value,
+      discount: Number(discountRef.current?.value),
+      price: Number(priceRef.current?.value),
+      shipping: Number(shippingRef.current?.value),
+      images: selectedImages,
+      description: selectedDescription[0],
+      coverImage: selectedCover[0],
+    });
+  };
 
   return (
-    <Modal.Window title="Add Product">
-      <Content $gap="1.2rem">
+    <Modal.Window title="Add Product" onSubmitApprove={handleSubmit}>
+      <Content $gap="1.2rem" as="form" onSubmit={handleSubmit}>
         <InputWithLabel
           id="product-name"
           placeholder="guitar"
@@ -45,24 +84,36 @@ export const AddProductModal = () => {
           ref={shippingRef}
         />
 
-        <FilePicker setSelectedFiles={setSelectedImages} multiple />
+        <InputWithLabel
+          id="discount"
+          placeholder="0"
+          title="Discount"
+          type="number"
+          ref={discountRef}
+        />
 
-        {selectedImages.length > 0 && (
-          <div>
-            {selectedImages.map((image, index) => (
-              <img
-                key={index}
-                src={image}
-                alt={`Selected ${index}`}
-                style={{
-                  width: "200px",
-                  objectFit: "cover",
-                  margin: "5px",
-                }}
-              />
-            ))}
-          </div>
-        )}
+        <FilePickerDisplay
+          label="Product cover"
+          id="product-cover"
+          selectedImages={selectedCover}
+          setSelectedImages={setSelectedCover}
+          multiple={false}
+        />
+
+        <FilePickerDisplay
+          label="Product images"
+          id="product-images"
+          selectedImages={selectedImages}
+          setSelectedImages={setSelectedImages}
+          multiple
+        />
+
+        <FilePicker
+          label="Product description (.md)"
+          id="product-description"
+          accept=".md, text/markdown"
+          setSelectedFiles={setSelectedDescription}
+        />
       </Content>
     </Modal.Window>
   );
