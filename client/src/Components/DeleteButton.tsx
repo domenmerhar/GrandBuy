11b2ebbdd0278ee11655052946230b5048d8dc21
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-08 09:12:20 +0100

    Add DeleteButton.tsx

---
+++ client/src/Components/DeleteButton.tsx
@@ -0,0 +1,18 @@
+import { HiOutlineTrash } from "react-icons/hi";
+import { SquareButton } from "../Util/SquareButton";
+import { FC } from "react";
+
+interface DeleteButtonProps {
+  handleDelete: (
+    e?: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => unknown;
+  size: "small" | "medium" | "large";
+}
+
+export const DeleteButton: FC<DeleteButtonProps> = ({ handleDelete, size }) => {
+  return (
+    <SquareButton $color="red" $size={size} onClick={handleDelete}>
+      <HiOutlineTrash />
+    </SquareButton>
+  );
+};
