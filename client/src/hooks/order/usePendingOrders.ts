8551814d98480721c1b7813eceb01e469293b28b
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-09 08:20:23 +0100

    Add usePendingOrders.ts

---
+++ client/src/hooks/order/usePendingOrders.ts
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import { useMe } from "../useMe";
+import { useJWT } from "../useJWT";
+import { getSellerOrderedItems } from "../../api/order/getSellerOrderedItems";
+
+export const usePendingOrders = () => {
+  const { JWT } = useJWT();
+  const { data } = useMe();
+
+  const sellerId = data?.data?._id;
+
+  return useQuery({
+    queryKey: ["seller-orders", sellerId, 1, "-createdAt", "all"],
+    queryFn: () =>
+      getSellerOrderedItems({
+        JWT,
+        page: 1,
+        sort: "-createdAt",
+        filter: "all",
+      }),
+  });
+};
