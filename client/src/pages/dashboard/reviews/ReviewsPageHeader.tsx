4e6469db3557b3b1fa90c4f705f7a4f2be0f7d6c
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-15 20:39:58 +0100

    Update ReviewsPageHeader.tsx

--- client/src/pages/dashboard/reviews/ReviewsPageHeader.tsx
+++ client/src/pages/dashboard/reviews/ReviewsPageHeader.tsx
@@ -2,27 +2,30 @@ import { Select } from "../../../Util/Select";
 import { Row } from "../../../Util/Row";
 import { Header } from "../../../Util/Header";
 import { IOption } from "../../../Util/types";
-
-const selectOptions: IOption[] = [
-  {
-    value: "all",
-    name: "All",
-  },
-  {
-    value: "oldest",
-    name: "Sort by age (oldest)",
-  },
-  {
-    value: "newest",
-    name: "Sort by age (newest)",
-  },
-];
+import { useTranslation } from "react-i18next";
 
 export const ReviewsPageHeader = () => {
+  const { t } = useTranslation();
+
+  const selectOptions: IOption[] = [
+    {
+      value: "all",
+      name: t("all"),
+    },
+    {
+      value: "oldest",
+      name: t("sortByDateOldest"),
+    },
+    {
+      value: "newest",
+      name: t("sortByDateNewest"),
+    },
+  ];
+
   return (
     <Row $flexWrap="wrap" $justifyContent="space-between" $alignItems="center">
       <Header as="h1" $color="orange" $size="medium">
-        Reviews
+        {t("reviews")}
       </Header>
 
       <Select options={selectOptions} />
