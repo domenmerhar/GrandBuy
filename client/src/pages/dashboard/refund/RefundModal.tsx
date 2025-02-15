0804e2bb02174efada1ee37b56fe690d4e78c937
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-15 20:39:24 +0100

    Update RefundModal.tsx

--- client/src/pages/dashboard/refund/RefundModal.tsx
+++ client/src/pages/dashboard/refund/RefundModal.tsx
@@ -1,14 +1,17 @@
+import { useTranslation } from "react-i18next";
 import { TextareaWithLabel } from "../../../Components/TextareaWithLabel";
 import { Column } from "../../../Util/Column";
 import { Modal } from "../../../Util/Modal";
 
 export const RefundModal = () => {
+  const { t } = useTranslation();
+
   return (
-    <Modal.Window title="Respond to request">
+    <Modal.Window title={t("respondToRequest")}>
       <Column $gap="8px">
         <TextareaWithLabel
-          label="Response"
-          placeholder="Write your response here"
+          label={t("response")}
+          placeholder={t("writeYourResponseHere")}
           id="response"
         />
       </Column>
