bc7fff24c837d66ce4e0399332c71f5c041d7e7c
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-15 20:34:59 +0100

    Update useAddProductToCard.ts

--- client/src/hooks/cart/useAddProductToCard.ts
+++ client/src/hooks/cart/useAddProductToCard.ts
@@ -1,22 +1,25 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { addProductToCart } from "../../api/cart/addProductToCard";
+import { useTranslation } from "react-i18next";
 
 export const useAddProductToCard = () => {
+  const { t } = useTranslation();
+
   const client = useQueryClient();
 
   return useMutation({
     mutationFn: addProductToCart,
 
     onMutate: () => {
-      toast.loading("Adding to cart...", { id: "add-to-cart" });
+      toast.loading(t("addingToCart"), { id: "add-to-cart" });
     },
 
     onSuccess: (data) => {
       if (data.status !== "success" || data?.errors?.length > 0)
         return toast.error("Failed to add to cart", { id: "add-to-cart" });
 
-      toast.success("Added to cart", { id: "add-to-cart" });
+      toast.success(t("addedToCart"), { id: "add-to-cart" });
 
       client.invalidateQueries({
         predicate: (query) => query.queryKey[0].includes("cart"),
