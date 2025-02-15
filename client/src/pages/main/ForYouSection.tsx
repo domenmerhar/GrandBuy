6afecf0a8b72a58e863d040eafbf2efe88f2a0a9
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-15 20:40:20 +0100

    Update ForYouSection.tsx

--- client/src/pages/main/ForYouSection.tsx
+++ client/src/pages/main/ForYouSection.tsx
@@ -4,16 +4,19 @@ import { Row } from "../../Util/Row";
 import { Under50 } from "./Under50";
 import { FreeShipping } from "./FreeShipping";
 import styled from "styled-components";
+import { useTranslation } from "react-i18next";
 
 const StyledForYouSection = styled(Column)`
   margin-bottom: 3.2rem;
 `;
 
 export const ForYouSection = () => {
+  const { t } = useTranslation();
+
   return (
     <StyledForYouSection>
       <Header $color="orange" $size="medium" as="h2">
-        Designed for you
+        {t("designedForYou")}
       </Header>
       <Row $gap="3.2rem" $flexWrap="wrap">
         <Under50 />
