aa25fe67469b51eb3a595ffcdf37758ea684d2a2
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-15 20:33:12 +0100

    Update SellerNavigationButtons.tsx

--- client/src/Components/Dashboard/SellerNavigationButtons.tsx
+++ client/src/Components/Dashboard/SellerNavigationButtons.tsx
@@ -2,23 +2,26 @@ import { NavigationTextButton } from "../../Util/NavigationTextButton";
 import { HiArrowUturnLeft } from "react-icons/hi2";
 import { BiPackage } from "react-icons/bi";
 import { HiOutlineEye, HiOutlineMicrophone } from "react-icons/hi";
+import { useTranslation } from "react-i18next";
 
 export const SellerNavigationButtons = () => {
+  const { t } = useTranslation();
+
   return (
     <>
       <NavigationTextButton to="overview" variant="dark">
         <HiOutlineEye size={24} />
-        Overview
+        {t("overview")}
       </NavigationTextButton>
 
       <NavigationTextButton to="reviews?sort=newest" variant="dark">
         <HiOutlineMicrophone size={24} />
-        Reviews
+        {t("reviews")}
       </NavigationTextButton>
 
       <NavigationTextButton to="orders?sort=oldest&filter=all" variant="dark">
         <BiPackage size={24} />
-        Orders
+        {t("orders")}
       </NavigationTextButton>
 
       <NavigationTextButton
@@ -27,7 +30,7 @@ export const SellerNavigationButtons = () => {
         variant="dark"
       >
         <HiArrowUturnLeft size={24} />
-        Refunds
+        {t("refunds")}
       </NavigationTextButton>
 
       <NavigationTextButton
@@ -51,7 +54,7 @@ export const SellerNavigationButtons = () => {
             d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 0 1 0 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 0 1 0-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375Z"
           />
         </svg>
-        Coupons
+        {t("coupons")}
       </NavigationTextButton>
     </>
   );
