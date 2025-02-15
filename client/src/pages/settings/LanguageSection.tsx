132fdd84cd65c490eca8560ac7ef5661e69e7274
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-15 20:53:49 +0100

    Update LanguageSection.tsx

--- client/src/pages/settings/LanguageSection.tsx
+++ client/src/pages/settings/LanguageSection.tsx
@@ -2,15 +2,18 @@ import styled from "styled-components";
 import { LanguageButton } from "../../Components/Button/LanguageButton";
 import { HeaderUppercaseBold } from "../../Util/HeaderUppercaseBold";
 import { SettingsForm } from "./SettingsForm";
+import { useTranslation } from "react-i18next";
 
 const StyledLanguageSection = styled(SettingsForm)`
   align-items: flex-start;
 `;
 
 export const LanguageSection = () => {
+  const { t } = useTranslation();
+
   return (
     <StyledLanguageSection as="div">
-      <HeaderUppercaseBold as="h2">Language</HeaderUppercaseBold>
+      <HeaderUppercaseBold as="h2">{t("language")}</HeaderUppercaseBold>
 
       <LanguageButton />
     </StyledLanguageSection>
