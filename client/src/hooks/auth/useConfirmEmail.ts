8f5274d89169bb0d692bbb587f7c8ab959e9441a
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-15 20:34:26 +0100

    Update useConfirmEmail.ts

--- client/src/hooks/auth/useConfirmEmail.ts
+++ client/src/hooks/auth/useConfirmEmail.ts
@@ -2,8 +2,10 @@ import { useMutation } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import { confirmEmail } from "../../api/auth/confirmEmail";
+import { useTranslation } from "react-i18next";
 
 export const useConfirmEmail = () => {
+  const { t } = useTranslation();
   const navigate = useNavigate();
 
   return useMutation({
@@ -11,7 +13,7 @@ export const useConfirmEmail = () => {
 
     onSuccess: (data) => {
       if (data.status !== "success" || data?.errors?.length > 0)
-        return toast.error("Couldn't confirm email, please try again.", {
+        return toast.error(t("couldntConfirmEmail"), {
           id: "confirmEmail",
         });
 
