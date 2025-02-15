fb26f8feee077d8030090c5d9c636a5ba4bc9802
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-15 20:32:53 +0100

    Update DashboardAdminList.tsx

--- client/src/Components/Dashboard/DashboardAdminList.tsx
+++ client/src/Components/Dashboard/DashboardAdminList.tsx
@@ -11,38 +11,40 @@ import {
   HiOutlineMegaphone,
   HiOutlineNoSymbol,
 } from "react-icons/hi2";
+import { useTranslation } from "react-i18next";
 
 export const DashboardAdminList = () => {
+  const { t } = useTranslation();
   return (
     <Column $gap="1.2rem">
       <NavigationTextButton to="overview" variant="dark">
         <HiOutlineEye size={24} />
-        Overview
+        {t("overview")}
       </NavigationTextButton>
 
       <NavigationTextButton to="ban" variant="dark">
         <HiOutlineNoSymbol size={24} />
-        Bans
+        {t("bans")}
       </NavigationTextButton>
 
       <NavigationTextButton to="admin" variant="dark">
         <HiOutlineUserGroup size={24} />
-        Admins
+        {t("admins")}
       </NavigationTextButton>
 
       <NavigationTextButton to="warning" variant="dark">
         <HiOutlineExclamationTriangle size={24} />
-        Warning
+        {t("warnings")}
       </NavigationTextButton>
 
       <NavigationTextButton to="notification" variant="dark">
         <HiOutlineBell size={24} />
-        Notifications
+        {t("notifications")}
       </NavigationTextButton>
 
       <NavigationTextButton to="request" variant="dark">
         <HiOutlineKey size={24} />
-        Requests
+        {t("requests")}
       </NavigationTextButton>
 
       <NavigationTextButton to="coupons" variant="dark" iconColoring="stroke">
@@ -62,12 +64,12 @@ export const DashboardAdminList = () => {
             d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 0 1 0 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 0 1 0-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375Z"
           />
         </svg>
-        Coupons
+        {t("coupons")}
       </NavigationTextButton>
 
       <NavigationTextButton to="report" variant="dark">
         <HiOutlineMegaphone size={24} />
-        Reports
+        {t("reports")}
       </NavigationTextButton>
     </Column>
   );
