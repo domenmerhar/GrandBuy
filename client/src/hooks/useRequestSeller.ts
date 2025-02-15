cbce209f33bb49c75442763a1731f0d0038161f6
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-15 20:34:06 +0100

    Update useRequestSeller.tsx

--- client/src/hooks/useRequestSeller.ts
+++ client/src/hooks/useRequestSeller.ts
@@ -1,23 +1,24 @@
 import { useMutation } from "@tanstack/react-query";
 import { requestSeller } from "../api/user/requestSeller";
 import toast from "react-hot-toast";
+import { useTranslation } from "react-i18next";
 
 export const useRequestSeller = () => {
+  const { t } = useTranslation();
   const { mutate } = useMutation({
     mutationFn: requestSeller,
 
-    onMutate: () =>
-      toast.loading("Sending request ...", { id: "requestSeller" }),
+    onMutate: () => toast.loading(t("sendingRequest"), { id: "requestSeller" }),
 
     onSuccess: (data) => {
       if (data?.status !== "success") {
-        toast.error("Something went wrong", {
+        toast.error(t("somethingWentWrong"), {
           id: "requestSeller",
         });
         return;
       }
 
-      toast.success("Request sent", { id: "requestSeller" });
+      toast.success(t("requestSent"), { id: "requestSeller" });
     },
   });
 
