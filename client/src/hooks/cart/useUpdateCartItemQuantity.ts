a4983606b091781b05fa2e4315cc94b5f484d523
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-15 20:35:40 +0100

    Update useUpdateCartItemQuantity.ts

--- client/src/hooks/cart/useUpdateCartItemQuantity.ts
+++ client/src/hooks/cart/useUpdateCartItemQuantity.ts
@@ -1,24 +1,26 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { updateCartItemQuantity } from "../../api/cart/updateCartItemQuantity";
+import { useTranslation } from "react-i18next";
 
 export const useUpdateCartItemQuantity = () => {
+  const { t } = useTranslation();
   const client = useQueryClient();
 
   return useMutation({
     mutationFn: updateCartItemQuantity,
 
     onMutate: () => {
-      toast.loading("Updating quantity...", { id: "update-cart-item" });
+      toast.loading(t("updatingQuantity"), { id: "update-cart-item" });
     },
 
     onSuccess: (data) => {
       if (data.status !== "success" || data?.errors?.length > 0)
-        return toast.error("Failed to update quantity", {
+        return toast.error(t("failedToUpdateQuantity"), {
           id: "update-cart-item",
         });
 
-      toast.success("Updated quantity", { id: "update-cart-item" });
+      toast.success(t("updatedQuantity"), { id: "update-cart-item" });
 
       client.invalidateQueries({
         predicate: (query) => query.queryKey[0].includes("cartItems"),
