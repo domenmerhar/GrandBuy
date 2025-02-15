fad66251bb6980a36717a240337322c79c58c243
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-15 20:40:06 +0100

    Update ReviewsPageOverviewCards.tsx

--- client/src/pages/dashboard/reviews/ReviewsPageOverviewCards.tsx
+++ client/src/pages/dashboard/reviews/ReviewsPageOverviewCards.tsx
@@ -3,6 +3,7 @@ import { OverviewCard } from "../../../Components/OverviewCard";
 import { BiStar } from "react-icons/bi";
 import { HiOutlineMicrophone } from "react-icons/hi";
 import { FC } from "react";
+import { useTranslation } from "react-i18next";
 
 interface ReviewsPageOverviewCardsProps {
   reviewsCount: number;
@@ -13,17 +14,19 @@ export const ReviewsPageOverviewCards: FC<ReviewsPageOverviewCardsProps> = ({
   reviewsCount,
   averageRating,
 }) => {
+  const { t } = useTranslation();
+
   return (
     <Row $gap="1.6rem" $flexWrap="wrap">
       <OverviewCard
         icon={<HiOutlineMicrophone />}
-        title="Reviews"
+        title={t("reviews")}
         content={String(reviewsCount)}
       />
 
       <OverviewCard
         icon={<BiStar />}
-        title="Average Rating"
+        title={t("averageRating")}
         content={String(averageRating)}
       />
     </Row>
