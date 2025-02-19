7c1db211ed6a9e307c6c6c27a3eab410fb009165
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-19 19:51:52 +0100

    Update OrderResponseModal.tsx

--- client/src/pages/dashboard/orders/OrderRespondModal.tsx
+++ client/src/pages/dashboard/orders/OrderRespondModal.tsx
@@ -1,16 +1,31 @@
 import { useSearchParams } from "react-router-dom";
 import { Modal } from "../../../Util/Modal";
 import { useTranslation } from "react-i18next";
+import { useShipOrder } from "../../../hooks/order/useShipOrder";
+import { useJWT } from "../../../hooks/useJWT";
+import toast from "react-hot-toast";
 
 export const OrderRespondModal = () => {
   const { t } = useTranslation();
-  const [, setSearchParams] = useSearchParams();
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const orderId = searchParams.get("orderId");
+
+  const { JWT } = useJWT();
+  const { mutate: shipOrder } = useShipOrder();
 
   const handleModalExit = () =>
     setSearchParams((searchParams) => {
       searchParams.delete("orderId");
       return searchParams;
     });
+
+  const handleShip = () => {
+    if (!orderId || !JWT)
+      return toast.error(t("somethingWentWrong"), { id: "handle-order" });
+
+    shipOrder({ orderId, JWT });
+  };
 
   return (
     <Modal.Window
@@ -22,11 +37,12 @@ export const OrderRespondModal = () => {
           color: "red",
           onClick: handleModalExit,
         },
+
         {
           key: "approve",
           text: t("approve"),
           color: "green",
-          onClick: handleModalExit,
+          onClick: handleShip,
         },
       ]}
       onClose={handleModalExit}
