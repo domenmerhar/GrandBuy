3d9879eccd7a8e43818fc821e03547773871ca3d
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-19 19:51:44 +0100

    Update useShipOrder.ts

---
+++ client/src/hooks/order/useShipOrder.ts
@@ -0,0 +1,28 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { useTranslation } from "react-i18next";
+import { shipOrder } from "../../api/order/shipOrder";
+
+export const useShipOrder = () => {
+  const { t } = useTranslation();
+  const client = useQueryClient();
+
+  return useMutation({
+    mutationFn: shipOrder,
+
+    onSuccess: (data) => {
+      console.log(data);
+
+      if (data.status !== "success" || data?.errors?.length > 0)
+        return toast.error(t("failedToShipOrder"), { id: "handle-order" });
+
+      client.invalidateQueries({
+        predicate: (query) =>
+          (query.queryKey[0] as string).includes("seller-orders") ||
+          query.queryKey[0] === "seller-orders",
+      });
+
+      toast.success(t("orderShipped"), { id: "handle-order" });
+    },
+  });
+};
