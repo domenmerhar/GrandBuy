a9b0aac197143170e404094af394f0086c7d86fa
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-22 09:37:22 +0100

    Add useGetSellerReviews.ts

---
+++ client/src/hooks/repliesReviews/useGetSellerReviews.ts
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import { useAuthContext } from "../../contexts/AuthContext";
+import { useSearchParams } from "react-router-dom";
+import { useMe } from "../useMe";
+import { SortCreatedAt } from "../../Util/types";
+import { getSellerReviews } from "../../api/repliesReviews/getSellerReviews";
+
+export const useGetSellerReviews = () => {
+  const [searchParams] = useSearchParams();
+  const { JWT } = useAuthContext();
+  const { data } = useMe();
+
+  const userId = data?.data?._id;
+  const sortStr = String(searchParams.get("sort"));
+
+  const page = Number(searchParams.get("page")) || 1;
+  const sort: SortCreatedAt =
+    sortStr === "+createdAt" ? "+createdAt" : "-createdAt";
+
+  return useQuery({
+    queryKey: ["seller-reviews", userId, page, sort],
+    queryFn: () => getSellerReviews({ JWT, page, sort }),
+  });
+};
