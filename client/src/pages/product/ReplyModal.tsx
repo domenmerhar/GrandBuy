c4997683d85c51c86f437a9d66c20720810bc473
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-22 12:52:46 +0100

    Updat ReplyModal.tsx

--- client/src/pages/product/ReplyModal.tsx
+++ client/src/pages/product/ReplyModal.tsx
@@ -1,35 +1,65 @@
 import { useTranslation } from "react-i18next";
 import { Modal } from "../../Util/Modal";
 import { useSearchParams } from "react-router-dom";
+import { TextareaWithLabel } from "../../Components/TextareaWithLabel";
+import { Column } from "../../Util/Column";
+import { useRef } from "react";
+import toast from "react-hot-toast";
+import useCreateReply from "../../hooks/repliesReviews/useCreateReply";
+import { useJWT } from "../../hooks/useJWT";
 
 export const ReplyModal = () => {
   const { t } = useTranslation();
-  const [, setSearchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { mutate: createReply } = useCreateReply();
+  const { JWT } = useJWT();
 
-  const handleReplyClose = () => {
+  const reviewId = searchParams.get("reply")!;
+
+  const removeParam = () => {
     setSearchParams((searchParams) => {
       searchParams.delete("reply");
       return searchParams;
     });
   };
 
+  const handleReply = () => {
+    removeParam();
+    if (!reviewRef.current?.value)
+      return toast.error(t("pleaseEnterAllFields"), { id: "reply" });
+
+    createReply({ JWT, reviewId, reply: reviewRef.current.value });
+  };
+
+  const reviewRef = useRef<HTMLTextAreaElement>(null);
+
   return (
     <Modal.Window
       title={t("reply")}
-      onClose={handleReplyClose}
+      onClose={removeParam}
       buttons={[
         {
           key: "cancel",
           text: t("cancel"),
           color: "red",
-          onClick: handleReplyClose,
+          onClick: removeParam,
+        },
+        {
+          key: "reply",
+          text: t("reply"),
+          color: "green",
+          onClick: handleReply,
         },
       ]}
     >
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita
-      possimus nesciunt eveniet at vitae officiis, illum architecto animi atque
-      nemo qui, ab commodi. Beatae, quod mollitia optio tenetur voluptatem
-      molestias.
+      <Column $gap=".8rem">
+        <TextareaWithLabel
+          id="review"
+          label={t("review")}
+          ref={reviewRef}
+          maxLength={500}
+        />
+      </Column>
     </Modal.Window>
   );
 };
