80938b177deaffb4b6d3efd55392b41db9617b2c
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-22 09:36:38 +0100

    Add getSellerReviews.ts

---
+++ client/src/api/repliesReviews/getSellerReviews.ts
@@ -0,0 +1,31 @@
+import { toApiPath } from "../../functions/toApiPath";
+import { SortCreatedAt } from "../../Util/types";
+
+export const getSellerReviews = async ({
+  JWT,
+  page,
+  sort,
+}: {
+  JWT: string;
+  page: number;
+  sort: SortCreatedAt;
+}) => {
+  const limit = Number(import.meta.env.VITE_PRODUCTS_PER_STEPPER);
+
+  const queryParamsStr = [
+    limit && `limit=${limit}`,
+    page && `page=${page}`,
+    sort && `sort=${sort}`,
+  ].join("&");
+
+  const res = await fetch(toApiPath(`review/seller?${queryParamsStr}`), {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${JWT}`,
+    },
+  });
+
+  const data = await res.json();
+
+  return data;
+};
