99d32bbbd7cf0e2cedceb091ca87d721724d0799
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-22 09:38:00 +0100

    Update ReviewsPage.tsx

--- client/src/pages/dashboard/reviews/ReviewsPage.tsx
+++ client/src/pages/dashboard/reviews/ReviewsPage.tsx
@@ -1,21 +1,29 @@
-import { Row } from "../../../Util/Row";
-import { ReviewCardDashboard } from "./ReviewCardDashboard";
-import { Modal } from "../../../Util/Modal";
 import { ReviewsPageHeader } from "./ReviewsPageHeader";
 import { ReviewsPageOverviewCards } from "./ReviewsPageOverviewCards";
+import { Stepper } from "../../../Util/Stepper";
+import { DashboardReviews } from "./DashboardReviews";
+import useGetSellerReviewCount from "../../../hooks/repliesReviews/useGetSellerReviewCount";
+import useGetSellerAverageRating from "../../../hooks/repliesReviews/useGetSellerAverageRating";
 
 export const ReviewsPage = () => {
+  const { data: reviewResponse } = useGetSellerReviewCount();
+  const { data: averageRatingResponse } = useGetSellerAverageRating();
+
+  const reviewsCount = reviewResponse?.data?.totalCount || 0;
+  const averageRating =
+    Number(averageRatingResponse?.data?.averageRating?.toFixed(1)) || 0;
+
   return (
     <>
       <ReviewsPageHeader />
 
-      <ReviewsPageOverviewCards reviewsCount={10} averageRating={4.5} />
+      <ReviewsPageOverviewCards
+        reviewsCount={reviewsCount}
+        averageRating={averageRating}
+      />
+      <DashboardReviews />
 
-      <Row $alignItems="flex-start">
-        <Modal>
-          <ReviewCardDashboard />
-        </Modal>
-      </Row>
+      <Stepper searchParamName="page" />
     </>
   );
 };
