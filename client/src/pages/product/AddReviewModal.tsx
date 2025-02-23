0e80988582eb6aea58c21c0baf298fd0bc09557a
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-23 10:29:28 +0100

    Update AddReviewModal.tsx

--- client/src/pages/product/AddReviewModal.tsx
+++ client/src/pages/product/AddReviewModal.tsx
@@ -1,20 +1,38 @@
 import { Column } from "../../Util/Column";
 import { Modal } from "../../Util/Modal";
 import { RatingInteractive } from "../search/RatingInteractive";
-import { useSearchParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import { useRef } from "react";
 import { TextareaWithLabel } from "../../Components/TextareaWithLabel";
 import { useTranslation } from "react-i18next";
+import useCreateReviewOnProduct from "../../hooks/repliesReviews/useCreateReviewOnProduct";
+import toast from "react-hot-toast";
+import { useJWT } from "../../hooks/useJWT";
 
 export const AddReviewModal = () => {
   const { t } = useTranslation();
-  const { setIsOpen } = Modal.useModalContext();
-  const [, setSearchParams] = useSearchParams();
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { JWT } = useJWT();
+  const { mutate: createReview } = useCreateReviewOnProduct();
+  const { productId } = useParams();
+
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+  const rating = Number(searchParams.get("rating"));
 
   const handleSubmit = () => {
     removeRatingParam();
-    setIsOpen(false);
+
+    if (!textAreaRef.current?.value || !rating)
+      toast.error(t("fillAllFields"), { id: "review" });
+
+    createReview({
+      JWT,
+      rating,
+      productId: String(productId),
+      review: String(textAreaRef.current?.value),
+    });
   };
 
   const removeRatingParam = () =>
