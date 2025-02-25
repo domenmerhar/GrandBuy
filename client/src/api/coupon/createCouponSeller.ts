72f0e1bb4fa45b3fc69e96a852af0b46c5b2284f
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-25 12:32:55 +0100

    Add createCouponSeller.ts

---
+++ client/src/api/coupon/createCouponSeller.ts
@@ -0,0 +1,32 @@
+import { toApiPath } from "../../functions/toApiPath";
+
+export default async function createCouponSeller({
+  JWT,
+  code,
+  products,
+  discount,
+  expireAt,
+}: {
+  JWT: string;
+  code: string;
+  products: string[];
+  discount: number;
+  expireAt: Date;
+}) {
+  const Authorization = `Bearer ${JWT}`;
+  const body = JSON.stringify({ code, products, discount, expireAt });
+
+  console.log(body);
+
+  const response = await fetch(toApiPath(`coupon/seller`), {
+    method: "POST",
+    headers: {
+      Authorization,
+      "Content-Type": "application/json",
+    },
+    body,
+  });
+
+  const data = await response.json();
+  return data;
+}
