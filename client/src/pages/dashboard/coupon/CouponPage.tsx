0dfcc2007d01dee315b068c2a6605e555ed76b30
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-25 12:34:12 +0100

    Update CouponPage.tsx

--- client/src/pages/dashboard/coupon/CouponPage.tsx
+++ client/src/pages/dashboard/coupon/CouponPage.tsx
@@ -1,13 +1,14 @@
+import { Modal } from "../../../Util/Modal";
 import { CouponOverview } from "./CouponOverview";
 import { CouponPageHeader } from "./CouponPageHeader";
 import { Coupons } from "./Coupons";
 
 export const CouponPage = () => {
   return (
-    <>
+    <Modal>
       <CouponPageHeader />
       <CouponOverview />
       <Coupons />
-    </>
+    </Modal>
   );
 };
