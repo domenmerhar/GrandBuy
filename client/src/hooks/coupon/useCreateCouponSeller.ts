2934552a8b7bd48b25b78d44b0eea82960ed5c28
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-25 12:33:28 +0100

    Add useCreateCouponSeller.ts

---
+++ client/src/hooks/coupon/useCreateCouponSeller.ts
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { useTranslation } from "react-i18next";
+import createCouponSeller from "../../api/coupon/createCouponSeller";
+
+export default function useCreateCouponSeller() {
+  const { t } = useTranslation();
+  const client = useQueryClient();
+
+  return useMutation({
+    mutationFn: createCouponSeller,
+
+    onSuccess: (data) => {
+      console.log(data);
+
+      if (data.status !== "success" || data?.errors?.length > 0)
+        return toast.error(t("somethingWentWrong"), { id: "coupon" });
+
+      client.invalidateQueries({
+        predicate: (query) => String(query.queryKey[0]).includes("coupon"),
+      });
+    },
+  });
+}
