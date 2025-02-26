48c04d910c11db270480fcd3800b9289e4f424eb
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-26 17:05:56 +0100

    Update Select.tsx

--- client/src/Util/Select.tsx
+++ client/src/Util/Select.tsx
@@ -14,6 +14,7 @@ const StyledSelect = styled.select`
   border: none;
   box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
   border-radius: 12px;
+  font-size: 1.6rem;
 `;
 
 const Option = styled.option``;
