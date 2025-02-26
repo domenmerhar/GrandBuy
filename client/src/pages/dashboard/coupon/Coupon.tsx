632e74e966fd184fba5cce58bc8f65c003707882
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-26 08:45:23 +0100

    Update Coupon.tsx

--- client/src/pages/dashboard/coupon/Coupon.tsx
+++ client/src/pages/dashboard/coupon/Coupon.tsx
@@ -1,14 +1,26 @@
 import styled from "styled-components";
 import { BadgeCard } from "../../../Util/BadgeCard";
 import { Column } from "../../../Util/Column";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { HiChevronDown } from "react-icons/hi";
 
 const GrayedText = styled.span`
   color: var(--gray-7);
 `;
 
+const ChevronHolder = styled.span<{ $isOpen: boolean }>`
+  transition: all 200ms;
+  transform: ${({ $isOpen }) => ($isOpen ? "rotate(180deg)" : "rotate(0deg)")};
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
 const AffectedItems = styled.p`
+  display: flex;
+  align-items: center;
   font-weight: 550;
   font-size: 1.8rem;
 `;
@@ -36,6 +48,8 @@ export const Coupon: FC<Coupon> = ({
   affectedItems,
 }) => {
   const { t } = useTranslation();
+  const isShort = affectedItems.length < 10;
+  const [isOpen, setIsOpen] = useState<boolean>(isShort);
 
   return (
     <BadgeCard>
@@ -54,12 +68,34 @@ export const Coupon: FC<Coupon> = ({
       </Column>
 
       <Column $gap="4px">
-        <AffectedItems>{t("affectedItems")}:</AffectedItems>
-        <BadgeCard.ItemList>
-          {affectedItems.map(({ _id, name }: { _id: string; name: string }) => (
-            <Li key={_id}>{name}</Li>
-          ))}
-        </BadgeCard.ItemList>
+        <AffectedItems onClick={() => setIsOpen((prev) => !prev)}>
+          {!isShort ? (
+            <ChevronHolder $isOpen={isOpen}>
+              <HiChevronDown />
+            </ChevronHolder>
+          ) : null}
+          {t("affectedItems")}:
+        </AffectedItems>
+        {isOpen || isShort ? (
+          <BadgeCard.ItemList>
+            {affectedItems.map(
+              ({ _id, name }: { _id: string; name: string }) => (
+                <Li key={_id}>{name}</Li>
+              )
+            )}
+          </BadgeCard.ItemList>
+        ) : (
+          <>
+            <BadgeCard.ItemList>
+              {affectedItems
+                .slice(0, 5)
+                .map(({ _id, name }: { _id: string; name: string }) => (
+                  <Li key={_id}>{name}</Li>
+                ))}
+              ...
+            </BadgeCard.ItemList>
+          </>
+        )}
       </Column>
     </BadgeCard>
   );
