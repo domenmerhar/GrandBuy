062ba58296269f95173227d006e35e4259f2c6be
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-26 10:54:07 +0100

    Update toApiFilesPath.ts

--- client/src/functions/toApiFilesPath.ts
+++ client/src/functions/toApiFilesPath.ts
@@ -1,2 +1,4 @@
-export const toApiFilesPath = (file: string) =>
-  `${import.meta.env.VITE_API_LINK}/files/${file}`;
+export const toApiFilesPath = (file: string | undefined) =>
+  file
+    ? `${import.meta.env.VITE_API_LINK}/files/${file}`
+    : import.meta.env.VITE_PLACEHOLDER_IMAGE;
