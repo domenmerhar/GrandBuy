18477230abaa6f41c4e7d1223cfd14d84833f94e
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-26 09:44:19 +0100

    Update CouponPage.tsx

--- client/src/pages/dashboard/coupon/CouponPage.tsx
+++ client/src/pages/dashboard/coupon/CouponPage.tsx
@@ -1,14 +1,38 @@
+import { useState } from "react";
 import { Modal } from "../../../Util/Modal";
+import CouponModal from "./CouponModal";
 import { CouponOverview } from "./CouponOverview";
 import { CouponPageHeader } from "./CouponPageHeader";
 import { Coupons } from "./Coupons";
+import oneWeekInFuture from "../../../functions/oneWeekInFuture";
 
 export const CouponPage = () => {
+  const [code, setCode] = useState<string>("");
+  const [discount, setDiscount] = useState<number | "">("");
+  const [expireAt, setExpireAt] = useState<number>(oneWeekInFuture());
+  const [productIds, setProductIds] = useState<string[]>([]);
+
   return (
     <Modal>
       <CouponPageHeader />
       <CouponOverview />
-      <Coupons />
+      <Coupons
+        setCode={setCode}
+        setDiscount={setDiscount}
+        setExpireAt={setExpireAt}
+        setProductIds={setProductIds}
+      />
+
+      <CouponModal
+        code={code}
+        setCode={setCode}
+        discount={discount}
+        setDiscount={setDiscount}
+        expireAt={expireAt}
+        setExpireAt={setExpireAt}
+        productIds={productIds}
+        setProductIds={setProductIds}
+      />
     </Modal>
   );
 };
