1b7170ce77ae1902c121417a0955ae3664ffee50
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-26 10:15:33 +0100

    Add useExpireCoupon.ts

---
+++ client/src/hooks/coupon/useExpireCoupon.ts
@@ -0,0 +1,23 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { useTranslation } from "react-i18next";
+import expireCoupon from "../../api/coupon/expireCoupon";
+
+export default function useExpireCoupon() {
+  const { t } = useTranslation();
+  const client = useQueryClient();
+
+  return useMutation({
+    mutationFn: expireCoupon,
+
+    onSettled: (data) => {
+      if (!data)
+        return client.invalidateQueries({
+          predicate: (query) =>
+            String(query.queryKey[0]).includes("coupons-seller"),
+        });
+
+      return toast.error(t("somethingWentWrong"), { id: "coupon" });
+    },
+  });
+}
