4c5645116aa866077c59151cc7c75d3cae4780bf
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-26 09:44:27 +0100

    Update Coupons.tsx

--- client/src/pages/dashboard/coupon/Coupons.tsx
+++ client/src/pages/dashboard/coupon/Coupons.tsx
@@ -4,52 +4,86 @@ import { useInfinite } from "../../../hooks/useInfinite";
 import { useJWT } from "../../../hooks/useJWT";
 import { useMe } from "../../../hooks/useMe";
 import { useSearchParams } from "react-router-dom";
-import { ICoupon, SortCreatedAt, SortDiscount } from "../../../Util/types";
+import {
+  CouponProps,
+  ICoupon,
+  SortCreatedAt,
+  SortDiscount,
+} from "../../../Util/types";
 import { Coupon } from "./Coupon";
 import { toDate } from "../../../functions/toDate";
+import styled from "styled-components";
+import React from "react";
 
-export const Coupons = () => {
-  const { JWT } = useJWT();
-  const { data: dataMe } = useMe();
-  const [searchParams] = useSearchParams();
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(25ch, 35ch));
+  justify-content: space-between;
+  gap: 3.2rem;
+`;
 
-  const userId = dataMe?.data?._id;
+export const Coupons = React.memo(
+  ({ setCode, setDiscount, setExpireAt, setProductIds }: CouponProps) => {
+    const { JWT } = useJWT();
+    const { data: dataMe } = useMe();
+    const [searchParams] = useSearchParams();
 
-  const sortStr = searchParams.get("sort")!;
-  const sort = ["+createdAt", "-createdAt", "+discount", "-discount"].includes(
-    sortStr
-  )
-    ? (sortStr as SortCreatedAt | SortDiscount)
-    : "+createdAt";
+    const userId = dataMe?.data?._id;
 
-  const queryFn = ({ pageParam }: { pageParam: unknown }) => {
-    if (pageParam === null) return;
+    const sortStr = searchParams.get("sort")!;
+    let sort: SortCreatedAt | SortDiscount;
+    switch (sortStr) {
+      case "oldest":
+        sort = "+createdAt";
+        break;
 
-    return getCouponsSeller({
-      page: Number(pageParam),
-      sort,
-      JWT: JWT,
+      case "discount-highest":
+        sort = "-discount";
+        break;
+
+      case "discount-lowest":
+        sort = "+discount";
+        break;
+
+      default:
+        sort = "-createdAt";
+        break;
+    }
+
+    const queryFn = ({ pageParam }: { pageParam: unknown }) => {
+      if (pageParam === null) return;
+
+      return getCouponsSeller({
+        page: Number(pageParam),
+        sort,
+        JWT: JWT,
+      });
+    };
+
+    const data = useInfinite({
+      queryFn,
+      queryKey: ["coupons-seller", userId, sort],
     });
-  };
 
-  const data = useInfinite({
-    queryFn,
-    queryKey: ["couponsSeller", userId, sort],
-  });
+    const renderFn = ({ data }: { data: { coupons: ICoupon[] } }) => {
+      return data?.coupons?.map(
+        ({ _id, code, expireAt, discount, products }: ICoupon) => (
+          <Coupon
+            key={_id}
+            couponId={_id}
+            affectedItems={products}
+            code={code}
+            validUntil={toDate(expireAt)}
+            discount={discount}
+            setCode={setCode}
+            setDiscount={setDiscount}
+            setExpireAt={setExpireAt}
+            setProductIds={setProductIds}
+          />
+        )
+      );
+    };
 
-  const renderFn = ({ data }: { data: { coupons: ICoupon[] } }) => {
-    return data?.coupons?.map(
-      ({ _id, code, expireAt, discount, products }: ICoupon) => (
-        <Coupon
-          key={_id}
-          affectedItems={products}
-          code={code}
-          validUntil={toDate(expireAt)}
-          discount={discount}
-        />
-      )
-    );
-  };
-
-  return <InfiniteProducts {...data} renderFn={renderFn} />;
-};
+    return <InfiniteProducts {...data} renderFn={renderFn} container={Grid} />;
+  }
+);
