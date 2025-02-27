b09511fb9dfe931ec0ff93a9b35fefd33c3e3198
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-27 10:40:56 +0100

    Update DashboardRefunds.tsx

--- client/src/pages/dashboard/refund/DashboardRefunds.tsx
+++ client/src/pages/dashboard/refund/DashboardRefunds.tsx
@@ -1,23 +1,45 @@
+import { ErrorBox } from "../../../Components/ErrorBox";
+import { SpinnerInBox } from "../../../Components/SpinnerInBox";
+import useGetSellerRefunds from "../../../hooks/refund/useGetSellerRefunds";
 import { Modal } from "../../../Util/Modal";
-import { Row } from "../../../Util/Row";
-import { RefundCard } from "../../refund/RefundCard";
+import RefundCard from "../../refund/RefundCard";
 import { RefundModal } from "./RefundModal";
+import { RefundItem } from "../../../Util/types/index";
+import styled from "styled-components";
+
+const RefundGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(35rem, 1fr));
+  gap: 1.6rem;
+  justify-content: space-between;
+`;
+
 export const DashboardRefunds = () => {
   const { setIsOpen } = Modal.useModalContext();
 
-  return (
-    <Row
-      $gap="3.2rem"
-      $justifyContent="center"
-      $alignItems="flex-start"
-      $flexWrap="wrap"
-    >
-      <RefundCard />
-      <RefundCard />
-      <RefundCard />
-      <RefundCard />
+  const { data, error, isLoading } = useGetSellerRefunds();
 
+  if (isLoading) return <SpinnerInBox fullPage={false} />;
+  if (error) return <ErrorBox />;
+
+  return (
+    <RefundGrid>
+      {data?.data?.refunds?.map(
+        ({ _id, createdAt, reason, cartItemId, status, user }: RefundItem) => (
+          <RefundCard
+            key={_id}
+            date={createdAt}
+            productName={cartItemId?.name}
+            productQuantity={cartItemId?.quantity}
+            reason={reason}
+            status={status}
+            userImage={user?.image}
+            username={user?.username}
+            userId={user?._id}
+          />
+        )
+      )}
       <RefundModal />
-    </Row>
+    </RefundGrid>
   );
 };
