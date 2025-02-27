0c6dfafcfff2533ee022650a390c25c180077224
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-27 10:39:40 +0100

    Add getSellerRefunds.ts

---
+++ client/src/api/refund/getSellerRefunds.ts
@@ -0,0 +1,33 @@
+import { toApiPath } from "../../functions/toApiPath";
+import { RefundStatus, SortCreatedAt } from "../../Util/types";
+
+export const getSellerRefunds = async ({
+  JWT,
+  page,
+  status,
+  sort,
+}: {
+  JWT: string;
+  page: number;
+  status?: RefundStatus;
+  sort: SortCreatedAt;
+}) => {
+  const limit = Number(import.meta.env.VITE_REFUNDS_NOTIFICATIONS_PAGE_SIZE);
+
+  const res = await fetch(
+    toApiPath(
+      `refund/seller?page=${page}&limit=${limit}&sort=${sort}${status && `&status=${status}`}`
+    ),
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${JWT}`,
+      },
+    }
+  );
+
+  const data = await res.json();
+  const nextItem = data?.refunds?.length === limit ? page + 1 : null;
+
+  return { ...data, nextItem };
+};
