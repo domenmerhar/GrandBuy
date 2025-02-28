098692bb541a8277d6fcdaf46c50f1f9cc0224d1
Domen Merhar <domen.merhar@students.vegova.si>
2025-02-28 13:47:25 +0100

    Add useEditProduct.ts

---
+++ client/src/hooks/products/useEditProduct.ts
@@ -0,0 +1,27 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { useTranslation } from "react-i18next";
+import { Modal } from "../../Util/Modal";
+import { useParams } from "react-router-dom";
+import { updateProduct } from "../../api/product/updateProduct";
+
+export const useEditProduct = () => {
+  const { t } = useTranslation();
+  const client = useQueryClient();
+  const { closeModal } = Modal.useModalContext();
+
+  const { productId } = useParams();
+
+  return useMutation({
+    mutationFn: updateProduct,
+
+    onSuccess: (data) => {
+      console.log(data);
+      if (data.status !== "success" || data?.errors?.length > 0)
+        return toast.error(t("somethingWentWrong"), { id: "edit-product" });
+
+      client.invalidateQueries({ queryKey: ["product", productId] });
+      closeModal();
+    },
+  });
+};
