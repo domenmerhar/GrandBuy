96997699b96d1eae76d33bc54f7e6196798f3f38
Domen Merhar <domen.merhar@students.vegova.si>
2025-03-03 11:48:33 +0100

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -23,14 +23,14 @@ import stripeWebhookRouter from "./router/stripeWebhookRouter";
 
 const hourLimiter = rateLimit({
   windowMs: 60 * 60 * 1000,
-  limit: 100,
+  limit: 1000,
   standardHeaders: "draft-7",
   legacyHeaders: false,
 });
 
 const dayLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  limit: 400,
+  limit: 2000,
   standardHeaders: "draft-7",
   legacyHeaders: false,
 });
