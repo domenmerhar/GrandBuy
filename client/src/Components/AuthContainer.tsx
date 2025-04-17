28d522327b3936f143504507ed393f799cb8ab56
Domen Merhar <domen.merhar@students.vegova.si>
2025-04-17 18:18:43 +0200

    Update AuthContainer.tsx

--- client/src/Components/AuthContainer.tsx
+++ client/src/Components/AuthContainer.tsx
@@ -7,7 +7,7 @@ interface AuthContainerProps {
 }
 
 const BackgroundImage = styled.div`
-  background-image: url("/src/public/shopping-bag.jpg");
+  background-image: url("https://images.unsplash.com/photo-1573855619003-97b4799dcd8b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
   width: 100vw;
   height: 100vh;
 
